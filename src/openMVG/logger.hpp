8f8a21e707830fa7b32ed8d84ebc774135b1792e
Clement Aymard <blog.clm@gmail.com>
2016-04-21 11:46:16 +0200

    first approach for boost log system
    poparteu/openMVG#97

--- src/openMVG/logger.hpp
+++ src/openMVG/logger.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
+#define BOOST_LOG_DYN_LINK 1
+
+#include <boost/log/trivial.hpp>
 
 // Defines simple logging macros for the popart project
 //
 #ifdef WANTS_POPART_COUT
-#define POPART_COUT(x) std::cout << x << std::endl
-#define POPART_CERR(x) std::cerr << x << std::endl
-#define POPART_COUT_DEBUG(x) std::cout << x << std::endl
+#define POPART_COUT(x) BOOST_LOG_TRIVIAL(info) << x
+#define POPART_CERR(x) BOOST_LOG_TRIVIAL(error) << x
+#define POPART_COUT_DEBUG(x) BOOST_LOG_TRIVIAL(debug) << x
 #else
 #define POPART_COUT(x) 
 #define POPART_CERR(x) 
