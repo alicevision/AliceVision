fca2d8c212c860dafbc6acfa4f0e3cc99a390fd0
Lilian Calvet <lilian.calvet@gmail.com>
2016-07-22 22:15:14 +0200

    [popsift] Misc script/install.

---
+++ sift.sh
@@ -0,0 +1,3 @@
+mkdir -p /home/lilian/data/Features_Repeatability/vgg_oxford_feat_eval/boat/$2/
+
+Linux-x86_64-RELEASE/openMVG_sample_siftPutative -d $1 -i /home/lilian/data/Features_Repeatability/vgg_oxford_feat_eval/boat/img$3.ppm -o /home/lilian/data/Features_Repeatability/vgg_oxford_feat_eval/boat/$2/
