209bc0d9f4c778292f61dd3b2f50b4a2c0e33224
Simone Gasparini <simone.gasparini@gmail.com>
2017-01-23 23:18:34 +0100

    [software] fix wrong doc command line

--- src/software/SfM/main_ComputeFeatures.cpp
+++ src/software/SfM/main_ComputeFeatures.cpp
@@ -265,7 +265,7 @@ int main(int argc, char **argv)
     << "   AKAZE_MLDB:  AKAZE with binary descriptors\n"
 #ifdef HAVE_CCTAG
       << "   CCTAG3: CCTAG markers with 3 crowns\n"
-      << "   CCTAG3: CCTAG markers with 4 crowns\n"
+      << "   CCTAG4: CCTAG markers with 4 crowns\n"
       << "   SIFT_CCTAG3: CCTAG markers with 3 crowns\n" 
       << "   SIFT_CCTAG4: CCTAG markers with 4 crowns\n" 
 #endif
