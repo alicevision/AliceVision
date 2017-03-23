65d9a819e55c67fda147cc1363fa4dd2784e9654
Simone Gasparini <simone.gasparini@gmail.com>
2017-03-23 15:23:35 +0100

    [multiview] fix format (tabs)

--- src/openMVG/multiview/triangulation_nview.cpp
+++ src/openMVG/multiview/triangulation_nview.cpp
@@ -55,95 +55,98 @@ namespace openMVG {
       0, -1,  x(1),
       1, 0, -x(0);
     return skew;
+}
+
+void TriangulateNViewAlgebraic(const Mat2X &x,
+                               const std::vector< Mat34 > &Ps,
+                               Vec4 *X)
+{
+  Mat2X::Index nviews = x.cols();
+  assert(nviews == Ps.size());
+
+  Mat design(2 * nviews, 4);
+  for(int i = 0; i < nviews; i++)
+  {
+    design.block<2, 4>(2 * i, 0) = SkewMatMinimal(x.col(i)) * Ps[i];
   }
+  Nullspace(&design, X);
+}
 
-  void TriangulateNViewAlgebraic(const Mat2X &x,
-    const std::vector< Mat34 > &Ps,
-    Vec4 *X) {
-      Mat2X::Index nviews = x.cols();
-      assert(nviews == Ps.size());
+double Triangulation::error(const Vec3 &X) const
+{
+  double squared_reproj_error = 0.0;
+  for(std::size_t i = 0; i < views.size(); i++)
+  {
+    const Mat34& PMat = views[i].first;
+    const Vec2 & xy = views[i].second;
+    const Vec2 p = Project(PMat, X);
+    squared_reproj_error += (xy - p).norm();
+  }
+  return squared_reproj_error;
+}
 
-      Mat design(2*nviews, 4);
-      for (int i = 0; i < nviews; i++) {
-        design.block<2, 4>(2*i, 0) = SkewMatMinimal(x.col(i)) * Ps[i];
+// Camera triangulation using the iterated linear method
+
+Vec3 Triangulation::compute(int iter) const
+{
+  const int nviews = int(views.size());
+  assert(nviews >= 2);
+
+  // Iterative weighted linear least squares
+  Mat3 AtA;
+  Vec3 Atb, X;
+  std::vector<double> weights(nviews, double(1.0));
+  for(int it = 0; it < iter; ++it)
+  {
+    AtA.fill(0.0);
+    Atb.fill(0.0);
+    for(int i = 0; i < nviews; ++i)
+    {
+      const Mat34& PMat = views[i].first;
+      const Vec2 & p = views[i].second;
+      const double w = weights[i];
+
+      Vec3 v1, v2;
+      for(int j = 0; j < 3; j++)
+      {
+        v1[j] = w * (PMat(0, j) - p(0) * PMat(2, j));
+        v2[j] = w * (PMat(1, j) - p(1) * PMat(2, j));
+        Atb[j] += w * (v1[j] * (p(0) * PMat(2, 3) - PMat(0, 3))
+                + v2[j] * (p(1) * PMat(2, 3) - PMat(1, 3)));
       }
-      Nullspace(&design, X);
+
+      for(int k = 0; k < 3; k++)
+      {
+        for(int j = 0; j <= k; j++)
+        {
+          const double v = v1[j] * v1[k] + v2[j] * v2[k];
+          AtA(j, k) += v;
+          if(j < k) AtA(k, j) += v;
+        }
+      }
+    }
+
+    X = AtA.inverse() * Atb;
+
+    // Compute reprojection error, min and max depth, and update weights
+    zmin = std::numeric_limits<double>::max();
+    zmax = -std::numeric_limits<double>::max();
+    err = 0;
+    for(int i = 0; i < nviews; ++i)
+    {
+      const Mat34& PMat = views[i].first;
+      const Vec2 & p = views[i].second;
+      const Vec3 xProj = PMat * Vec4(X(0), X(1), X(2), 1.0);
+      const double z = xProj(2);
+      const Vec2 x = xProj.head<2>() / z;
+      if(z < zmin) zmin = z;
+      if(z > zmax) zmax = z;
+      err += (p - x).norm();
+      weights[i] = 1.0 / z;
+    }
   }
-
-	double Triangulation::error(const Vec3 &X) const
-	{
-		double squared_reproj_error = 0.0;
-		for (std::size_t i=0;i<views.size();i++)
-		{
-			const Mat34& PMat = views[i].first;
-			const Vec2 & xy = views[i].second;
-			const Vec2 p = Project(PMat, X);
-			squared_reproj_error += (xy-p).norm();
-		}
-		return squared_reproj_error;
-	}
-
-	// Camera triangulation using the iterated linear method
-	Vec3 Triangulation::compute(int iter) const
-	{
-		const int nviews = int(views.size());
-		assert(nviews>=2);
-
-		// Iterative weighted linear least squares
-		Mat3 AtA;
-		Vec3 Atb, X;
-		std::vector<double> weights(nviews,double(1.0));
-		for (int it=0;it<iter;++it)
-		{
-			AtA.fill(0.0);
-			Atb.fill(0.0);
-			for (int i=0;i<nviews;++i)
-			{
-				const Mat34& PMat = views[i].first;
-				const Vec2 & p = views[i].second;
-				const double w = weights[i];
-
-				Vec3 v1,v2;
-				for (int j=0;j<3;j++)
-				{
-					v1[j] = w * ( PMat(0,j) - p(0) * PMat(2,j) );
-					v2[j] = w * ( PMat(1,j) - p(1) * PMat(2,j) );
-					Atb[j] += w * ( v1[j] * ( p(0) * PMat(2,3) - PMat(0,3) )
-            + v2[j] * ( p(1) * PMat(2,3) - PMat(1,3) ) );
-				}
-
-				for (int k=0;k<3;k++)
-				{
-					for (int j=0;j<=k;j++)
-					{
-						const double v = v1[j] * v1[k] + v2[j] * v2[k];
-						AtA(j,k) += v;
-						if (j<k) AtA(k,j) += v;
-					}
-				}
-			}
-
-			X = AtA.inverse() * Atb;
-
-			// Compute reprojection error, min and max depth, and update weights
-			zmin = std::numeric_limits<double>::max();
-			zmax = - std::numeric_limits<double>::max();
-			err = 0;
-			for (int i=0;i<nviews;++i)
-			{
-				const Mat34& PMat = views[i].first;
-				const Vec2 & p = views[i].second;
-				const Vec3 xProj = PMat * Vec4(X(0), X(1), X(2), 1.0);
-				const double z = xProj(2);
-        const Vec2 x = xProj.head<2>() / z;
-				if (z<zmin) zmin = z;
-				if (z>zmax) zmax = z;
-				err += (p-x).norm();
-				weights[i] = 1.0 / z;
-			}
-		}
-		return X;
-	}
+  return X;
+}
 
 
 }  // namespace openMVG
--- src/openMVG/multiview/triangulation_nview.hpp
+++ src/openMVG/multiview/triangulation_nview.hpp
@@ -32,58 +32,58 @@
 
 namespace openMVG {
 
-  /// Compute a 3D position of a point from several images of it. In particular,
-  ///  compute the projective point X in R^4 such that x = PX.
-  /// Algorithm is the standard DLT; for derivation see appendix of Keir's thesis.
-  void TriangulateNView(
-    const Mat2X &x, // x's are 2D coordinates (x,y,1) in each image
-    const std::vector< Mat34 > &Ps, // Ps are projective cameras
-    Vec4 *X);
+/// Compute a 3D position of a point from several images of it. In particular,
+///  compute the projective point X in R^4 such that x = PX.
+/// Algorithm is the standard DLT; for derivation see appendix of Keir's thesis.
+void TriangulateNView(const Mat2X &x, // x's are 2D coordinates (x,y,1) in each image
+                      const std::vector< Mat34 > &Ps, // Ps are projective cameras
+                      Vec4 *X);
 
-  // This method uses the algebraic distance approximation.
-  // Note that this method works better when the 2D points are normalized
-  // with an isotopic normalization.
-  void TriangulateNViewAlgebraic(
-    const Mat2X &x, // x's are 2D coordinates (x,y,1) in each image
-    const std::vector< Mat34 > &Ps, // Ps are projective cameras.
-    Vec4 *X);
+// This method uses the algebraic distance approximation.
+// Note that this method works better when the 2D points are normalized
+// with an isotopic normalization.
+void TriangulateNViewAlgebraic(const Mat2X &x, // x's are 2D coordinates (x,y,1) in each image
+                               const std::vector< Mat34 > &Ps, // Ps are projective cameras.
+                               Vec4 *X);
 
-  //Iterated linear method
-  class Triangulation
-	{
-	public:
+//Iterated linear method
 
-		size_t size() const {	return views.size();}
+class Triangulation
+{
+public:
 
-		void clear()  { views.clear();}
+  size_t size() const {	return views.size();}
 
-		void add(const Mat34& projMatrix, const Vec2 & p) {
-			views.push_back( std::pair<Mat34, Vec2>(projMatrix,p) );
-		}
+  void clear()  { views.clear();}
 
-    // Return squared L2 sum of error
-		double error(const Vec3 &X) const;
+  void add(const Mat34& projMatrix, const Vec2 & p)
+  {
+    views.push_back(std::pair<Mat34, Vec2>(projMatrix, p));
+  }
 
-		Vec3 compute(int iter = 3) const;
+  // Return squared L2 sum of error
+  double error(const Vec3 &X) const;
 
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// Accessors
+  Vec3 compute(int iter = 3) const;
 
-		// These values are defined after a successful call to compute
-		double minDepth() const { return zmin; }
-		double maxDepth() const { return zmax; }
-		double error()    const { return err; }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Accessors
 
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// Data members
+  // These values are defined after a successful call to compute
+  double minDepth() const { return zmin; }
+  double maxDepth() const { return zmax; }
+  double error()    const { return err; }
 
-	protected:
-		mutable double zmin; // min depth, mutable since modified in compute(...) const;
-    mutable double zmax; // max depth, mutable since modified in compute(...) const;
-    mutable double err;  // re-projection error, mutable since modified in compute(...) const;
-		std::vector< std::pair<Mat34, Vec2> > views; // Proj matrix and associated image point
-	};
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Data members
 
-}  // namespace openMVG
+protected:
+  mutable double zmin; // min depth, mutable since modified in compute(...) const;
+  mutable double zmax; // max depth, mutable since modified in compute(...) const;
+  mutable double err; // re-projection error, mutable since modified in compute(...) const;
+  std::vector< std::pair<Mat34, Vec2> > views; // Proj matrix and associated image point
+};
+
+} // namespace openMVG
 
 #endif  // OPENMVG_MULTIVIEW_TRIANGULATION_NVIEW_H
