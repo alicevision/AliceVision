acbdd14977dbb096387f1c7fbb4510c7aa22cbf9
Simone Gasparini <simone.gasparini@gmail.com>
2017-05-16 09:57:01 +0200

    [multiview] just formatting
    tab -> spaces

--- src/openMVG/multiview/solver_resection_p4pf.hpp
+++ src/openMVG/multiview/solver_resection_p4pf.hpp
@@ -1,29 +1,29 @@
 /*
-* Copyright (c) 2011, Laurent Kneip, ETH Zurich
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of ETH Zurich nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL ETH ZURICH BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) 2011, Laurent Kneip, ETH Zurich
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ETH Zurich nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ETH ZURICH BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 // Copyright (c) 2012, 2013 Pierre MOULON.
 
@@ -38,78 +38,90 @@
 #include "openMVG/numeric/numeric.h"
 
 namespace openMVG {
-	namespace resection {
-		
-		double dabs(double a);
+namespace resection {
 
-		void GJ(double *A, int rcnt, int ccnt, double tol);
+double dabs(double a);
 
-		// prepare polynomial coefficients
-		void CalcCoefs(double const *src1, double const *src2, double const *src3, double const *src4,
-			double const *src5, double *dst1 );
+void GJ(double *A, int rcnt, int ccnt, double tol);
 
-		// [glab, glac, glad, glbc, glbd, glcd], [a1; a2], [b1; b2], [c1; c2], [d1;d2]
-		//  glXY - ||X-Y||^2 - quadratic distances between 3D points X and Y
-		//  a1 (a2) = x (resp y) measurement of the first 2D point
-		//  b1 (b2) = x (resp y) measurement of the second 2D point
-		//  c1 (c2) = x (resp y) measurement of the third 2D point
-		//  d1 (d2) = x (resp y) measurement of the fourth 2D point
-		//
-		// output *A - 10x10 action matrix
-		void compute_p4pf_poses(double *glab, double *a1, double *b1, double *c1, double *d1, double *A );
+// prepare polynomial coefficients
+void CalcCoefs(double const *src1, double const *src2, double const *src3, double const *src4,
+               double const *src5, double *dst1);
 
-		bool isNan(Eigen::MatrixXcd *A);
+// [glab, glac, glad, glbc, glbd, glcd], [a1; a2], [b1; b2], [c1; c2], [d1;d2]
+//  glXY - ||X-Y||^2 - quadratic distances between 3D points X and Y
+//  a1 (a2) = x (resp y) measurement of the first 2D point
+//  b1 (b2) = x (resp y) measurement of the second 2D point
+//  c1 (c2) = x (resp y) measurement of the third 2D point
+//  d1 (d2) = x (resp y) measurement of the fourth 2D point
+//
+// output *A - 10x10 action matrix
+void compute_p4pf_poses(double *glab, double *a1, double *b1, double *c1, double *d1, double *A);
 
-		bool validSol(Eigen::MatrixXcd *sol, Mat *vSol);
+bool isNan(Eigen::MatrixXcd *A);
 
-		void getRigidTransform(Mat *pp1, Mat *pp2, Mat *R, Vec3 *t);
+bool validSol(Eigen::MatrixXcd *sol, Mat *vSol);
 
-		// The structure M contain one output model
-		struct M {
-			double _f;
-			Mat _R;
-			Vec3 _t;
+void getRigidTransform(Mat *pp1, Mat *pp2, Mat *R, Vec3 *t);
 
-			M(Mat R, Vec3 t, double f) : _R(R), _t(t), _f(f) {}
+// The structure M contain one output model
 
-			Mat34 getP() const {
-				Mat34 P;
-				Mat K = Mat(3, 3);
-				K << _f, 0, 0,
-					0, _f, 0,
-					0, 0, 1;
-				P.block(0, 0, 3, 3) = K*_R;
-				P.block(0, 3, 3, 1) = K*_t;
-				return P;
-			}
-		};
+struct M
+{
+  double _f;
+  Mat _R;
+  Vec3 _t;
 
-		/*
-		*      Author: Martin Bujnak, adapted to openMVG by Michal Polic
-		* Description: Compute the absolute pose and focal length of a camera using three 3D-to-2D correspondences
-		*   Reference: [1] A general solution to the p4p
-		*              Bujnak, M., Kukelova, Z., and Pajdla T.
-		*              CVPR 2008
-		*
-		*       Input: featureVectors: 2x4 matrix with feature vectors with subtracted principal point (each column is a vector)
-		*              worldPoints: 3x4 matrix with corresponding 3D world points (each column is a point)
-		*
-		*      Output: solutions: M x n vector that will contain the each solution in structure M (rotation matrix M._R,
-		*						  translation vector M._t, focal length M._f). Following equation holds for each solution:
-		*						  lambda*pt2D = diag([M._f M._f 1])*[M._R M._t] * pt3D
-		*/
-		struct P4PfSolver {
-			enum { MINIMUM_SAMPLES = 4 };
-			enum { MAX_MODELS = 10 };
-			
-			// Solve the problem of camera pose.
-			static void Solve(const Mat &pt2Dx, const Mat &pt3Dx, std::vector<M> *models);
+  M(Mat R, Vec3 t, double f) : _R(R), _t(t), _f(f) { }
 
-			// Compute the residual of the projection distance(pt2D, Project(P,pt3D))
-			static double Error(const M & model, const Vec2 & pt2D, const Vec3 & pt3D);
-		};
+  Mat34 getP() const
+  {
+    Mat34 P;
+    Mat K = Mat(3, 3);
+    K << _f, 0, 0,
+            0, _f, 0,
+            0, 0, 1;
+    P.block(0, 0, 3, 3) = K*_R;
+    P.block(0, 3, 3, 1) = K*_t;
+    return P;
+  }
+};
 
-	} // namespace resection
+/*
+ *      Author: Martin Bujnak, adapted to openMVG by Michal Polic
+ * Description: Compute the absolute pose and focal length of a camera using three 3D-to-2D correspondences
+ *   Reference: [1] A general solution to the p4p
+ *              Bujnak, M., Kukelova, Z., and Pajdla T.
+ *              CVPR 2008
+ *
+ *       Input: featureVectors: 2x4 matrix with feature vectors with subtracted principal point (each column is a vector)
+ *              worldPoints: 3x4 matrix with corresponding 3D world points (each column is a point)
+ *
+ *      Output: solutions: M x n vector that will contain the each solution in structure M (rotation matrix M._R,
+ *						  translation vector M._t, focal length M._f). Following equation holds for each solution:
+ *						  lambda*pt2D = diag([M._f M._f 1])*[M._R M._t] * pt3D
+ */
+struct P4PfSolver
+{
+
+  enum
+  {
+    MINIMUM_SAMPLES = 4
+  };
+
+  enum
+  {
+    MAX_MODELS = 10
+  };
+
+  // Solve the problem of camera pose.
+  static void Solve(const Mat &pt2Dx, const Mat &pt3Dx, std::vector<M> *models);
+
+  // Compute the residual of the projection distance(pt2D, Project(P,pt3D))
+  static double Error(const M & model, const Vec2 & pt2D, const Vec3 & pt3D);
+};
+
+} // namespace resection
 } // namespace openMVG
 
 #endif // OPENMVG_MULTIVIEW_RESECTION_P4PF_H_
--- src/openMVG/multiview/solver_resection_p4pf_test.cpp
+++ src/openMVG/multiview/solver_resection_p4pf_test.cpp
@@ -36,95 +36,108 @@
 
 using namespace openMVG;
 
-
-bool isEqual(const resection::M first, const resection::M second) {
-	double eps = 1e-3;
-	return	((first._R - second._R).norm() < first._R.maxCoeff() * eps &&
-			(first._t - second._t).norm() < first._t.maxCoeff() * eps &&
-			abs(first._f - second._f) < first._f * eps);
+bool isEqual(const resection::M first, const resection::M second)
+{
+  double eps = 1e-3;
+  return ((first._R - second._R).norm() < first._R.maxCoeff() * eps &&
+          (first._t - second._t).norm() < first._t.maxCoeff() * eps &&
+          abs(first._f - second._f) < first._f * eps);
 }
 
+TEST(Resection_P4Pf, AssignmentWithOneResult)
+{
+  // INPUT DATA
+  Mat pt2D_1 = Mat(2, 4);
+  pt2D_1 << -493.1500, 1051.9100, 176.9500, -1621.9800,
+          -878.4550, -984.7530, -381.4300, -543.3450;
+  Mat pt3D_1 = Mat(3, 4);
+  pt3D_1 << 2.7518, 2.2375, 1.1940, 2.5778,
+          0.1336, -0.3709, 0.2048, -0.9147,
+          -0.5491, -2.0511, 1.1480, -1.6151;
 
-TEST(Resection_P4Pf, AssignmentWithOneResult) {
-	// INPUT DATA
-	Mat pt2D_1 = Mat(2, 4);
-	pt2D_1 <<	-493.1500, 1051.9100, 176.9500, -1621.9800,
-				-878.4550, -984.7530, -381.4300, -543.3450;
-	Mat pt3D_1 = Mat(3, 4);
-	pt3D_1 <<	2.7518,  2.2375, 1.1940, 2.5778,
-				0.1336, -0.3709, 0.2048, -0.9147,
-				-0.5491, -2.0511, 1.1480, -1.6151;
+  // EXPECTED RESULT
+  Mat R_1 = Mat(3, 3);
+  R_1 << -0.97189, 0.05884, -0.22797, -0.02068, -0.98586, -0.16631, -0.23454, -0.15692, 0.95936;
+  Vec3 t_1;
+  t_1 << 2.00322, -1.27420, 2.92685;
+  resection::M sol_1(R_1, t_1, 887.17549);
 
-	// EXPECTED RESULT
-	Mat R_1 = Mat(3, 3); R_1 << -0.97189, 0.05884, -0.22797, -0.02068, -0.98586, -0.16631, -0.23454, -0.15692, 0.95936;
-	Vec3 t_1; t_1 << 2.00322, -1.27420, 2.92685;
-	resection::M sol_1(R_1, t_1, 887.17549);
+  // PROCESS THE RESECTION P4Pf
+  std::vector<resection::M> models_1;
+  resection::P4PfSolver::Solve(pt2D_1, pt3D_1, &models_1);
 
-	// PROCESS THE RESECTION P4Pf
-	std::vector<resection::M> models_1;
-	resection::P4PfSolver::Solve(pt2D_1, pt3D_1, &models_1);
-
-	bool pass = true;
-	if (!(models_1.size() == 1 && isEqual(models_1.at(0),sol_1)))
-		pass = false;
-	EXPECT_TRUE(pass);
+  bool pass = true;
+  if(!(models_1.size() == 1 && isEqual(models_1.at(0), sol_1)))
+    pass = false;
+  EXPECT_TRUE(pass);
 }
 
-TEST(Resection_P4Pf, AssignmentWithMoreResults) {
-	// DATA
-	Mat pt2D_2 = Mat(2, 4);
-	pt2D_2 << 774.88000, -772.31000, -1661.63300, -1836.57300,
-		-534.74500, -554.09400, -585.53300, -430.03000;
-	Mat pt3D_2 = Mat(3, 4);
-	pt3D_2 << 2.01852, 1.00709, 0.74051, 0.61962,
-		0.02133, 0.30770, 0.16656, 0.11249,
-		-1.68077, 0.81502, 1.21056, 1.22624;
-	
-	// RESULTS
-	Mat R_21 = Mat(3, 3);  R_21 << 0.74908, 0.58601, -0.30898, 0.65890, -0.61061, 0.43933, 0.06879, -0.53268, -0.84352;
-	Mat R_22 = Mat(3, 3);  R_22 << 0.06352, -0.56461, -0.82291, -0.97260, 0.14975, -0.17781, 0.22362, 0.81166, -0.53963;
-	Mat R_23 = Mat(3, 3);  R_23 << 0.02362, -0.60298, -0.79741, -0.96400, 0.19758, -0.17796, 0.26486, 0.77290, -0.57661;
-	Vec3 t_21; t_21 << -1.17794, -1.17674, 3.57853;
-	Vec3 t_22; t_22 << 0.08257, 0.57753, 1.04335;
-	Vec3 t_23; t_23 << 0.16029, 0.58720, 1.07571;
-	resection::M sol_21(R_21, t_21, 4571.95746);
-	resection::M sol_22(R_22, t_22, 1193.30606);
-	resection::M sol_23(R_23, t_23, 1315.17564);
+TEST(Resection_P4Pf, AssignmentWithMoreResults)
+{
+  // DATA
+  Mat pt2D_2 = Mat(2, 4);
+  pt2D_2 << 774.88000, -772.31000, -1661.63300, -1836.57300,
+          -534.74500, -554.09400, -585.53300, -430.03000;
+  Mat pt3D_2 = Mat(3, 4);
+  pt3D_2 << 2.01852, 1.00709, 0.74051, 0.61962,
+          0.02133, 0.30770, 0.16656, 0.11249,
+          -1.68077, 0.81502, 1.21056, 1.22624;
 
-	// PROCESS
-	std::vector<resection::M> models_2;
-	resection::P4PfSolver::Solve(pt2D_2, pt3D_2, &models_2);
+  // RESULTS
+  Mat R_21 = Mat(3, 3);
+  R_21 << 0.74908, 0.58601, -0.30898, 0.65890, -0.61061, 0.43933, 0.06879, -0.53268, -0.84352;
+  Mat R_22 = Mat(3, 3);
+  R_22 << 0.06352, -0.56461, -0.82291, -0.97260, 0.14975, -0.17781, 0.22362, 0.81166, -0.53963;
+  Mat R_23 = Mat(3, 3);
+  R_23 << 0.02362, -0.60298, -0.79741, -0.96400, 0.19758, -0.17796, 0.26486, 0.77290, -0.57661;
+  Vec3 t_21;
+  t_21 << -1.17794, -1.17674, 3.57853;
+  Vec3 t_22;
+  t_22 << 0.08257, 0.57753, 1.04335;
+  Vec3 t_23;
+  t_23 << 0.16029, 0.58720, 1.07571;
+  resection::M sol_21(R_21, t_21, 4571.95746);
+  resection::M sol_22(R_22, t_22, 1193.30606);
+  resection::M sol_23(R_23, t_23, 1315.17564);
 
-	bool pass = true;
-	if (!(models_2.size() == 3
-		&& isEqual(models_2.at(0),sol_21)
-		&& isEqual(models_2.at(1),sol_22)
-		&& isEqual(models_2.at(2),sol_23)))
-		pass = false;
-	EXPECT_TRUE(pass);
+  // PROCESS
+  std::vector<resection::M> models_2;
+  resection::P4PfSolver::Solve(pt2D_2, pt3D_2, &models_2);
+
+  bool pass = true;
+  if(!(models_2.size() == 3
+          && isEqual(models_2.at(0), sol_21)
+          && isEqual(models_2.at(1), sol_22)
+          && isEqual(models_2.at(2), sol_23)))
+    pass = false;
+  EXPECT_TRUE(pass);
 }
 
+TEST(Resection_P4Pf, AssignmentWithNoResults)
+{
+  // DATA
+  Mat pt2D_3 = Mat(2, 4);
+  pt2D_3 << 774.88000, -570.41000, -1881.86960, 1529.54000,
+          -534.74500, -834.63100, -167.32000, -1203.28000;
+  Mat pt3D_3 = Mat(3, 4);
+  pt3D_3 << 2.01852, 1.28149, 0.55264, 2.29633,
+          0.02133, 0.26101, 0.14578, -1.80998,
+          -1.68077, 0.70813, 1.22217, -1.76850;
 
-TEST(Resection_P4Pf, AssignmentWithNoResults) {
-	// DATA
-	Mat pt2D_3 = Mat(2, 4);
-	pt2D_3 <<	774.88000, -570.41000, -1881.86960, 1529.54000,
-				-534.74500, -834.63100, -167.32000, -1203.28000;
-	Mat pt3D_3 = Mat(3, 4);
-	pt3D_3 <<	2.01852, 1.28149, 0.55264, 2.29633,
-				0.02133, 0.26101, 0.14578, -1.80998,
-				-1.68077, 0.70813, 1.22217, -1.76850;
+  // PROCESS
+  std::vector<resection::M> models_3;
+  resection::P4PfSolver::Solve(pt2D_3, pt3D_3, &models_3);
 
-	// PROCESS
-	std::vector<resection::M> models_3;
-	resection::P4PfSolver::Solve(pt2D_3, pt3D_3, &models_3);
-	
-	bool pass = true;
-	if (models_3.size() != 0)
-		pass = false;
-	EXPECT_TRUE(pass);
+  bool pass = true;
+  if(models_3.size() != 0)
+    pass = false;
+  EXPECT_TRUE(pass);
 }
 
 /* ************************************************************************* */
-int main() { TestResult tr; return TestRegistry::runAllTests(tr); }
+int main()
+{
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
 /* ************************************************************************* */
