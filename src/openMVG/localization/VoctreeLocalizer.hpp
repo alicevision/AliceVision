c6077ae96468d7196893324d1cd14e0c59e1be7a
Gregoire DE LILLO <gregoire.delillo@me.com>
2017-05-19 19:17:05 +0200

    [localization] minor variables renaming

--- src/openMVG/localization/VoctreeLocalizer.hpp
+++ src/openMVG/localization/VoctreeLocalizer.hpp
@@ -28,7 +28,7 @@ namespace localization {
 
 struct FrameData
 {
-  FrameData(const LocalizationResult &locResult, const features::MapRegionsPerDesc& regions)
+  FrameData(const LocalizationResult &locResult, const features::MapRegionsPerDesc& regionsPerDesc)
     : _locResult(locResult)
   {
     // now we need to filter out and keep only the regions with 3D data associated
@@ -36,24 +36,26 @@ struct FrameData
     const auto &inliers = _locResult.getInliers();
 
     // Regions for each describer type
-    for(const auto& regionsIt : regions)
+    for(const auto& regionsPerDescIt : regionsPerDesc)
     {
       // feature in image are <featureID, point3Did> associations
       std::vector<features::FeatureInImage> featuresInImage;
       featuresInImage.reserve(inliers.size());
 
+      assert(inliers.size() <= associationIDs.size());
       for(const auto &idx : inliers)
       {
         assert(idx < associationIDs.size());
         const auto &association = associationIDs[idx];
-        if(association.descType != regionsIt.first)
+        assert(association.descType != features::EImageDescriberType::UNINITIALIZED);
+        if(association.descType != regionsPerDescIt.first)
           continue;
 
-        // add it to featuresInImage
+       // add it to featuresInImage
         featuresInImage.emplace_back(association.featId, association.landmarkId);
       }
 
-      _regions[regionsIt.first] = createFilteredRegions(*regionsIt.second, featuresInImage, _regionsWith3D[regionsIt.first]);
+      _regions[regionsPerDescIt.first] = createFilteredRegions(*regionsPerDescIt.second, featuresInImage, _regionsWith3D[regionsPerDescIt.first]);
     }
   }
   
