6de1d8b121a87cd0ea521c20e5d24a02479f829c
Simone Gasparini <simone.gasparini@gmail.com>
2017-06-21 18:35:07 +0200

    [robust_estimation] better test coverage

--- src/openMVG/robust_estimation/rand_sampling_test.cpp
+++ src/openMVG/robust_estimation/rand_sampling_test.cpp
@@ -8,6 +8,7 @@
 #include "openMVG/robust_estimation/rand_sampling.hpp"
 #include "testing/testing.h"
 #include <set>
+#include <vector>
 
 using namespace openMVG;
 using namespace openMVG::robust;
@@ -15,19 +16,21 @@ using namespace openMVG::robust;
 // Assert that each time exactly N random number are picked (no repetition)
 TEST(UniformSampleTest, NoRepetions) {
 
-  std::vector<std::size_t> samples;
-  for (std::size_t total = 1; total < 500; total *= 2)
+  for(std::size_t upperBound = 1; upperBound < 513; upperBound *= 2)
   { 
     //Size of the data set
-    for (std::size_t num_samples = 1; num_samples <= total; num_samples *= 2)
-    { //Size of the consensus set
-      UniformSample(num_samples, total, &samples);
+    for(std::size_t numSamples = 1; numSamples <= upperBound; numSamples *= 2)
+    { 
+      //Size of the consensus set
+      std::vector<std::size_t> samples;
+      std::cout << "Upper " << upperBound << " Lower " << 0 << " numSamples " << numSamples << "\n";
+      UniformSample(numSamples, upperBound, &samples);
       std::set<std::size_t> myset;
-      for (std::size_t i = 0; i < num_samples; ++i) 
+      for(const auto& s : samples) 
       {
-        myset.insert(samples[i]);
-        CHECK(samples[i] >= 0);
-        CHECK(samples[i] < total);
+        myset.insert(s);
+        CHECK(s >= 0);
+        CHECK(s < upperBound);
       }
       CHECK_EQUAL(num_samples, myset.size());
     }
@@ -36,46 +39,47 @@ TEST(UniformSampleTest, NoRepetions) {
 
 TEST(UniformSampleTest, NoRepetionsBeginEnd) {
 
-  std::vector<std::size_t> samples;
-  for (std::size_t end = 1; end < 500; end *= 2)
+  for(std::size_t upperBound = 1; upperBound < 513; upperBound *= 2)
   { 
     //Size of the data set
-    for (std::size_t num_samples = 1; num_samples <= end; num_samples *= 2)
+    for(std::size_t numSamples = 1; numSamples <= upperBound; numSamples *= 2)
     { 
       //Size of the consensus set
-      assert((end-num_samples) >= 0);
-      const std::size_t begin = end-num_samples;
-      UniformSample(begin, end, num_samples, &samples);
+      assert((upperBound-numSamples) >= 0);
+      const std::size_t begin = upperBound-numSamples;
+      std::cout << "Upper " << upperBound << " Lower " << begin << " numSamples " << numSamples << "\n";
+      std::vector<std::size_t> samples;
+      UniformSample(begin, upperBound, numSamples, &samples);
       std::set<std::size_t> myset;
-      for (std::size_t i = 0; i < num_samples; ++i) 
+      for(const auto& s : samples) 
       {
-        myset.insert(samples[i]);
-        CHECK(samples[i] >= begin);
-        CHECK(samples[i] < end);
+        myset.insert(s);
+        CHECK(s >= begin);
+        CHECK(s < upperBound);
       }
-      CHECK_EQUAL(num_samples, myset.size());
+      CHECK_EQUAL(numSamples, myset.size());
     }
   }
 }
 
 TEST(UniformSampleTest, randSample) {
   
-  for (std::size_t upperBound = 1; upperBound < 501; upperBound *= 2)
+  for(std::size_t upperBound = 1; upperBound < 513; upperBound *= 2)
   { 
-    for (std::size_t numSamples = 1; numSamples <= upperBound; numSamples *= 2)
+    for(std::size_t numSamples = 1; numSamples <= upperBound; numSamples *= 2)
     { 
       assert((upperBound-numSamples) >= 0);
       const std::size_t lowerBound = upperBound-numSamples;
       const auto samples = randSample<std::size_t>(lowerBound, upperBound, numSamples);
       
       std::set<std::size_t> myset;
-//      std::cout << "Upper " << upperBound << " Lower " << lowerBound << " numSamples " << numSamples << "\n";
-      for (std::size_t i = 0; i < numSamples; ++i) 
+      std::cout << "Upper " << upperBound << " Lower " << lowerBound << " numSamples " << numSamples << "\n";
+      for(const auto& s : samples) 
       {
 //        std::cout << samples[i] << " ";
-        myset.insert(samples[i]);
-        CHECK(samples[i] >= lowerBound);
-        CHECK(samples[i] < upperBound);
+        myset.insert(s);
+        CHECK(s >= lowerBound);
+        CHECK(s < upperBound);
       }
 //      std::cout  << "\n";
       // this verifies no repetitions
