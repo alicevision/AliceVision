ae7ee71490114e4c3b03501307b53dab38d180ba
Carsten Griwodz <griff@simula.no>
2017-08-28 10:21:33 +0200

    mapping equirect to 6 cube face file directly

--- src/software/SfM/main_convertVid2Img.cpp
+++ src/software/SfM/main_convertVid2Img.cpp
@@ -1,11 +1,8 @@
 #include "openMVG/image/image.hpp"
 #include "openMVG/image/pixel_types.hpp"
 
-// #include "openMVG/system/timer.hpp"
-
 #include "third_party/cmdLine/cmdLine.h"
 #include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
-// #include "third_party/progress/progress.hpp"
 
 extern "C" {
 #include <libavformat/avformat.h>
@@ -17,6 +14,13 @@ extern "C" {
 #include <cstdlib>
 #include <fstream>
 #include <string>
+
+// #include <assert.h>
+// #include <math.h>
+// #include <stdlib.h>
+// #include <iostream>
+#include <iomanip>
+// #include <algorithm>
 
 using namespace std;
 // using namespace openMVG;
@@ -54,7 +58,7 @@ static const char* cam_cube_conf_names[5] =
 };
 
 // R-right L-left U-up D-down F-front B-back
-static char cam_cube_order[7] = "RLUDFB";
+static char cam_cube_order[7] = "rludfb";
 
 static cam_mode_360_t stringToMode( const string& mode )
 {
@@ -76,6 +80,172 @@ static const char* cubeConfToString( cam_cube_conf_t mode )
 }
 
 static bool verbose = false;
+
+class GetCubemapFromEquirect
+{
+public:
+    /* Create an object with the edge length of each side of the cubemap that is the target
+     * of the mapping, and the width and the height of the equirectangular plane that is the
+     * source of the mapping.
+     */
+    GetCubemapFromEquirect( int cube_edge, int rect_x, int rect_y )
+        : _cube_x( cube_edge )
+        , _cube_y( cube_edge )
+        , _rect_x( rect_x )
+        , _rect_y( rect_y )
+    { }
+
+    /* Alternative constructor supporting non-square sides of the cubemap. Probably
+     * unexpected effects due to non-square top and bottom "squares".
+     */
+    GetCubemapFromEquirect( int cube_x, int cube_y, int rect_x, int rect_y )
+        : _cube_x( cube_x )
+        , _cube_y( cube_y )
+        , _rect_x( rect_x )
+        , _rect_y( rect_y )
+    { }
+
+    /* Map coordinates for one face of the cubemap into a floating point address on
+     * the equirectangular plane.
+     * The face must be specified by one of the characters f, r, b, l, u or d
+     * for "front, right, back, left, up or down.
+     * Note that the camera is supposed to be at the center of the CUBE, with face f
+     * in front of it and face b behind it. The center of the front face is also the
+     * center of the equirectangular plane.
+     */
+    bool mapCoord( double& rect_x, double& rect_y, double cube_x, double cube_y, const char cube_face );
+
+    /* For the floating point coordinates in the equirectangular plane, compute the
+     * integer index of the 4 nearest pixels and their weights.
+     */
+    void weightedPix( double wgt[4], int x[4], int y[4], double rect_x, double rect_y );
+
+private:
+    int _cube_x;
+    int _cube_y;
+    int _rect_x;
+    int _rect_y;
+
+    bool projectOntoUnitSphere( double& horiz, double& vert, double x, double y, const char face );
+
+    void projectOntoUnitCyclinder( double& x, double& y, double horiz, double vert );
+};
+
+bool GetCubemapFromEquirect::mapCoord( double& rect_x, double& rect_y, double cube_x, double cube_y, const char cube_face )
+{
+    double out[2];
+    double cylout[2];
+
+    cube_x /= _cube_x;
+    cube_y /= _cube_y;
+
+    // cube_x = -1.0 * ( cube_x - 0.5 );
+    // cube_y = -1.0 * ( cube_y - 0.5 );
+    cube_x = ( cube_x - 0.5 );
+    cube_y = ( cube_y - 0.5 );
+
+    bool ret = projectOntoUnitSphere( out[0], out[1], cube_x, cube_y, cube_face );
+    if( ! ret ) {
+        cerr << "Could not compute projectOntoUnitSphere" << endl;
+        return false;
+    }
+
+    projectOntoUnitCyclinder( rect_x, rect_y, out[0], out[1] );
+
+    rect_x *= _rect_x;
+    rect_y *= _rect_y;
+
+    return true;
+}
+
+bool GetCubemapFromEquirect::projectOntoUnitSphere( double& horiz, double& vert, double x, double y, const char face )
+{
+    if( x > 0.5 || x < -0.5 || y > 0.5 || y < -0.5 ) return false;
+
+    if( face == 'r' )
+    {
+      /* Right face. (0,0) -> 90 degrees rotation, 0 degrees elevation */
+      double l  = sqrt( x*x + 0.5*0.5 );
+      horiz = M_PI/2.0 + atan( x / 0.5 );
+      vert  = atan( y / l );
+    }
+    else if( face == 'l' )
+    {
+      /* Left face. (0,0) -> 270 degrees rotation, 0 degrees elevation */
+      double l  = sqrt( x*x + 0.5*0.5 );
+      horiz = M_PI/2.0*3.0 + atan( x / 0.5 );
+      vert  = atan( y / l );
+    }
+    else if( face == 'd' )
+    {
+      /* Up face. (0,0) -> 0 degrees rotation, 90 degrees elevation */
+      double l  = sqrt( x*x + y*y );
+      horiz = M_PI - atan2( x, y );
+      vert  = M_PI/2.0 - atan( l / 0.5 );
+    }
+    else if( face == 'u' )
+    {
+      /* Down face. (0,0) -> 0 degrees rotation, 90 degrees elevation */
+      double l  = sqrt( x*x + y*y );
+      horiz = atan2( x, y );
+      vert  = atan( l / 0.5 ) - M_PI/2.0;
+    }
+    else if( face == 'f' )
+    {
+      /* Front face. (0,0) -> 0 degrees rotation, 0 degrees elevation */
+      double l  = sqrt( x*x + 0.5*0.5 );
+      horiz = atan( x / 0.5 );
+      vert  = atan( y / l );
+    }
+    else if( face == 'b' )
+    {
+      /* Back face. (0,0) -> 180 degrees rotation, 0 degrees elevation */
+      double l  = sqrt( x*x + 0.5*0.5 );
+      horiz = M_PI - atan( -x / 0.5 );
+      vert  = atan( y / l );
+    }
+    else
+    {
+      cerr << "Programming Error: side of cube must be given as f,r,b,l,u,d" << endl;
+      return false;
+    }
+    while( horiz < M_PI ) horiz += ( 2.0*M_PI );
+    while( horiz > M_PI ) horiz -= ( 2.0*M_PI );
+    while( vert  < M_PI ) vert  += ( 2.0*M_PI );
+    while( vert  > M_PI ) vert  -= ( 2.0*M_PI );
+    return true;
+}
+
+void GetCubemapFromEquirect::projectOntoUnitCyclinder( double& x, double& y, double horiz, double vert )
+{
+    x = horiz / (2.0 * M_PI);
+    x += 0.5;
+    y = sin( vert );
+    y = ( y + 1.0 ) / 2.0;
+}
+
+void GetCubemapFromEquirect::weightedPix( double wgt[4], int x[4], int y[4], double rect_x, double rect_y )
+{
+    x[0] = (int)trunc(rect_x);
+    x[1] = x[0] + 1;
+
+    y[0] = (int)trunc(rect_y);
+    y[2] = y[0] + 1;
+
+    double w_wgt = x[1] - rect_x;
+    double h_wgt = y[2] - rect_y;
+
+    x[2] = x[0] = x[0] % _rect_x;
+    x[3] = x[1] = x[1] % _rect_x;
+    y[1] = y[0] = y[0] % _rect_y;
+    y[3] = y[2] = y[2] % _rect_y;
+
+    wgt[0] = w_wgt * h_wgt;
+    wgt[1] = ( 1.0 - w_wgt ) * h_wgt;
+    wgt[2] = w_wgt * ( 1.0 - h_wgt );
+    wgt[3] = ( 1.0 - w_wgt ) * ( 1.0 - h_wgt );
+}
+
 
 class ConvertVideo
 {
@@ -351,6 +521,29 @@ private:
                     printExif( filebase.str(), filename.str() );
                 }
             }
+            else if( _mode_360 == cam_mode_360_t::EQUIRECT && _split_cube )
+            {
+                for( int i=0; i<6; i++ ) {
+                    ostringstream filebase;
+
+                    filebase << "file-" << cam_cube_order[i] << "-" << numbering;
+
+                    int outsz = min( _width, _height ) / 2;
+                    openMVG::image::Image<openMVG::image::RGBColor> out( outsz, outsz, false );
+                
+                    const int linesize = outframe->linesize[0];
+                    uint8_t*  data     = outframe->data[0];
+
+                    extractFaceFromEquirect( cam_cube_order[i], linesize, data, out, outsz );
+
+                    ostringstream filename;
+
+                    filename << filebase.str() << (_img_format==img_format_t::PNG ? ".png" : ".jpg" );
+                    const string fullname = _outputDirectory + "/" + filename.str();
+                    int err = openMVG::image::WriteImage( fullname.c_str(), out );
+                    printExif( filebase.str(), filename.str() );
+                }
+            }
             else
             {
                 ostringstream filename;
@@ -399,6 +592,45 @@ private:
     av_frame_free( &frame );
 
     return true;
+  }
+
+  void extractFaceFromEquirect( const char face,
+                                const int  linesize,
+                                const uint8_t* in,
+                                openMVG::image::Image<openMVG::image::RGBColor>& out,
+                                const int   outsz )
+  {
+    GetCubemapFromEquirect m( outsz, _width, _height );
+
+    for( int h=0; h<outsz; h++ ) {
+      for( int w=0; w<outsz; w+=1 ) {
+        double rect_x, rect_y;
+
+        bool ret = m.mapCoord( rect_x, rect_y, w, h, face );
+        if( ! ret ) {
+          cerr << "Failed to map (" << w << "," << h << ")" << endl;
+          continue;
+        }
+
+        double wgt[4];
+        int    x[4];
+        int    y[4];
+        int    rgb[3];
+
+        m.weightedPix( wgt, x, y, rect_x, rect_y );
+
+        for( int i=0; i<3; i++ ) {
+            rgb[i] = in[y[0]*linesize+x[0]*3+i] * wgt[0]
+                   + in[y[1]*linesize+x[1]*3+i] * wgt[1]
+                   + in[y[2]*linesize+x[2]*3+i] * wgt[2]
+                   + in[y[3]*linesize+x[3]*3+i] * wgt[3];
+        }
+
+        openMVG::image::RGBColor wrt( rgb[0], rgb[1], rgb[2] );
+
+        out( h, w ) = wrt;
+      }
+    }
   }
 
   void setCubeFaceSize( int& face_w, int& face_h )
