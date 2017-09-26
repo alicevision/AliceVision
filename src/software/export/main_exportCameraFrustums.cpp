76a806ebb86c31e8dbc0ae667f904d55fcbd8def
Gregoire De Lillo <gregoire.delillo@me.com>
2017-09-26 17:29:59 +0200

    [software] exportCameraFrustums use boost program options

--- src/software/export/main_exportCameraFrustums.cpp
+++ src/software/export/main_exportCameraFrustums.cpp
@@ -4,58 +4,90 @@
 #include "aliceVision/sfm/sfm.hpp"
 #include "aliceVision/system/Timer.hpp"
 
-#include "dependencies/cmdLine/cmdLine.h"
 #include "dependencies/stlplus3/filesystemSimplified/file_system.hpp"
+
+#include <boost/program_options.hpp>
 
 using namespace aliceVision;
 using namespace aliceVision::sfm;
+using namespace std;
+namespace po = boost::program_options;
 
 /// Export camera frustrums as a triangle PLY file
 int main(int argc, char **argv)
 {
-  using namespace std;
-  std::cout << "Export camera frustums" << std::endl;
+  // command-line parameters
 
-  CmdLine cmd;
+  std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
+  std::string sfmDataFilename;
+  std::string plyOutFilename;
 
-  std::string sSfMData_Filename;
-  std::string sOutFile = "";
+  po::options_description allParams("AliceVision exportCameraFrustums");
 
-  cmd.add( make_option('i', sSfMData_Filename, "input_file") );
-  cmd.add( make_option('o', sOutFile, "output_file") );
+  po::options_description requiredParams("Required parameters");
+  requiredParams.add_options()
+    ("input,i", po::value<std::string>(&sfmDataFilename)->required(),
+      "SfMData file.")
+    ("output,o", po::value<std::string>(&plyOutFilename)->required(),
+      "PLY file to store the camera frustums as triangle meshes.");
 
-  try {
-    if (argc == 1) throw std::string("Invalid command line parameter.");
-    cmd.process(argc, argv);
-  } catch(const std::string& s) {
-    std::cerr << "Usage: " << argv[0] << '\n'
-    << "[-i|--input_file] path to a SfMData scene\n"
-    << "[-o|--output_file] PLY file to store the camera frustums as triangle meshes.\n"
-    << std::endl;
+  po::options_description logParams("Log parameters");
+  logParams.add_options()
+    ("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
+      "verbosity level (fatal,  error, warning, info, debug, trace).");
 
-    std::cerr << s << std::endl;
-    return EXIT_FAILURE;
-  }
+  allParams.add(requiredParams).add(logParams);
 
-  // Load input SfMData scene
-  SfMData sfm_data;
-  if (!Load(sfm_data, sSfMData_Filename, ESfMData(VIEWS|INTRINSICS|EXTRINSICS))) {
-    std::cerr << std::endl
-      << "The input SfMData file \""<< sSfMData_Filename << "\" cannot be read." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  // Assert that we can create the output directory/file
-  if (!stlplus::folder_exists( stlplus::folder_part(sOutFile) ))
-    if(!stlplus::folder_create( stlplus::folder_part(sOutFile) ))
-      return EXIT_FAILURE;
-
-  // If sfm_data have not structure, cameras are displayed as tiny normalized cones
-  const FrustumFilter frustum_filter(sfm_data);
-  if (!sOutFile.empty())
+  po::variables_map vm;
+  try
   {
-    if (frustum_filter.export_Ply(sOutFile))
+    po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+    if(vm.count("help") || (argc == 1))
+    {
+      ALICEVISION_COUT(allParams);
       return EXIT_SUCCESS;
+    }
+    po::notify(vm);
+  }
+  catch(boost::program_options::required_option& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
+    return EXIT_FAILURE;
+  }
+  catch(boost::program_options::error& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
+    return EXIT_FAILURE;
+  }
+
+  // set verbose level
+  system::Logger::get()->setLogLevel(verboseLevel);
+
+  // export
+  {
+    // Load input SfMData scene
+    SfMData sfmData;
+    if (!Load(sfmData, sfmDataFilename, ESfMData(VIEWS|INTRINSICS|EXTRINSICS))) {
+      std::cerr << std::endl
+        << "The input SfMData file \""<< sfmDataFilename << "\" cannot be read." << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    // Assert that we can create the output directory/file
+    if (!stlplus::folder_exists( stlplus::folder_part(plyOutFilename) ))
+      if(!stlplus::folder_create( stlplus::folder_part(plyOutFilename) ))
+        return EXIT_FAILURE;
+
+    // If sfm_data have not structure, cameras are displayed as tiny normalized cones
+    const FrustumFilter frustum_filter(sfmData);
+    if (!plyOutFilename.empty())
+    {
+      if (frustum_filter.export_Ply(plyOutFilename))
+        return EXIT_SUCCESS;
+    }
   }
   return EXIT_FAILURE;
 }
