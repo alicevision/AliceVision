209b7902c3d670fed0779e845cc68259b2bd5f16
Gregoire De Lillo <gregoire.delillo@me.com>
2017-09-26 17:32:00 +0200

    [software] exportMeshlab use boost program options

--- src/software/export/main_exportMeshlab.cpp
+++ src/software/export/main_exportMeshlab.cpp
@@ -5,8 +5,9 @@
 #include "aliceVision/image/image.hpp"
 #include "aliceVision/numeric/numeric.hpp"
 
-#include "dependencies/cmdLine/cmdLine.h"
 #include "dependencies/stlplus3/filesystemSimplified/file_system.hpp"
+
+#include <boost/program_options.hpp>
 
 #include <fstream>
 
@@ -15,58 +16,82 @@ using namespace aliceVision::camera;
 using namespace aliceVision::geometry;
 using namespace aliceVision::image;
 using namespace aliceVision::sfm;
+namespace po = boost::program_options;
 
 int main(int argc, char **argv)
 {
-  CmdLine cmd;
+  // command-line parameters
 
-  std::string sSfMData_Filename;
-  std::string sPly = "";
-  std::string sOutDir = "";
+  std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
+  std::string sfmDataFilename;
+  std::string plyPath;
+  std::string outDirectory;
 
-  cmd.add( make_option('i', sSfMData_Filename, "sfmdata") );
-  cmd.add( make_option('p', sPly, "ply") );
-  cmd.add( make_option('o', sOutDir, "outdir") );
+  po::options_description allParams("AliceVision exportMeshlab");
 
-  try {
-      if (argc == 1) throw std::string("Invalid command line parameter.");
-      cmd.process(argc, argv);
-  } catch(const std::string& s) {
-      std::cerr << "Usage: " << argv[0] << '\n'
-      << "[-i|--sfmdata] filename, the SfMData file to convert\n"
-      << "[-p|--ply] path\n"
-      << "[-o|--outdir] path\n"
-      << std::endl;
+  po::options_description requiredParams("Required parameters");
+  requiredParams.add_options()
+    ("input,i", po::value<std::string>(&sfmDataFilename)->required(),
+      "SfMData file.")
+    ("ply", po::value<std::string>(&plyPath)->required(),
+      "Ply.")
+    ("output,o", po::value<std::string>(&outDirectory)->required(),
+      "Output directory.");
 
-      std::cerr << s << std::endl;
-      return EXIT_FAILURE;
+  po::options_description logParams("Log parameters");
+  logParams.add_options()
+    ("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
+      "verbosity level (fatal,  error, warning, info, debug, trace).");
+
+  allParams.add(requiredParams).add(logParams);
+
+  po::variables_map vm;
+  try
+  {
+    po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+    if(vm.count("help") || (argc == 1))
+    {
+      ALICEVISION_COUT(allParams);
+      return EXIT_SUCCESS;
+    }
+    po::notify(vm);
   }
-
-  std::cout << " You called : " <<std::endl
-            << argv[0] << std::endl
-            << "--sfmdata " << sSfMData_Filename << std::endl
-            << "--ply " << sPly << std::endl
-            << "--outdir " << sOutDir << std::endl;
-
-  // Create output dir
-  if (!stlplus::folder_exists(sOutDir))
-    stlplus::folder_create( sOutDir );
-
-  // Read the SfM scene
-  SfMData sfm_data;
-  if (!Load(sfm_data, sSfMData_Filename, ESfMData(VIEWS|INTRINSICS|EXTRINSICS))) {
-    std::cerr << std::endl
-      << "The input SfMData file \""<< sSfMData_Filename << "\" cannot be read." << std::endl;
+  catch(boost::program_options::required_option& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
+    return EXIT_FAILURE;
+  }
+  catch(boost::program_options::error& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
     return EXIT_FAILURE;
   }
 
-  std::ofstream outfile( stlplus::create_filespec( sOutDir, "sceneMeshlab", "mlp" ).c_str() );
+  // set verbose level
+  system::Logger::get()->setLogLevel(verboseLevel);
+
+  // Create output dir
+  if (!stlplus::folder_exists(outDirectory))
+    stlplus::folder_create( outDirectory );
+
+  // Read the SfM scene
+  SfMData sfm_data;
+  if (!Load(sfm_data, sfmDataFilename, ESfMData(VIEWS|INTRINSICS|EXTRINSICS))) {
+    std::cerr << std::endl
+      << "The input SfMData file \""<< sfmDataFilename << "\" cannot be read." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::ofstream outfile( stlplus::create_filespec( outDirectory, "sceneMeshlab", "mlp" ).c_str() );
 
   // Init mlp file
   outfile << "<!DOCTYPE MeshLabDocument>" << outfile.widen('\n')
     << "<MeshLabProject>" << outfile.widen('\n')
     << " <MeshGroup>" << outfile.widen('\n')
-    << "  <MLMesh label=\"" << sPly << "\" filename=\"" << sPly << "\">" << outfile.widen('\n')
+    << "  <MLMesh label=\"" << plyPath << "\" filename=\"" << plyPath << "\">" << outfile.widen('\n')
     << "   <MLMatrix44>" << outfile.widen('\n')
     << "1 0 0 0 " << outfile.widen('\n')
     << "0 1 0 0 " << outfile.widen('\n')
