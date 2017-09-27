4682be611a6d664eea2e875b97ce273fb8417daa
Gregoire De Lillo <gregoire.delillo@me.com>
2017-09-27 10:49:27 +0200

    [software] frustumFiltering use boost program options

--- src/software/utils/main_frustumFiltering.cpp
+++ src/software/utils/main_frustumFiltering.cpp
@@ -5,13 +5,16 @@
 #include "aliceVision/system/Timer.hpp"
 #include "aliceVision/matchingImageCollection/pairBuilder.hpp"
 
-#include "dependencies/cmdLine/cmdLine.h"
 #include "dependencies/stlplus3/filesystemSimplified/file_system.hpp"
+
+#include <boost/program_options.hpp>
 
 #include <cstdlib>
 
 using namespace aliceVision;
 using namespace aliceVision::sfm;
+using namespace std;
+namespace po = boost::program_options;
 
 /// Build a list of pair that share visibility content from the SfMData structure
 PairSet BuildPairsFromStructureObservations(const SfMData & sfm_data)
@@ -52,65 +55,90 @@ PairSet BuildPairsFromFrustumsIntersections(
 
 int main(int argc, char **argv)
 {
-  using namespace std;
-  std::cout << std::endl
-    << "-----------------------------------------------------------\n"
-    << "Frustum filtering:\n"
-    << "-----------------------------------------------------------\n"
-    << "Compute camera cones that share some putative visual content.\n"
-    << "------------------------------------------------------------"
-    << std::endl;
+  // command-line parameters
 
-  CmdLine cmd;
+  std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
+  std::string sfmDataFilename;
+  std::string outputFilename;
+  double zNear = -1.;
+  double zFar = -1.;
 
-  std::string sSfMData_Filename;
-  std::string sOutFile;
-  double z_near = -1.;
-  double z_far = -1.;
+  po::options_description allParams(
+    "Compute camera cones that share some putative visual content.\n"
+    "AliceVision FrustumFiltering");
 
-  cmd.add( make_option('i', sSfMData_Filename, "input_file") );
-  cmd.add( make_option('o', sOutFile, "output_file") );
-  cmd.add( make_option('n', z_near, "z_near") );
-  cmd.add( make_option('f', z_far, "z_far") );
+  po::options_description requiredParams("Required parameters");
+  requiredParams.add_options()
+    ("input,i", po::value<std::string>(&sfmDataFilename)->required(),
+      "SfMData file.")
+    ("output,o", po::value<std::string>(&outputFilename)->required(),
+      "Output pair filename.");
 
-  try {
-    if (argc == 1) throw std::string("Invalid parameter.");
-    cmd.process(argc, argv);
-  } catch(const std::string& s) {
-    std::cerr << "Usage: " << argv[0] << '\n'
-    << "[-i|--input_file] path to a SfMData scene\n"
-    << "[-o|--output_file] filename of the output pair file\n"
-    << "[-n|--z_near] 'optional' distance of the near camera plane\n"
-    << "[-f|--z_far] 'optional' distance of the far camera plane\n"
-    << std::endl;
+  po::options_description optionalParams("Optional parameters");
+  optionalParams.add_options()
+    ("zNear", po::value<double>(&zNear)->default_value(zNear),
+      "Distance of the near camera plane.")
+    ("zFar", po::value<double>(&zFar)->default_value(zFar),
+      "Distance of the far camera plane.");
 
-    std::cerr << s << std::endl;
+  po::options_description logParams("Log parameters");
+  logParams.add_options()
+    ("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
+      "verbosity level (fatal,  error, warning, info, debug, trace).");
+
+  allParams.add(requiredParams).add(optionalParams).add(logParams);
+
+  po::variables_map vm;
+  try
+  {
+    po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+    if(vm.count("help") || (argc == 1))
+    {
+      ALICEVISION_COUT(allParams);
+      return EXIT_SUCCESS;
+    }
+    po::notify(vm);
+  }
+  catch(boost::program_options::required_option& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
+    return EXIT_FAILURE;
+  }
+  catch(boost::program_options::error& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
     return EXIT_FAILURE;
   }
 
+  // set verbose level
+  system::Logger::get()->setLogLevel(verboseLevel);
+
   // Assert that we can create the output directory
-  if (!stlplus::folder_exists( stlplus::folder_part(sOutFile) ))
-    if(!stlplus::folder_create( stlplus::folder_part(sOutFile) ))
+  if (!stlplus::folder_exists( stlplus::folder_part(outputFilename) ))
+    if(!stlplus::folder_create( stlplus::folder_part(outputFilename) ))
       return EXIT_FAILURE;
 
   // Load input SfMData scene
   SfMData sfm_data;
-  if (!Load(sfm_data, sSfMData_Filename, ESfMData(ALL))) {
+  if (!Load(sfm_data, sfmDataFilename, ESfMData(ALL))) {
     std::cerr << std::endl
-      << "The input SfMData file \""<< sSfMData_Filename << "\" cannot be read." << std::endl;
+      << "The input SfMData file \""<< sfmDataFilename << "\" cannot be read." << std::endl;
     return EXIT_FAILURE;
   }
 
   aliceVision::system::Timer timer;
 
-  const PairSet pairs = BuildPairsFromFrustumsIntersections(sfm_data, z_near, z_far, stlplus::folder_part(sOutFile));
+  const PairSet pairs = BuildPairsFromFrustumsIntersections(sfm_data, zNear, zFar, stlplus::folder_part(outputFilename));
   /*const PairSet pairs = BuildPairsFromStructureObservations(sfm_data); */
 
   std::cout << "#pairs: " << pairs.size() << std::endl;
   std::cout << std::endl << " Pair filtering took (s): " << timer.elapsed() << std::endl;
 
   // export pairs on disk
-  if (savePairs(sOutFile, pairs))
+  if (savePairs(outputFilename, pairs))
   {
     return EXIT_SUCCESS;
   }
