ad6cb02ef92722839b22639cfc3453731c2da64f
Gregoire De Lillo <gregoire.delillo@me.com>
2017-09-27 10:50:25 +0200

    [software] sfmAlignment use boost program options

--- src/software/utils/main_sfmAlignment.cpp
+++ src/software/utils/main_sfmAlignment.cpp
@@ -5,8 +5,9 @@
 #include "aliceVision/sfm/utils/alignment.hpp"
 #include <aliceVision/config.hpp>
 
-#include "dependencies/cmdLine/cmdLine.h"
 #include "dependencies/stlplus3/filesystemSimplified/file_system.hpp"
+
+#include <boost/program_options.hpp>
 
 #include <string>
 #include <sstream>
@@ -14,62 +15,91 @@
 
 using namespace aliceVision;
 using namespace aliceVision::sfm;
+namespace po = boost::program_options;
 
 static bool parseAlignScale(const std::string& alignScale, double& S, Mat3& R, Vec3& t);
 
 // Convert from a SfMData format to another
 int main(int argc, char **argv)
 {
-  CmdLine cmd;
+  // command-line parameters
 
-  std::string sSfMData_Filename_In;
-  std::string sSfMData_Filename_InRef;
-  std::string sSfMData_Filename_Out;
-  std::string sSfm_Data_YAlignScale;
+  std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
+  std::string sfmDataFilename;
+  std::string outSfMDataFilename;
 
-  cmd.add(make_option('i', sSfMData_Filename_In, "input_file"));
-  cmd.add(make_option('r', sSfMData_Filename_InRef, "reference_file"));
-  cmd.add(make_option('y', sSfm_Data_YAlignScale, "y_align_scale"));
-  cmd.add(make_option('o', sSfMData_Filename_Out, "output_file"));
+  // user optional parameters
 
-  try {
-      if (argc == 1) throw std::string("Invalid command line parameter.");
-      cmd.process(argc, argv);
-  } catch(const std::string& s) {
-      std::cerr << "Usage: " << argv[0] << '\n'
-        << "[-i|--input_file] path to the input SfMData scene to align.\n"
-        << "[-o|--output_file] path to the output SfMData scene\n"
-        << "\t .json, .bin, .xml, .ply, .baf"
-#if ALICEVISION_IS_DEFINED(ALICEVISION_HAVE_ALEMBIC)
-           ", .abc"
-#endif
-           "\n"
-        << "\n[Optional]\n"
-        << "[-r|--reference_file] path to the scene used as the reference coordinate system\n"
-        << "[-y|--y_align_scale] align [X,Y,Z] to +Y-axis, rotate around Y by R deg, scale by S; syntax: X,Y,Z;R;S \n"
-        << std::endl;
+  std::string sfmDataReferenceFilename;
+  std::string sfmDataYAlignScale;
 
-      std::cerr << s << std::endl;
-      return EXIT_FAILURE;
+  po::options_description allParams("AliceVision sfmAlignment");
+
+  po::options_description requiredParams("Required parameters");
+  requiredParams.add_options()
+    ("input,i", po::value<std::string>(&sfmDataFilename)->required(),
+      "SfMData file to align.")
+    ("output,o", po::value<std::string>(&outSfMDataFilename)->required(),
+      "Output SfMData scene.");
+
+  po::options_description optionalParams("Optional parameters");
+  optionalParams.add_options()
+    ("reference,r", po::value<std::string>(&sfmDataReferenceFilename)->default_value(sfmDataReferenceFilename),
+      "Path to the scene used as the reference coordinate system.")
+    ("yAlignScale", po::value<std::string>(&sfmDataYAlignScale)->default_value(sfmDataYAlignScale),
+      "Align [X,Y,Z] to +Y-axis, rotate around Y by R deg, scale by S; syntax: X,Y,Z;R;S");
+
+  po::options_description logParams("Log parameters");
+  logParams.add_options()
+    ("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
+      "verbosity level (fatal,  error, warning, info, debug, trace).");
+
+  allParams.add(requiredParams).add(optionalParams).add(logParams);
+
+  po::variables_map vm;
+  try
+  {
+    po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+    if(vm.count("help") || (argc == 1))
+    {
+      ALICEVISION_COUT(allParams);
+      return EXIT_SUCCESS;
+    }
+    po::notify(vm);
   }
-  
+  catch(boost::program_options::required_option& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
+    return EXIT_FAILURE;
+  }
+  catch(boost::program_options::error& e)
+  {
+    ALICEVISION_CERR("ERROR: " << e.what());
+    ALICEVISION_COUT("Usage:\n\n" << allParams);
+    return EXIT_FAILURE;
+  }
 
-  if (sSfMData_Filename_In.empty() ||
-      sSfMData_Filename_Out.empty())
+  // set verbose level
+  system::Logger::get()->setLogLevel(verboseLevel);
+  
+  if (sfmDataFilename.empty() ||
+      outSfMDataFilename.empty())
   {
     std::cerr << "Invalid input or output filename." << std::endl;
     return EXIT_FAILURE;
   }
 
-  if (sSfMData_Filename_InRef.empty() &&
-      sSfm_Data_YAlignScale.empty())
+  if (sfmDataReferenceFilename.empty() &&
+      sfmDataYAlignScale.empty())
   {
     std::cerr << "At least one of -y and -r must be specified." << std::endl;
     return EXIT_FAILURE;
   }
   
-  if (!sSfMData_Filename_InRef.empty() &&
-      !sSfm_Data_YAlignScale.empty())
+  if (!sfmDataReferenceFilename.empty() &&
+      !sfmDataYAlignScale.empty())
   {
     std::cerr << "Must specify exactly one of alignment and reference scene." << std::endl;
     return EXIT_FAILURE;
@@ -77,10 +107,10 @@ int main(int argc, char **argv)
 
   // Load input scene
   SfMData sfm_data_in;
-  if (!Load(sfm_data_in, sSfMData_Filename_In, ESfMData(ALL)))
+  if (!Load(sfm_data_in, sfmDataFilename, ESfMData(ALL)))
   {
     std::cerr << std::endl
-      << "The input SfMData file \"" << sSfMData_Filename_In << "\" cannot be read." << std::endl;
+      << "The input SfMData file \"" << sfmDataFilename << "\" cannot be read." << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -88,14 +118,14 @@ int main(int argc, char **argv)
   Mat3 R;
   Vec3 t;
 
-  if (!sSfMData_Filename_InRef.empty())
+  if (!sfmDataReferenceFilename.empty())
   {
     // Load reference scene
     SfMData sfm_data_inRef;
-    if (!Load(sfm_data_inRef, sSfMData_Filename_InRef, ESfMData(ALL)))
+    if (!Load(sfm_data_inRef, sfmDataReferenceFilename, ESfMData(ALL)))
     {
       std::cerr << std::endl
-        << "The reference SfMData file \"" << sSfMData_Filename_InRef << "\" cannot be read." << std::endl;
+        << "The reference SfMData file \"" << sfmDataReferenceFilename << "\" cannot be read." << std::endl;
       return EXIT_FAILURE;
     }
 
@@ -105,13 +135,13 @@ int main(int argc, char **argv)
     {
       std::cerr << std::endl
         << "Failed to find similarity between the 2 SfM scenes:"
-        << "\"" << sSfMData_Filename_In << "\", "
-        << "\"" << sSfMData_Filename_InRef << "\""
+        << "\"" << sfmDataFilename << "\", "
+        << "\"" << sfmDataReferenceFilename << "\""
         << std::endl;
       return EXIT_FAILURE;
     }
   }
-  else if (!parseAlignScale(sSfm_Data_YAlignScale, S, R, t))
+  else if (!parseAlignScale(sfmDataYAlignScale, S, R, t))
   {
     std::cerr << std::endl << "Failed to parse align/scale argument.";
     return EXIT_FAILURE;
@@ -124,13 +154,13 @@ int main(int argc, char **argv)
 
   applyTransform(sfm_data_in, S, R, t);
   
-  std::cout << "Save into \"" << sSfMData_Filename_Out << "\"" << std::endl;
+  std::cout << "Save into \"" << outSfMDataFilename << "\"" << std::endl;
   
   // Export the SfMData scene in the expected format
-  if (!Save(sfm_data_in, sSfMData_Filename_Out, ESfMData(ALL)))
+  if (!Save(sfm_data_in, outSfMDataFilename, ESfMData(ALL)))
   {
     std::cerr << std::endl
-      << "An error occurred while trying to save \"" << sSfMData_Filename_Out << "\"." << std::endl;
+      << "An error occurred while trying to save \"" << outSfMDataFilename << "\"." << std::endl;
     return EXIT_FAILURE;
   }
 
