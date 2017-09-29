1f84fdbd98922d50368339cfb05a8c94b2b282c5
Gregoire De Lillo <gregoire.delillo@me.com>
2017-09-29 11:54:31 +0200

    [unitTest] Remove CppUnitLite

--- src/aliceVision/multiview/rotationAveraging/rotationAveraging_test.cpp
+++ src/aliceVision/multiview/rotationAveraging/rotationAveraging_test.cpp
@@ -1,7 +1,6 @@
 // This file is part of the AliceVision project and is made available under
 // the terms of the MPL2 license (see the COPYING.md file).
 
-#include "CppUnitLite/TestHarness.h"
 #include "aliceVision/multiview/rotationAveraging/rotationAveraging.hpp"
 #include "aliceVision/multiview/essential.hpp"
 #include <aliceVision/system/Logger.hpp>
