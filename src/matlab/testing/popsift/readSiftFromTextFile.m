aec201b993caf9dde2b42107d19afe7bdc2aab1f
Lilian Calvet <lilian.calvet@gmail.com>
2017-10-17 09:52:12 +0200

    Remove feature matching eval.

--- src/matlab/testing/popsift/readSiftFromTextFile.m
+++
@@ -1,9 +0,0 @@
-function [f,des] = readSiftFromTextFile(fileName)
-
-M = dlmread(fileName, ' ', 2, 0);
-
-f = M(:,1:5)';
-f(1:2,:) = f(1:2,:)+1;
-des = M(:,6:end)';
-
-f(3,:) = sqrt(1./f(3,:));
--- src/matlab/testing/popsift/setPath.m
+++
@@ -1,7 +0,0 @@
-% external
-addpath('/home/lilian/matlab_workspace/common/extern/vision/feature/repeatability_demo');
-addpath(genpath('/home/lilian/matlab_workspace/common/extern/vision/vlfeat/vlfeat-0.9.20/'));
-
-addpath(genpath('/home/lilian/matlab_workspace/common/Pierre'));
-addpath(genpath('/home/lilian/matlab_workspace/common/Lilian'));
-addpath(genpath('/home/lilian/matlab_workspace/common/extern/vision/kovesi'));
