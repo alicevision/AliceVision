ada632569635cda7e108583a58124b5bbed19105
Gregoire De Lillo <gregoire.delillo@me.com>
2017-11-09 17:25:15 +0100

    [software] CameraInit now use `.sfm` file instead of custom JSON file
    - use now image folder or `.sfm` file in input
    - add checks
    - some refactoring

---
+++ src/aliceVision/sfm/viewIO.hpp
@@ -0,0 +1,37 @@
+// This file is part of the AliceVision project.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <aliceVision/sfm/View.hpp>
+#include <aliceVision/camera/cameraCommon.hpp>
+#include <aliceVision/camera/IntrinsicBase.hpp>
+
+#include <memory>
+
+namespace aliceVision {
+namespace sfm {
+
+/**
+ * @brief update an incomplete view (at least only the image path)
+ * @param view The given incomplete view
+ */
+void updateIncompleteView(View& view);
+
+/**
+ * @brief creat an intrinsic for the given View
+ * @param view The given view
+ * @param defaultSensorWidth (-1 if unknown)
+ * @param defaultFocalLengthPx (-1 if unknown)
+ * @param defaultIntrinsicType (PINHOLE_CAMERA_START if unknown)
+ * @return shared_ptr IntrinsicBase
+ */
+std::shared_ptr<camera::IntrinsicBase> getViewIntrinsic(const View& view,
+                                                double defaultSensorWidth = -1,
+                                                double defaultFocalLengthPx = -1,
+                                                camera::EINTRINSIC defaultIntrinsicType = camera::PINHOLE_CAMERA_START);
+
+} // namespace sfm
+} // namespace aliceVision
