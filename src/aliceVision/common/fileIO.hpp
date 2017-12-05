f3e873fc6eabe67010169a591b880a8dae2ea517
Gregoire De Lillo <gregoire.delillo@me.com>
2017-12-05 16:44:40 +0100

    [common] remove unused functions in `fileIO`

--- src/aliceVision/common/fileIO.hpp
+++ src/aliceVision/common/fileIO.hpp
@@ -14,7 +14,6 @@ struct multiviewInputParams;
 bool FileExists(const std::string& filePath);
 bool FolderExists(const std::string& folderPath);
 
-
 std::string mv_getFileNamePrefixRcTc(multiviewInputParams* mip, int rc, int tc);
 std::string mv_getFileNamePrefix(multiviewInputParams* mip, int index);
 std::string mv_getFileName(multiviewInputParams* mip, int index, int mv_file_type);
@@ -28,23 +27,8 @@ int get2dPointsNum(int imgFileId, multiviewInputParams* mip);
 int load2dPoints(point2d** _out, int imgFileId, multiviewInputParams* mip);
 staticVector<point2d>* load2dPoints(int imgFileId, multiviewInputParams* mip);
 void load2dPoints(staticVector<point2d>* out, int imgFileId, multiviewInputParams* mip);
-//bool saveImgAsImg(const std::string& fileName, const std::string& fileNameT, int imgFileId, multiviewInputParams* mip);
-//void downscaleImage(const std::string& fileName, const std::string& fileNameT, int imgFileId, multiviewInputParams* mip,
-//                    int scaleFactor);
-//void blurImage(const std::string& fileName, const std::string& fileNameT, int imgFileId, multiviewInputParams* mip,
-//               int scaleFactor, int blurWin);
-void memcpyRGBImageFromFileToArr(int camId, rgb* imgArr, int imgFileId, multiviewInputParams* mip, bool transpose, int scaleFactor,
-                                 int bandType);
 void memcpyRGBImageFromFileToArr(int camId, rgb* imgArr, const std::string& fileNameOrigStr, multiviewInputParams* mip,
                                  bool transpose, int scaleFactor, int bandType);
-void memcpyRGBImageFromFileToArr(int camId, unsigned char* imgArrR, unsigned char* imgArrG, unsigned char* imgArrB, int imgFileId,
-                                 multiviewInputParams* mip, bool transpose, int scaleFactor, int bandType);
-void memcpyRGBImageFromFileToArr(int camId, unsigned char* imgArrR, unsigned char* imgArrG, unsigned char* imgArrB,
-                                 const std::string& fileNameOrigStr, multiviewInputParams* mip, bool transpose,
-                                 int scaleFactor, int bandType);
-void memcpyGrayImageFromFileToArr(int camId, unsigned char* imgArr, int imgFileId, multiviewInputParams* mip, bool transpose,
-                                  int scaleFactor, int bandType);
-
 struct seed_io_block            // 80 bytes
 {
     orientedPoint op;           // 28 bytes
@@ -62,7 +46,7 @@ void saveSeedsToFile(staticVector<seedPoint>* seeds, int refImgFileId, multiview
 bool loadSeedsFromFile(staticVector<seedPoint>** seeds, const std::string& fileName);
 bool loadSeedsFromFile(staticVector<seedPoint>** seeds, int refImgFileId, multiviewInputParams* mip, int mv_file_type);
 int getSeedsSizeFromFile(int refImgFileId, multiviewInputParams* mip, int mv_file_type);
-int getGrowedSizeFromFile(int refImgFileId, multiviewInputParams* mip);
+//int getGrowedSizeFromFile(int refImgFileId, multiviewInputParams* mip);
 
 void saveUniqueIdAliveToFile(std::vector<bool>* uniqueIdAlive, multiviewInputParams* mip);
 void loadUniqueIdAliveFromFile(std::vector<bool>* uniqueIdAlive, multiviewInputParams* mip);
@@ -96,35 +80,24 @@ struct outStruct
 
 void savePrematchedToFile(outStruct* outv, int size, int refImgFileId, int tarImgFileId, multiviewInputParams* mip);
 outStruct* loadPrematchedFromFile(int* sz, int refImgFileId, int tarImgFileId, multiviewInputParams* mip);
+
 //bool loadAgreedVisMapToFileFromFileWithoutAllocation(idValue* vm, int refImgFileId, multiviewInputParams* mip);
 //bool saveAgreedVisMapToFile(idValue* vm, int refImgFileId, multiviewInputParams* mip);
 //bool loadDiskSizeMapFromFileWithoutAllocation(int* vm, int refImgFileId, multiviewInputParams* mip);
 //bool saveDiskSizeMapToFile(int* vm, int refImgFileId, multiviewInputParams* mip);
+
 void deleteAllFiles(multiviewInputParams* mip);
-void saveToImg(const std::string& fileName, unsigned char* img, int width, int height, bool T);
-void saveToImgResize(const std::string& fileName, unsigned char* img, int width, int height, int widthNew,
-                     int heightNew, bool T);
-unsigned char* imgResize(unsigned char* img, int width, int height, int widthNew, int heightNew, bool T);
 bool getDepthMapInfo(int refImgFileId, multiviewInputParams* mip, float& mindepth, float& maxdepth,
                      staticVector<int>** tcams);
-void saveSegToImgCol(rgb* colors, const std::string& fileName, int* img, int width, int height, bool T);
-void saveSegToImg(const std::string& fileName, int* img, int width, int height, bool T);
-void saveToImg(const std::string& fileName, Color* img, int width, int height, bool T);
 bool getDepthMapInfoDepthLimits(int refImgFileId, multiviewInputParams* mip, float& mindepth, float& maxdepth);
-void saveImgAsPgm(const std::string& img, const std::string& imgpgm);
 bool DeleteDirectory(const std::string& sPath);
 bool getDirectoryFiles(std::vector<std::string>& out, const std::string& sPath, const std::string& ext);
-void imagesc(const std::string& imageFileName, float* data, int w, int h, float minVal, float maxVal, bool T = false);
-void imagesc(const std::string& imageFileName, int* data, int w, int h, int minVal, int maxVal, bool T = false);
-void imagesc(const std::string& imageFileName, unsigned short* data, int w, int h, unsigned short minVal,
-             unsigned short maxVal, bool T);
-void imagesc(const std::string& imageFileName, float* data, int w, int h, bool T = false);
-//void imagesc16bitGrayNpg(const std::string& imageFileName, float* data, int w, int h, float minVal, float maxVal);
 
 typedef struct SiftKeypoint
 {
     float x, y, s, o; // x, y, scale, orientation.
 } SiftKeypoint;
+
 void readSifts(const std::string& fileName, staticVector<float>** descriptors, staticVector<SiftKeypoint>** keys);
 
 void splitString(const std::string& str, const std::string& delimiters, std::vector<std::string>& tokens);
