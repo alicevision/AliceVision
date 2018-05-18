37c2654947edbf3587d611afe3d7738995efa23f
Carsten Griwodz <griff@simula.no>
2018-05-18 13:12:56 +0200

    [cmake] CUDA compute cap. selection, NVTX profiling option

---
+++ src/aliceVision/system/nvtx.cpp
@@ -0,0 +1,23 @@
+#ifdef USE_NVTX
+
+#include <sstream>
+#include <nvToolsExtCuda.h>
+#include <boost/filesystem/path.hpp>
+
+#include "aliceVision/system/nvtx.hpp"
+
+void nvtxPushA( const char* label, const char* file, int line )
+{
+    boost::filesystem::path in( file );
+
+    std::ostringstream ostr;
+    ostr << label << " " << in.filename() << ":" << line;
+    nvtxRangePushA( ostr.str().c_str() );
+}
+
+void nvtxPop ( const char* )
+{
+    nvtxRangePop( );
+}
+
+#endif /* USE_NVTX */
