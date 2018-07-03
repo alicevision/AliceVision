4455cd97a25e4d5a8768a6028251fff6182e3f40
Yann Lanthony <yann.lanthony@gmail.com>
2018-07-03 16:09:03 +0200

    [cmake][bundle] call fixup_bundle only on one executable
    * 'fixup_bundle' automatically fixups the other executables in the bundle path
    * 'verify_app' is already called for each app in 'fixup_bundle'

--- src/cmake/MakeBundle.cmake
+++ src/cmake/MakeBundle.cmake
@@ -2,11 +2,12 @@ include(BundleUtilities)
 
 # Perform bundle fixup on all executables of an install directory
 # and generates a standalone bundle with all required runtime dependencies.
-# 
+#
 # This scripts accepts the following parameters:
 #   - CMAKE_INSTALL_PREFIX: install target path
 #   - BUNDLE_INSTALL_PREFIX: bundle installation path
-#   - BUNDLE_LIBS_PATHS: additional paths (semi-colon separated) to look for runtime dependencies 
+#   - BUNDLE_LIBS_PATHS: additional paths (colon separated) to look for runtime dependencies
+
 message(STATUS "Starting Bundle")
 message(STATUS "CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
 message(STATUS "BUNDLE_INSTALL_PREFIX: ${BUNDLE_INSTALL_PREFIX}")
@@ -17,17 +18,14 @@ set(LIBS_LOOKUPS_PATHS "${CMAKE_INSTALL_PREFIX}/lib")
 list(APPEND LIBS_LOOKUPS_PATHS ${BUNDLE_LIBS_PATHS})
 
 # Get all executables in installed 'bin' folder
+# and copy them in the bundle installation path
 get_bundle_all_executables(${CMAKE_INSTALL_PREFIX}/bin BUNDLE_APPS)
-message(STATUS "BUNDLE_APPS: ${BUNDLE_APPS}")
+file(INSTALL ${BUNDLE_APPS} DESTINATION ${BUNDLE_INSTALL_PREFIX})
 
-# Perform the bundle fixup
-foreach(f ${BUNDLE_APPS})
-  get_filename_component(filename "${f}" NAME)
-  set(bf "${BUNDLE_INSTALL_PREFIX}/${filename}")
-  message(STATUS "Bundle application: ${f} => ${bf}")
-  file(COPY "${f}" DESTINATION "${BUNDLE_INSTALL_PREFIX}")
-  fixup_bundle(${bf} "" "${LIBS_LOOKUPS_PATHS}" )
-  verify_app("${bf}")
-endforeach()
+# Get first bundled executable as reference app
+# fixup_bundle will automatically fixup all the others executable in the bundle
+get_bundle_all_executables(${BUNDLE_INSTALL_PREFIX} BUNDLE_APPS)
+list(GET BUNDLE_APPS 0 MAIN_APP)
+fixup_bundle(${MAIN_APP} "" "${LIBS_LOOKUPS_PATHS}")
 
-message(STATUS "Bundle done")
+message(STATUS "Bundle done: ${BUNDLE_INSTALL_PREFIX}")
