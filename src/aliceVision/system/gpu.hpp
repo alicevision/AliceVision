8406ef920d539226a3048a04cd5d475045806ed1
Carsten Griwodz <griff@simula.no>
2018-07-31 15:29:22 +0200

    [depthsMap] add texture size output to GPU properties

--- src/aliceVision/system/gpu.hpp
+++ src/aliceVision/system/gpu.hpp
@@ -89,8 +89,8 @@ inline std::string gpuInformationCUDA()
     information = "CUDA-Enabled GPU.\n";
     for(int i = 0; i < nbDevices; ++i)
     {
-      std::unique_ptr<cudaDeviceProp> deviceProperties(new cudaDeviceProp);
-      if(cudaGetDeviceProperties(deviceProperties.get(), i) != cudaSuccess)
+      cudaDeviceProp deviceProperties;
+      if(cudaGetDeviceProperties( &deviceProperties, i) != cudaSuccess )
         throw std::runtime_error("Cannot get properties for CUDA gpu device " + std::to_string(i));
 
       size_t avail;
@@ -102,30 +102,45 @@ inline std::string gpuInformationCUDA()
 
       deviceSS << "Device information:" << std::endl
                << "\t- id:                      " << i << std::endl
-               << "\t- name:                    " << deviceProperties->name << std::endl
-               << "\t- compute capability:      " << deviceProperties->major << "." << deviceProperties->minor << std::endl
-               << "\t- total device memory:     " << deviceProperties->totalGlobalMem / (1024 * 1024) << " MB " << std::endl
+               << "\t- name:                    " << deviceProperties.name << std::endl
+               << "\t- compute capability:      " << deviceProperties.major << "." << deviceProperties.minor << std::endl
+               << "\t- total device memory:     " << deviceProperties.totalGlobalMem / (1024 * 1024) << " MB " << std::endl
                << "\t- device memory available: " << avail / (1024 * 1024) << " MB " << std::endl
-               << "\t- per-block shared memory: " << deviceProperties->sharedMemPerBlock << std::endl
-               << "\t- warp size:               " << deviceProperties->warpSize << std::endl
-               << "\t- max threads per block:   " << deviceProperties->maxThreadsPerBlock << std::endl
-               << "\t- max threads per SM(X):   " << deviceProperties->maxThreadsPerMultiProcessor << std::endl
+               << "\t- per-block shared memory: " << deviceProperties.sharedMemPerBlock << std::endl
+               << "\t- warp size:               " << deviceProperties.warpSize << std::endl
+               << "\t- max threads per block:   " << deviceProperties.maxThreadsPerBlock << std::endl
+               << "\t- max threads per SM(X):   " << deviceProperties.maxThreadsPerMultiProcessor << std::endl
                << "\t- max block sizes:         "
-               << "{" << deviceProperties->maxThreadsDim[0]
-               << "," << deviceProperties->maxThreadsDim[1]
-               << "," << deviceProperties->maxThreadsDim[2] << "}" << std::endl
+               << "{" << deviceProperties.maxThreadsDim[0]
+               << "," << deviceProperties.maxThreadsDim[1]
+               << "," << deviceProperties.maxThreadsDim[2] << "}" << std::endl
                << "\t- max grid sizes:          "
-               << "{" << deviceProperties->maxGridSize[0]
-               << "," << deviceProperties->maxGridSize[1]
-               << "," << deviceProperties->maxGridSize[2] << "}" << std::endl
-               << "\t- number of SM(x)s:        " << deviceProperties->multiProcessorCount << std::endl
-               << "\t- registers per SM(x):     " << deviceProperties->regsPerMultiprocessor << std::endl
-               << "\t- registers per block:     " << deviceProperties->regsPerBlock << std::endl
-               << "\t- concurrent kernels:      " << (deviceProperties->concurrentKernels ? "yes":"no") << std::endl
-               << "\t- mapping host memory:     " << (deviceProperties->canMapHostMemory ? "yes":"no") << std::endl
-               << "\t- unified addressing:      " << (deviceProperties->unifiedAddressing ? "yes":"no") << std::endl
-               << "\t- texture alignment:       " << deviceProperties->textureAlignment << " byte" << std::endl
-               << "\t- pitch alignment:         " << deviceProperties->texturePitchAlignment << " byte" << std::endl;
+               << "{" << deviceProperties.maxGridSize[0]
+               << "," << deviceProperties.maxGridSize[1]
+               << "," << deviceProperties.maxGridSize[2] << "}" << std::endl
+               << "\t- max 2D array texture:    "
+               << "{" << deviceProperties.maxTexture2D[0]
+               << "," << deviceProperties.maxTexture2D[1] << "}" << std::endl
+               << "\t- max 3D array texture:    "
+               << "{" << deviceProperties.maxTexture3D[0]
+               << "," << deviceProperties.maxTexture3D[1]
+               << "," << deviceProperties.maxTexture3D[2] << "}" << std::endl
+               << "\t- max 2D linear texture:   "
+               << "{" << deviceProperties.maxTexture2DLinear[0]
+               << "," << deviceProperties.maxTexture2DLinear[1]
+               << "," << deviceProperties.maxTexture2DLinear[2] << "}" << std::endl
+               << "\t- max 2D layered texture:  "
+               << "{" << deviceProperties.maxTexture2DLayered[0]
+               << "," << deviceProperties.maxTexture2DLayered[1]
+               << "," << deviceProperties.maxTexture2DLayered[2] << "}" << std::endl
+               << "\t- number of SM(x)s:        " << deviceProperties.multiProcessorCount << std::endl
+               << "\t- registers per SM(x):     " << deviceProperties.regsPerMultiprocessor << std::endl
+               << "\t- registers per block:     " << deviceProperties.regsPerBlock << std::endl
+               << "\t- concurrent kernels:      " << (deviceProperties.concurrentKernels ? "yes":"no") << std::endl
+               << "\t- mapping host memory:     " << (deviceProperties.canMapHostMemory ? "yes":"no") << std::endl
+               << "\t- unified addressing:      " << (deviceProperties.unifiedAddressing ? "yes":"no") << std::endl
+               << "\t- texture alignment:       " << deviceProperties.textureAlignment << " byte" << std::endl
+               << "\t- pitch alignment:         " << deviceProperties.texturePitchAlignment << " byte" << std::endl;
 
       information += deviceSS.str();
     }
