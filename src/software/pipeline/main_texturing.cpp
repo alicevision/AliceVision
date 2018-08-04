3053cdf849fba2cef97dc89bdbd4430f10866c33
Fabien Castan <fabcastan@gmail.com>
2018-08-04 19:42:00 +0200

    [software] texturing: update major version

--- src/software/pipeline/main_texturing.cpp
+++ src/software/pipeline/main_texturing.cpp
@@ -19,7 +19,7 @@
 
 // These constants define the current software version.
 // They must be updated when the command line is changed.
-#define ALICEVISION_SOFTWARE_VERSION_MAJOR 1
+#define ALICEVISION_SOFTWARE_VERSION_MAJOR 2
 #define ALICEVISION_SOFTWARE_VERSION_MINOR 0
 
 using namespace aliceVision;
