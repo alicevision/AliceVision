397f788fca2e77aa0d1fb65ab057c5f5f975e34c
natowi <natowi@users.noreply.github.com>
2018-12-19 18:54:32 +0100

    Typo fix 6
    textureSide->textureSize https://github.com/alicevision/meshroom/pull/321

--- src/aliceVision/mesh/UVAtlas.cpp
+++ src/aliceVision/mesh/UVAtlas.cpp
@@ -15,8 +15,8 @@ namespace mesh {
 using namespace std;
 
 UVAtlas::UVAtlas(const Mesh& mesh, mvsUtils::MultiViewParams& mp, StaticVector<StaticVector<int>*>* ptsCams,
-                                 unsigned int textureSide, unsigned int gutterSize)
-    : _textureSide(textureSide)
+                                 unsigned int textureSize, unsigned int gutterSize)
+    : _textureSize(textureSize)
     , _gutterSize(gutterSize)
     , _mesh(mesh)
 {
@@ -243,8 +243,8 @@ void UVAtlas::createTextureAtlases(vector<Chart>& charts, mvsUtils::MultiViewPar
         ChartRect* root = new ChartRect();
         root->LU.x = 0;
         root->LU.y = 0;
-        root->RD.x = _textureSide - 1;
-        root->RD.y = _textureSide - 1;
+        root->RD.x = _textureSize - 1;
+        root->RD.y = _textureSize - 1;
 
         const auto insertChart = [&](size_t idx) -> bool
         {
