fe03ca190c7fb557e61e406f10a4b031a186568d
Yann Lanthony <yann.lanthony@gmail.com>
2019-04-18 15:08:50 +0200

    [pipeline] featureMatching: output one match file by default
    `matchFilePerImage` was activated by default to handle parallel runs of `featureMatching` on different ranges of views of the same scene. This is now handled by the prefix added to output files when using range related parameters.

--- src/software/pipeline/main_featureMatching.cpp
+++ src/software/pipeline/main_featureMatching.cpp
@@ -108,7 +108,7 @@ int main(int argc, char **argv)
   bool savePutativeMatches = false;
   bool guidedMatching = false;
   int maxIteration = 2048;
-  bool matchFilePerImage = true;
+  bool matchFilePerImage = false;
   size_t numMatchesToKeep = 0;
   bool useGridSort = true;
   bool exportDebugFiles = false;
