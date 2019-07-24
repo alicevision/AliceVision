fa3cc528b1ad3c547957eed9da49ba08f74dee58
Fabien Castan <fabcastan@gmail.com>
2019-07-24 20:14:57 +0200

    [depthMap] bug fix: use RAII instead of pointers
    In particular configurations (failed to find Tc cameras), sgmrc can stop early before allocating the _volumeBestIdVal, which is later used in refine step.

--- src/aliceVision/depthMap/RefineRc.cpp
+++ src/aliceVision/depthMap/RefineRc.cpp
@@ -57,12 +57,12 @@ DepthSimMap* RefineRc::getDepthPixSizeMapFromSGM()
     StaticVector<IdValue> volumeBestIdVal;
     volumeBestIdVal.reserve(_width * _height);
 
-    for(int i = 0; i < _volumeBestIdVal->size(); i++)
+    for(int i = 0; i < _volumeBestIdVal.size(); i++)
     {
         // float sim = (*depthSimMapFinal->dsm)[i].y;
         // sim = std::min(sim,mp->simThr);
         const float sim = _sp->mp->simThr - 0.0001;
-        const int id = (*_volumeBestIdVal)[i].id;
+        const int id = _volumeBestIdVal[i].id;
 
         if(id > 0)
           volumeBestIdVal.push_back(IdValue(id, sim));
