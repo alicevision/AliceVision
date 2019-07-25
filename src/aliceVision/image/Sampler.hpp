bdd2b910b8c1de3a80dc971f9c84757d917449b8
Unknown <anouk.liberge@orange.fr>
2019-07-25 12:05:51 +0200

    [image][Sampler] bug fix : call to default constructor
    All channels need to be set to zero including alpha for RGBA colors

--- src/aliceVision/image/Sampler.hpp
+++ src/aliceVision/image/Sampler.hpp
@@ -430,7 +430,8 @@ struct Sampler2d
     _sampler( dx , coefs_x ) ;
     _sampler( dy , coefs_y ) ;
 
-    typename RealPixel<T>::real_type res(0) ;
+    // Default color constructor init all channels to zero
+    typename RealPixel<T>::real_type res;
 
     // integer position of sample (x,y)
     const int grid_x = static_cast<int>( floor( x ) );
