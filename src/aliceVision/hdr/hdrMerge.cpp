4202572d8ab2de820293582b1b7504d81de7c518
Anouk Liberge <anouk.liberge@orange.fr>
2019-08-06 12:03:31 +0200

    [hdr] change weight to reduce noise in dark areas

--- src/aliceVision/hdr/hdrMerge.cpp
+++ src/aliceVision/hdr/hdrMerge.cpp
@@ -71,7 +71,7 @@ void hdrMerge::process(const std::vector< image::Image<image::RGBfColor> > &imag
           //for each images
           const double value = images[i](y, x)(channel);
           const double time = times[i];
-          double w = std::max(0.f, weight(value, channel) - weight(0.01, 0));
+          double w = std::max(0.f, weight(value, channel) - weight(0.05, 0));
 
           const double r = response(value, channel);
 
