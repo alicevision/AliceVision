e5cf255b621d2e77be38e2538f83dc70260fc526
fabienservant <fabien.servant@interdigital.com>
2020-02-07 17:35:09 +0100

    Add radial derivatives

--- src/aliceVision/camera/DistortionRadial.hpp
+++ src/aliceVision/camera/DistortionRadial.hpp
@@ -64,6 +64,38 @@ public:
     return (p * r_coeff);
   }
 
+  virtual Eigen::Matrix2d getDerivativeAddDistoWrtPt(const Vec2 & p) const override {
+
+    const double k1 = _distortionParams[0];
+
+    const double r = sqrt(p(0)*p(0) + p(1)*p(1));
+    Eigen::Matrix<double, 1, 2> d_r_d_p;
+    d_r_d_p(0) = p(0) / r;
+    d_r_d_p(1) = p(1) / r;
+    
+    const double r2 = r * r;
+    const double r_coeff = 1.0 + k1 * r2;
+
+    double d_r_coeff_d_r = 2.0 * k1 * r;
+    Eigen::Matrix<double, 1, 2> d_r_coeff_d_p = d_r_coeff_d_r * d_r_d_p;
+
+    return Eigen::Matrix2d::Identity() * r_coeff + p * d_r_coeff_d_p;
+  }
+
+  virtual Eigen::MatrixXd getDerivativeAddDistoWrtDisto(const Vec2 & p) const  override {
+    
+    const double k1 = _distortionParams[0];
+
+    const double r = sqrt(p(0)*p(0) + p(1)*p(1));    
+    const double r2 = r * r;
+    
+    const double r_coeff = 1.0 + k1 * r2;
+
+    Eigen::MatrixXd ret = p * 2.0 * k1 * r;
+
+    return ret;
+  }
+
   /// Remove distortion (return p' such that disto(p') = p)
   virtual Vec2 remove_disto(const Vec2& p) const override {
     // Compute the radius from which the point p comes from thanks to a bisection
@@ -72,6 +104,36 @@ public:
     const double r2 = p(0)*p(0) + p(1)*p(1);
     const double radius = (r2 == 0) ? 1. : ::sqrt(radial_distortion::bisection_Radius_Solve(_distortionParams, r2, distoFunctor) / r2);
     return radius * p;
+  }
+
+  virtual Eigen::Matrix2d getDerivativeRemoveDistoWrtPt(const Vec2 & p) const override {
+
+    Vec2 undist = remove_disto(p);
+
+    Eigen::Matrix2d Jinv = getDerivativeAddDistoWrtPt(undist);
+
+    return Jinv.inverse();
+  }
+
+  virtual Eigen::MatrixXd getDerivativeRemoveDistoWrtDisto(const Vec2 & p) const override {
+    
+    Vec2 p_undist = remove_disto(p);
+
+    const double k1 = _distortionParams[0];
+
+    const double r = sqrt(p(0)*p(0) + p(1)*p(1));
+    const double r2 = r * r;
+    
+    const double r_coeff = 1.0 + k1 * r2;
+
+    Eigen::Matrix<double, 1, 1> d_rcoeff_d_params;
+    d_rcoeff_d_params(0, 0) = r2;
+    
+    Eigen::Matrix<double, 2, 1> ret;
+    ret(0, 0) = - (p(0) * d_rcoeff_d_params(0, 0)) / (r_coeff * r_coeff);
+    ret(1, 0) = - (p(1) * d_rcoeff_d_params(0, 0)) / (r_coeff * r_coeff);
+
+    return ret;
   }
 
   virtual double getUndistortedRadius(double r) const override {
@@ -108,10 +170,60 @@ public:
     const double r2 = r * r;
     const double r4 = r2 * r2;
     const double r6 = r4 * r2;
-    const double r_coeff = (1. + k1*r2 + k2*r4 + k3*r6);
+    const double r_coeff = (1. + k1 * r2 + k2 * r4 + k3 * r6);
 
     return (p * r_coeff);
   }
+
+  virtual Eigen::Matrix2d getDerivativeAddDistoWrtPt(const Vec2 & p) const override {
+
+    const double k1 = _distortionParams[0];
+    const double k2 = _distortionParams[1];
+    const double k3 = _distortionParams[2];
+
+    const double r = sqrt(p(0)*p(0) + p(1)*p(1));
+    Eigen::Matrix<double, 1, 2> d_r_d_p;
+    d_r_d_p(0) = p(0) / r;
+    d_r_d_p(1) = p(1) / r;
+    
+    const double r2 = r * r;
+    const double r3 = r2 * r;
+    const double r4 = r2 * r2;
+    const double r5 = r4 * r;
+    const double r6 = r4 * r2;
+    
+    const double r_coeff = 1.0 + k1 * r2 + k2 * r4 + k3 * r6;
+
+    double d_r_coeff_d_r = 2.0 * k1 * r + 4.0 * k2 * r3 + 6.0 * k3 * r5;
+    Eigen::Matrix<double, 1, 2> d_r_coeff_d_p = d_r_coeff_d_r * d_r_d_p;
+
+    return Eigen::Matrix2d::Identity() * r_coeff + p * d_r_coeff_d_p;
+  }
+
+  virtual Eigen::MatrixXd getDerivativeAddDistoWrtDisto(const Vec2 & p) const  override {
+    
+    const double k1 = _distortionParams[0];
+    const double k2 = _distortionParams[1];
+    const double k3 = _distortionParams[2];
+
+    const double r = sqrt(p(0)*p(0) + p(1)*p(1));
+    const double r2 = r * r;
+    const double r4 = r2 * r2;
+    const double r6 = r4 * r2;
+    
+
+    const double r_coeff = 1.0 + k1 * r2 + k2 * r4 + k3 * r6;
+
+    Eigen::Matrix<double, 1, 3> d_r_coeff_d_params;
+    d_r_coeff_d_params(0, 0) = r2;
+    d_r_coeff_d_params(0, 1) = r4;
+    d_r_coeff_d_params(0, 2) = r6;
+
+    Eigen::MatrixXd ret = p * d_r_coeff_d_params;
+
+    return ret;
+  }
+
 
   /// Remove distortion (return p' such that disto(p') = p)
   virtual Vec2 remove_disto(const Vec2& p) const override {
@@ -123,6 +235,46 @@ public:
       1. :
       ::sqrt(radial_distortion::bisection_Radius_Solve(_distortionParams, r2, distoFunctor) / r2);
     return radius * p;
+  }
+
+  virtual Eigen::Matrix2d getDerivativeRemoveDistoWrtPt(const Vec2 & p) const override {
+
+    Vec2 undist = remove_disto(p);
+
+    Eigen::Matrix2d Jinv = getDerivativeAddDistoWrtPt(undist);
+
+    return Jinv.inverse();
+  }
+
+  virtual Eigen::MatrixXd getDerivativeRemoveDistoWrtDisto(const Vec2 & p) const override {
+    
+    Vec2 p_undist = remove_disto(p);
+
+    const double k1 = _distortionParams[0];
+    const double k2 = _distortionParams[1];
+    const double k3 = _distortionParams[2];
+
+    const double r = sqrt(p(0)*p(0) + p(1)*p(1));
+    const double r2 = r * r;
+    const double r4 = r2 * r2;
+    const double r6 = r4 * r2;
+    
+    const double r_coeff = 1.0 + k1 * r2 + k2 * r4 + k3 * r6;
+
+    Eigen::Matrix<double, 1, 3> d_rcoeff_d_params;
+    d_rcoeff_d_params(0, 0) = r2;
+    d_rcoeff_d_params(0, 1) = r4;
+    d_rcoeff_d_params(0, 2) = r6;
+    
+    Eigen::Matrix<double, 2, 3> ret;
+    ret(0, 0) = - (p(0) * d_rcoeff_d_params(0, 0)) / (r_coeff * r_coeff);
+    ret(0, 1) = - (p(0) * d_rcoeff_d_params(0, 1)) / (r_coeff * r_coeff);
+    ret(0, 2) = - (p(0) * d_rcoeff_d_params(0, 2)) / (r_coeff * r_coeff);
+    ret(1, 0) = - (p(1) * d_rcoeff_d_params(0, 0)) / (r_coeff * r_coeff);
+    ret(1, 1) = - (p(1) * d_rcoeff_d_params(0, 1)) / (r_coeff * r_coeff);
+    ret(1, 2) = - (p(1) * d_rcoeff_d_params(0, 2)) / (r_coeff * r_coeff);
+
+    return ret;
   }
 
   virtual double getUndistortedRadius(double r) const override {
@@ -216,11 +368,7 @@ public:
     d_denum_d_params(0, 1) = 1;
     d_denum_d_params(0, 2) = 1;
 
-
-
     Eigen::Matrix<double, 1, 3> d_rcoeff_d_params = (denum * d_num_d_params - num * d_denum_d_params) / denum2;
-   
-
     Eigen::MatrixXd ret = p * d_rcoeff_d_params;
 
     return ret;
