72600eeb0d2b4c9d512c6ea6f0de4102b752cc3f
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-02 09:53:17 +0100

    [camera] use only override (no virtual)

--- src/aliceVision/camera/DistortionFisheye1.hpp
+++ src/aliceVision/camera/DistortionFisheye1.hpp
@@ -12,20 +12,24 @@
 namespace aliceVision {
 namespace camera {
 
-class DistortionFisheye1 : public Distortion {
+class DistortionFisheye1 : public Distortion
+{
 public:
-  DistortionFisheye1() {
+    
+  DistortionFisheye1()
+  {
     _distortionParams = {0.0};
   }
 
-  explicit DistortionFisheye1(double p1) {
+  explicit DistortionFisheye1(double p1)
+  {
     _distortionParams = {p1};
   }
 
   DistortionFisheye1* clone() const override { return new DistortionFisheye1(*this); }
 
   /// Add distortion to the point p (assume p is in the camera frame [normalized coordinates])
-  virtual Vec2 add_disto(const Vec2 & p) const override
+  Vec2 add_disto(const Vec2 & p) const override
   {
     const double k1 = _distortionParams.at(0);
     const double r = std::hypot(p(0), p(1));
@@ -34,7 +38,7 @@ public:
   }
 
   /// Remove distortion (return p' such that disto(p') = p)
-  virtual Vec2 remove_disto(const Vec2& p) const override {
+  Vec2 remove_disto(const Vec2& p) const override {
     const double k1 = _distortionParams.at(0);
     const double r = std::hypot(p(0), p(1));
     const double coef = 0.5 * std::tan(r * k1) / (std::tan(0.5 * k1) * r);
