2f25de783655c012c1a8428007ac8de80e6a4c78
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-02 10:33:45 +0100

    [camera] explicit

--- src/aliceVision/camera/PinholeBrown.hpp
+++ src/aliceVision/camera/PinholeBrown.hpp
@@ -23,7 +23,7 @@ class PinholeBrownT2 : public Pinhole
 {
     public:
 
-    PinholeBrownT2(int w = 0, int h = 0, double focal = 0.0, double ppx = 0, double ppy = 0, double k1 = 0.0, double k2 = 0.0, double k3 = 0.0, double t1 = 0.0, double t2 = 0.0)
+    explicit PinholeBrownT2(int w = 0, int h = 0, double focal = 0.0, double ppx = 0, double ppy = 0, double k1 = 0.0, double k2 = 0.0, double k3 = 0.0, double t1 = 0.0, double t2 = 0.0)
     : Pinhole(w, h, focal, ppx, ppy, std::shared_ptr<Distortion>(new DistortionBrown(k1, k2, k3, t1, t2)))
     {
     }
