9de8b10f45cf8fcc3877ced550788f2ca920ee4f
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-31 23:17:51 +0200

    [hdr] reduced scope, preincrement in loop and Eigen::Index

--- src/aliceVision/hdr/QuadProg++.cc
+++ src/aliceVision/hdr/QuadProg++.cc
@@ -81,7 +81,7 @@ double solve_quadprog(Eigen::MatrixXd & G, Eigen::VectorXd & g0,
     throw std::logic_error(msg.str());
   }
   x.resize(n);
-  register unsigned int i, j, k, l; /* indices */
+
   int ip; // this is the index of the constraint to be added to the active set
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> R(n, n), J(n, n);
   Eigen::VectorXd s(m + p), z(n), r(m + p), d(n), np(n), u(m + p), x_old(n), u_old(m + p);
@@ -103,34 +103,38 @@ double solve_quadprog(Eigen::MatrixXd & G, Eigen::VectorXd & g0,
 	
   /* compute the trace of the original matrix G */
   c1 = 0.0;
-  for (i = 0; i < n; i++)
+  for(Eigen::Index i = 0; i < n; ++i)
   {
-    c1 += G(i,i);
+      c1 += G(i,i);
   }
   /* decompose the matrix G in the form L^T L */
   cholesky_decomposition(G);
 
   /* initialize the matrix R */
-  for (i = 0; i < n; i++)
+  for(Eigen::Index i = 0; i < n; ++i)
   {
-    d[i] = 0.0;
-    for (j = 0; j < n; j++)
-      R(i, j) = 0.0;
+      d[i] = 0.0;
+      for(Eigen::Index j = 0; j < n; ++j)
+      {
+          R(i, j) = 0.0;
+      }
   }
   R_norm = 1.0; /* this variable will hold the norm of the matrix R */
   
   /* compute the inverse of the factorized matrix G^-1, this is the initial value for H */
   c2 = 0.0;
-  for (i = 0; i < n; i++) 
+  for(Eigen::Index i = 0; i < n; ++i)
   {
-    d[i] = 1.0;
-    forward_elimination(G, z, d);
-    for (j = 0; j < n; j++)
-      J(i, j) = z[j];
-    c2 += z[i];
-    d[i] = 0.0;
+      d[i] = 1.0;
+      forward_elimination(G, z, d);
+      for(Eigen::Index j = 0; j < n; ++j)
+      {
+          J(i, j) = z[j];
+      }
+      c2 += z[i];
+      d[i] = 0.0;
   }
-  
+
   /* c1 * c2 is an estimate for cond(G) */
   
   /* 
@@ -139,18 +143,22 @@ double solve_quadprog(Eigen::MatrixXd & G, Eigen::VectorXd & g0,
    * x = G^-1 * g0
    */
   cholesky_solve(G, x, g0);
-  for (i = 0; i < n; i++)
-    x[i] = -x[i];
+  for(Eigen::Index i = 0; i < n; ++i)
+  {
+      x[i] = -x[i];
+  }
   /* and compute the current solution value */ 
   f_value = 0.5 * scalar_product(g0, x);
   
   /* Add equality constraints to the working set A */
   iq = 0;
   std::cout << p << std::endl;
-  for (i = 0; i < p; i++)
+  for(Eigen::Index i = 0; i < p; ++i)
   {
-    for (j = 0; j < n; j++)
-      np[j] = CE(j, i);
+      for(Eigen::Index j = 0; j < n; ++j)
+    {
+        np[j] = CE(j, i);
+    }
     compute_d(d, J, np);
     update_z(z, J, d, iq);
     update_r(R, r, d, iq);
@@ -162,13 +170,17 @@ double solve_quadprog(Eigen::MatrixXd & G, Eigen::VectorXd & g0,
       t2 = (-scalar_product(np, x) - ce0[i]) / scalar_product(z, np);
     
     /* set x = x + t2 * z */
-    for (k = 0; k < n; k++)
-      x[k] += t2 * z[k];
+      for(Eigen::Index k = 0; k < n; ++k)
+      {
+          x[k] += t2 * z[k];
+      }
     
     /* set u = u+ */
     u[iq] = t2;
-    for (k = 0; k < iq; k++)
-      u[k] -= t2 * r[k];
+    for(Eigen::Index k = 0; k < iq; ++k)
+    {
+        u[k] -= t2 * r[k];
+    }
     
     /* compute the new solution value */
     f_value += 0.5 * (t2 * t2) * scalar_product(z, np);
@@ -183,12 +195,14 @@ double solve_quadprog(Eigen::MatrixXd & G, Eigen::VectorXd & g0,
   }
   
   /* set iai = K \ A */
-  for (i = 0; i < m; i++)
-    iai[i] = i;
+    for(Eigen::Index i = 0; i < m; ++i)
+    {
+        iai[i] = i;
+    }
   
 l1:	iter++;
   /* step 1: choose a violated constraint */
-  for (i = p; i < iq; i++)
+  for(Eigen::Index i = p; i < iq; ++i)
   {
     ip = A[i];
     iai[ip] = -1;
@@ -198,12 +212,14 @@ l1:	iter++;
   ss = 0.0;
   psi = 0.0; /* this value will contain the sum of all infeasibilities */
   ip = 0; /* ip will be the index of the chosen violated constraint */
-  for (i = 0; i < m; i++)
+  for(Eigen::Index i = 0; i < m; ++i)
   {
     iaexcl[i] = true;
     sum = 0.0;
-    for (j = 0; j < n; j++)
-      sum += CI(j, i) * x[j];
+    for(Eigen::Index j = 0; j < n; ++j)
+    {
+        sum += CI(j, i) * x[j];
+    }
     sum += ci0[i];
     s[i] = sum;
     psi += std::min(0.0, sum);
@@ -217,17 +233,19 @@ l1:	iter++;
   }
   
   /* save old values for u and A */
-  for (i = 0; i < iq; i++)
+  for(Eigen::Index i = 0; i < iq; ++i)
   {
     u_old[i] = u[i];
     A_old[i] = A[i];
   }
   /* and for x */
-  for (i = 0; i < n; i++)
-    x_old[i] = x[i];
-  
+  for(Eigen::Index i = 0; i < n; ++i)
+  {
+      x_old[i] = x[i];
+  }
+
 l2: /* Step 2: check for feasibility and determine a new S-pair */
-    for (i = 0; i < m; i++)
+    for(Eigen::Index i = 0; i < m; ++i)
     {
       if (s[i] < ss && iai[i] != -1 && iaexcl[i])
       {
@@ -241,7 +259,7 @@ l2: /* Step 2: check for feasibility and determine a new S-pair */
   }
   
   /* set np = n[ip] */
-  for (i = 0; i < n; i++)
+  for(Eigen::Index i = 0; i < n; ++i)
     np[i] = CI(i, ip);
   /* set u = [u 0]^T */
   u[iq] = 0.0;
@@ -258,11 +276,11 @@ l2a:/* Step 2a: determine step direction */
 
   
   /* Step 2b: compute step length */
-  l = 0;
+  Eigen::Index l = 0;
   /* Compute t1: partial step length (maximum step in dual space without violating dual feasibility */
   t1 = inf; /* +inf */
   /* find the index l s.t. it reaches the minimum of u+[x] / r */
-  for (k = p; k < iq; k++)
+  for(Eigen::Index k = p; k < iq; ++k)
   {
     if (r[k] > 0.0)
     {
@@ -300,7 +318,7 @@ l2a:/* Step 2a: determine step direction */
   if (t2 >= inf)
   {
     /* set u = u +  t * [-r 1] and drop constraint l from the active set A */
-    for (k = 0; k < iq; k++)
+    for(Eigen::Index k = 0; k < iq; ++k)
       u[k] -= t * r[k];
     u[iq] += t;
     iai[l] = l;
@@ -312,12 +330,12 @@ l2a:/* Step 2a: determine step direction */
   /* case (iii): step in primal and dual space */
   
   /* set x = x + t * z */
-  for (k = 0; k < n; k++)
+  for(Eigen::Index k = 0; k < n; ++k)
     x[k] += t * z[k];
   /* update the solution value */
   f_value += t * scalar_product(z, np) * (0.5 * t + u[iq]);
   /* u = u + t * [-r 1] */
-  for (k = 0; k < iq; k++)
+  for(Eigen::Index k = 0; k < iq; ++k)
     u[k] -= t * r[k];
   u[iq] += t;
 
@@ -332,15 +350,15 @@ l2a:/* Step 2a: determine step direction */
       iaexcl[ip] = false;
       delete_constraint(R, J, A, u, n, p, iq, ip);
 
-      for (i = 0; i < m; i++)
+      for(Eigen::Index i = 0; i < m; ++i)
         iai[i] = i;
-      for (i = p; i < iq; i++)
+      for(Eigen::Index i = p; i < iq; ++i)
 	    {
 	      A[i] = A_old[i];
 	      u[i] = u_old[i];
 				iai[A[i]] = -1;
 	    }
-      for (i = 0; i < n; i++)
+      for(Eigen::Index i = 0; i < n; ++i)
         x[i] = x_old[i];
       goto l2; /* go to step 2 */
     }    
@@ -358,7 +376,7 @@ l2a:/* Step 2a: determine step direction */
   
   /* update s[ip] = CI * x + ci0 */
   sum = 0.0;
-  for (k = 0; k < n; k++)
+  for(Eigen::Index k = 0; k < n; ++k)
     sum += CI(k, ip) * x[k];
   s[ip] = sum + ci0[ip];
   
