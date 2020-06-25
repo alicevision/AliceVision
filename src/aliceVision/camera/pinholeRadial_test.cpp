bdcd18ec155429be73a965c941067af2259b2ad3
Fabien Castan <fabcastan@gmail.com>
2020-06-25 14:56:23 +0200

    [camera] add project/backproject in unittests

--- src/aliceVision/camera/pinholeRadial_test.cpp
+++ src/aliceVision/camera/pinholeRadial_test.cpp
@@ -35,15 +35,24 @@ BOOST_AUTO_TEST_CASE(cameraPinholeRadial_disto_undisto_K1) {
   for(int i = 0; i < 10; ++i)
   {
     // generate random point inside the image domain (last random to avoid 0,0)
-    const Vec2 ptImage = (Vec2::Random() * 800./2.) + Vec2(500,500) + Vec2::Random();
+    const Vec2 ptImage_gt = (Vec2::Random() * 800./2.) + Vec2(500,500) + Vec2::Random();
 
-    const Vec2 ptCamera = cam.ima2cam(ptImage);
+    const Vec2 ptCamera = cam.ima2cam(ptImage_gt);
     // Check that adding and removing distortion allow to recover the provided point
     EXPECT_MATRIX_NEAR( ptCamera, cam.removeDistortion(cam.addDistortion(ptCamera)), epsilon);
-    EXPECT_MATRIX_NEAR( ptImage, cam.cam2ima(cam.removeDistortion(cam.addDistortion(ptCamera))), epsilon);
+    EXPECT_MATRIX_NEAR( ptImage_gt, cam.cam2ima(cam.removeDistortion(cam.addDistortion(ptCamera))), epsilon);
 
     // Assert that distortion field is not null and it has moved the initial provided point
     BOOST_CHECK(! (cam.addDistortion(ptCamera) == cam.removeDistortion(cam.addDistortion(ptCamera))) ) ;
+
+    // Check projection / back-projection
+    const double depth_gt = Vec2::Random()(0) * 100.0;
+    const geometry::Pose3 pose(Mat3::Random(), Vec3::Random());
+
+    const Vec3 pt3d = cam.backproject(ptImage_gt, true, pose, depth_gt);
+    const Vec2 pt2d_proj = cam.project(pose, pt3d, true);
+
+    EXPECT_MATRIX_NEAR(ptImage_gt, pt2d_proj, epsilon);
   }
 }
 
@@ -68,14 +77,24 @@ BOOST_AUTO_TEST_CASE(cameraPinholeRadial_disto_undisto_K3) {
   for(int i = 0; i < 10; ++i)
   {
     // generate random point inside the image domain (last random to avoid 0,0)
-    const Vec2 ptImage = (Vec2::Random() * 800./2.) + Vec2(500,500) + Vec2::Random();
+    const Vec2 ptImage_gt = (Vec2::Random() * 800./2.) + Vec2(500,500) + Vec2::Random();
 
-    const Vec2 ptCamera = cam.ima2cam(ptImage);
+    const Vec2 ptCamera = cam.ima2cam(ptImage_gt);
     // Check that adding and removing distortion allow to recover the provided point
     EXPECT_MATRIX_NEAR( ptCamera, cam.removeDistortion(cam.addDistortion(ptCamera)), epsilon);
-    EXPECT_MATRIX_NEAR( ptImage, cam.cam2ima(cam.removeDistortion(cam.addDistortion(ptCamera))), epsilon);
+    EXPECT_MATRIX_NEAR( ptImage_gt, cam.cam2ima(cam.removeDistortion(cam.addDistortion(ptCamera))), epsilon);
 
     // Assert that distortion field is not null and it has moved the initial provided point
-    BOOST_CHECK(! (cam.addDistortion(ptCamera) == cam.removeDistortion(cam.addDistortion(ptCamera))) ) ;
+    BOOST_CHECK(! (cam.addDistortion(ptCamera) == cam.removeDistortion(cam.addDistortion(ptCamera))));
+
+    // Check projection / back-projection
+    const double depth_gt = Vec2::Random()(0) * 100.0;
+    const geometry::Pose3 pose(geometry::randomPose());
+
+    const Vec3 pt3d = cam.backproject(ptImage_gt, true, pose, depth_gt);
+    const Vec2 pt2d_proj = cam.project(pose, pt3d, true);
+
+    EXPECT_MATRIX_NEAR(ptImage_gt, pt2d_proj, epsilon);
+
   }
 }
