d43ed9703f71ea6c3fe577a96f434da3023cfb61
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2020-07-28 19:22:35 +0200

    [fuseCut] DelaunayGraphCut: fix the order during the insertion of camera's points

--- src/aliceVision/fuseCut/DelaunayGraphCut.cpp
+++ src/aliceVision/fuseCut/DelaunayGraphCut.cpp
@@ -2195,12 +2195,6 @@ void DelaunayGraphCut::createDensePointCloud(Point3d hexah[8], const StaticVecto
 
   float minDist = hexah ? (hexah[0] - hexah[1]).size() / 1000.0f : 0.00001f;
 
-  // add points for cam centers
-  addPointsFromCameraCenters(cams, minDist);
-
-  // add 6 points to prevent singularities
-  addPointsToPreventSingularities(hexah, minDist);
-
   // add points from depth maps
   if(depthMapsFuseParams != nullptr)
     fuseFromDepthMaps(cams, hexah, *depthMapsFuseParams);
@@ -2208,6 +2202,12 @@ void DelaunayGraphCut::createDensePointCloud(Point3d hexah[8], const StaticVecto
   // add points from sfm
   if(sfmData != nullptr)
     addPointsFromSfM(hexah, cams, *sfmData);
+
+  // add points for cam centers
+  addPointsFromCameraCenters(cams, minDist);
+
+  // add 6 points to prevent singularities
+  addPointsToPreventSingularities(hexah, minDist);
 
   const int nGridHelperVolumePointsDim = mp->userParams.get<int>("LargeScale.nGridHelperVolumePointsDim", 10);
 
