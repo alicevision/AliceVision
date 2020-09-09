1adce34c86e6f4d96ec06c06ac2b41b78d4bc7f8
Fabien Castan <fabcastan@gmail.com>
2020-09-09 15:27:10 +0200

    [docker] add log

--- docker/build-centos.sh
+++ docker/build-centos.sh
@@ -10,6 +10,10 @@ test -e docker/fetch.sh || {
 test -z "$AV_VERSION" && AV_VERSION="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
 test -z "$CUDA_VERSION" && CUDA_VERSION=10.2
 test -z "$CENTOS_VERSION" && CENTOS_VERSION=7
+ 
+echo "AV_VERSION: $AV_VERSION"
+echo "CUDA_VERSION: $CUDA_VERSION"
+echo "CENTOS_VERSION: $CENTOS_VERSION"
 
 docker/fetch.sh
 
