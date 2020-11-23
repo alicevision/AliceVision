6caaec42fa098873e095f30bd9bfe3e8910eb63f
Fabien Castan <fabcastan@gmail.com>
2020-11-23 14:52:02 +0100

    [voctree] ImageMatching: support all sift variations

--- src/aliceVision/voctree/descriptorLoader.cpp
+++ src/aliceVision/voctree/descriptorLoader.cpp
@@ -64,37 +64,39 @@ void getListOfDescriptorFiles(const sfmData::SfMData& sfmData, const std::vector
   if(sfmData.getViews().empty())
     throw std::runtime_error("Can't get list of descriptor files, no views found");
 
+  std::vector<feature::EImageDescriberType> descTypes = {feature::EImageDescriberType::SIFT,
+                                                         feature::EImageDescriberType::DSPSIFT,
+                                                         feature::EImageDescriberType::SIFT_FLOAT,
+                                                         feature::EImageDescriberType::SIFT_UPRIGHT};
+  std::vector<std::string> allFeaturesFolders(featuresFolders);
+  // add features folders from SfMData in search
+  for(const auto& folder: sfmData.getFeaturesFolders())
+  {
+    if(std::find(allFeaturesFolders.begin(), allFeaturesFolders.end(), folder) == allFeaturesFolders.end())
+      allFeaturesFolders.push_back(folder);
+  }
+
   // explore the sfm_data container to get the files path
   for(const auto& view : sfmData.getViews())
   {
     bool found = false;
 
-    for(const std::string& featureFolder : featuresFolders)
+    for(const std::string& featureFolder : allFeaturesFolders)
     {
-      // generate the equivalent .desc file path
-      const std::string filepath = bfs::path(bfs::path(featureFolder) / (std::to_string(view.first) + "." + feature::EImageDescriberType_enumToString(feature::EImageDescriberType::SIFT) + ".desc")).string();
-
-      if(bfs::exists(filepath))
+      for(const feature::EImageDescriberType descType: descTypes)
       {
-        descriptorsFiles[view.first] = filepath;
-        found = true;
-        break;
+        // generate the equivalent .desc file path
+        const std::string filepath = bfs::path(bfs::path(featureFolder) / (std::to_string(view.first) + "." + feature::EImageDescriberType_enumToString(descType) + ".desc")).string();
+
+        if(bfs::exists(filepath))
+        {
+          descriptorsFiles[view.first] = filepath;
+          found = true;
+          break;
+        }
       }
-    }
-
-    if(found)
-      continue;
-
-    for(const std::string& featureFolder : sfmData.getFeaturesFolders())
-    {
-      const std::string filepath = bfs::path(bfs::path(featureFolder) / (std::to_string(view.first) + "." + feature::EImageDescriberType_enumToString(feature::EImageDescriberType::SIFT) + ".desc")).string();
-
-      if(bfs::exists(filepath))
-      {
-        descriptorsFiles[view.first] = filepath;
-        found = true;
+      if(found)
         break;
-      }
     }
 
     if(!found)
