e0fe2e09edf035a50e29e8b42f9b5d262537916e
ludchieng <lud.chieng@hotmail.fr>
2021-01-11 19:31:52 +0300

    [software] colorCheckerDetection: update
    add basic printing in log file

--- src/software/utils/main_colorCheckerDetection.cpp
+++ src/software/utils/main_colorCheckerDetection.cpp
@@ -1,20 +1,25 @@
+// This file is part of the AliceVision project.
+// Copyright (c) 2017 AliceVision contributors.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <aliceVision/sfmData/SfMData.hpp>
+#include <aliceVision/sfmDataIO/sfmDataIO.hpp>
+
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
+#include <aliceVision/config.hpp>
 
 #include <dependencies/vectorGraphics/svgDrawer.hpp>
 
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
-#include <OpenImageIO/imageio.h>
-#include <OpenImageIO/imagebuf.h>
-#include <OpenImageIO/imagebufalgo.h>
-
-#include <opencv2/mcc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/mcc.hpp>
 
 #include <string>
-#include <iostream>
-#include <iterator>
 #include <fstream>
 #include <vector>
 
@@ -27,30 +32,96 @@ using namespace aliceVision;
 
 namespace fs = boost::filesystem;
 namespace po = boost::program_options;
-namespace oiio = OIIO;
 
 
 int aliceVision_main(int argc, char** argv)
 {
     // command-line parameters
     std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
-    std::string inputPath;                      // media file path list
-    std::string outputFolder;                   // output folder for splited images
-    std::string splitMode;                      // split mode (exif, dualfisheye, equirectangular)
-    std::string dualFisheyeSplitPreset;         // dual-fisheye split type preset
-    std::size_t equirectangularNbSplits;        // nb splits for equirectangular image
-    std::size_t equirectangularSplitResolution; // split resolution for equirectangular image
-    bool equirectangularDemoMode;
+    std::string sfmDataFilename;
+    std::string outputFolder;
 
-    std::cout << OPENCV_VERSION << std::endl;
+    // user optional parameters
+    bool debug = false;
 
     po::options_description allParams(
         "This program is used to perform color checker detection\n"
         "AliceVision colorCheckerDetection");
 
+    po::options_description inputParams("Required parameters");
+    inputParams.add_options()
+        ("input,i", po::value<std::string>(&sfmDataFilename)->required(),
+         "SfMData file.")
+        ("output,o", po::value<std::string>(&outputFolder)->required(),
+         "Output path for the color checker detection files (*.ccdata).");
+
+    po::options_description optionalParams("Optional parameters");
+    optionalParams.add_options()
+        ("debug", po::value<bool>(&debug),
+         "Output debug data.");
+
     po::options_description logParams("Log parameters");
-    logParams.add_options()("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
-                            "verbosity level (fatal,  error, warning, info, debug, trace).");
+    logParams.add_options()
+        ("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
+         "verbosity level (fatal, error, warning, info, debug, trace).");
+
+    allParams.add(inputParams).add(optionalParams).add(logParams);
+
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+        if(vm.count("help") || (argc == 1))
+        {
+            ALICEVISION_COUT(allParams);
+            return EXIT_SUCCESS;
+        }
+        po::notify(vm);
+    }
+    catch(boost::program_options::required_option& e)
+    {
+        ALICEVISION_CERR("ERROR: " << e.what());
+        ALICEVISION_COUT("Usage:\n\n" << allParams);
+        return EXIT_FAILURE;
+    }
+    catch(boost::program_options::error& e)
+    {
+        ALICEVISION_CERR("ERROR: " << e.what());
+        ALICEVISION_COUT("Usage:\n\n" << allParams);
+        return EXIT_FAILURE;
+    }
+
+    ALICEVISION_COUT("Program called with the following parameters:");
+    ALICEVISION_COUT(vm);
+
+    // set verbose level
+    system::Logger::get()->setLogLevel(verboseLevel);
+
+    // create output folder
+    if(!fs::exists(outputFolder))
+    {
+        if(!fs::create_directory(outputFolder))
+        {
+            ALICEVISION_LOG_ERROR("Cannot create output folder");
+            return EXIT_FAILURE;
+        }
+    }
+
+    // load input scene
+    sfmData::SfMData sfmData;
+    std::cout << sfmData.getViews().size()  << std::endl;
+    if(!sfmDataIO::Load(sfmData, sfmDataFilename, sfmDataIO::ESfMData(sfmDataIO::VIEWS|sfmDataIO::INTRINSICS)))
+    {
+        ALICEVISION_LOG_ERROR("The input file '" + sfmDataFilename + "' cannot be read");
+        return EXIT_FAILURE;
+    }
+
+    std::ofstream f;
+    f.open(outputFolder + "/outputDebug.txt");
+    f << "SfMData: " << sfmDataFilename << "\n";
+    f << "SfMData views: " << sfmData.getViews() << "\n";
+    f.close();
 
     return EXIT_SUCCESS;
 }
