eaa36200c0b5df52c8d1c4fd285c55ff4920e5a5
Jean Mélou <jean.melou@gmail.com>
2021-02-03 11:31:33 +0100

    [photometricStereo] Initial commit

---
+++ src/software/pipeline/main_photometricStereo.cpp
@@ -0,0 +1,33 @@
+#include <aliceVision/image/all.hpp>
+#include <aliceVision/image/io.hpp>
+#include <aliceVision/system/Logger.hpp>
+#include <aliceVision/system/cmdline.hpp>
+
+// SFMData
+#include <aliceVision/sfmData/SfMData.hpp>
+#include <aliceVision/sfmDataIO/sfmDataIO.hpp>
+
+// Command line parameters
+#include <aliceVision/system/main.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <array>
+#include <sstream>
+#include <fstream>
+
+// These constants define the current software version.
+// They must be updated when the command line is changed.
+#define ALICEVISION_SOFTWARE_VERSION_MAJOR 0
+#define ALICEVISION_SOFTWARE_VERSION_MINOR 1
+
+int aliceVision_main(int argc, char **argv)
+{
+
+    return 0;
+}
