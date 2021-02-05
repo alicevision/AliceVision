fb748fdde86c90c49d86b295162a0dc561aad2c7
Jean Mélou <jean.melou@gmail.com>
2021-02-05 16:36:49 +0100

    [photometricStereo] Add parameters

--- src/software/pipeline/main_photometricStereo.cpp
+++ src/software/pipeline/main_photometricStereo.cpp
@@ -1,7 +1,5 @@
 #include <aliceVision/image/all.hpp>
 #include <aliceVision/image/io.hpp>
-#include <aliceVision/system/Logger.hpp>
-#include <aliceVision/system/cmdline.hpp>
 
 // SFMData
 #include <aliceVision/sfmData/SfMData.hpp>
@@ -9,6 +7,9 @@
 
 // Command line parameters
 #include <aliceVision/system/main.hpp>
+#include <aliceVision/system/Logger.hpp>
+#include <aliceVision/system/cmdline.hpp>
+
 #include <boost/algorithm/string.hpp>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
@@ -28,6 +29,48 @@
 
 int aliceVision_main(int argc, char **argv)
 {
+    namespace po = boost::program_options;
+    namespace fs = boost::filesystem;
+
+    std::string dataFolder;
+    std::vector<int> usedPictures;
+
+    po::options_description allParams("AliceVision photometricStereo");
+    po::options_description requiredParams("Required parameters");
+    requiredParams.add_options()
+    ("dataFolder,d", po::value<std::string>(&dataFolder)->required(), "Data folder")
+    ("usedPictures,i", po::value<std::vector<int>>(&usedPictures)->required()->multitoken(), "usedPictures.");
+
+    allParams.add(requiredParams);
+
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+        if(vm.count("help") || (argc == 1))
+        {
+          ALICEVISION_COUT(allParams);
+          return EXIT_SUCCESS;
+        }
+        po::notify(vm);
+    }
+    catch(boost::program_options::required_option& e)
+    {
+      ALICEVISION_CERR("ERROR: " << e.what());
+      ALICEVISION_COUT("Usage:\n\n" << allParams);
+      return EXIT_FAILURE;
+    }
+    catch(boost::program_options::error& e)
+    {
+      ALICEVISION_CERR("ERROR: " << e.what());
+      ALICEVISION_COUT("Usage:\n\n" << allParams);
+      return EXIT_FAILURE;
+    }
+
+    ALICEVISION_COUT("Program called with the following parameters:");
+    ALICEVISION_COUT(vm);
+
 
     return 0;
 }
