c1dc5d80d8bf711ab86aaa444d96e1629b752214
Fabien <fabien.servant@interdigital.com>
2021-02-13 09:06:55 +0100

    [matching] implement cross check for feature matching

--- src/aliceVision/matchingImageCollection/matchingCommon.cpp
+++ src/aliceVision/matchingImageCollection/matchingCommon.cpp
@@ -17,17 +17,17 @@ namespace aliceVision {
 namespace matchingImageCollection {
   
 
-std::unique_ptr<IImageCollectionMatcher> createImageCollectionMatcher(matching::EMatcherType matcherType, float distRatio)
+std::unique_ptr<IImageCollectionMatcher> createImageCollectionMatcher(matching::EMatcherType matcherType, float distRatio, bool crossMatching)
 {
   std::unique_ptr<IImageCollectionMatcher> matcherPtr;
   
   switch(matcherType)
   {
-    case matching::BRUTE_FORCE_L2:          matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, matching::BRUTE_FORCE_L2)); break;
-    case matching::ANN_L2:                  matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, matching::ANN_L2)); break;
-    case matching::CASCADE_HASHING_L2:      matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, matching::CASCADE_HASHING_L2)); break;
+    case matching::BRUTE_FORCE_L2:          matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, crossMatching, matching::BRUTE_FORCE_L2)); break;
+    case matching::ANN_L2:                  matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, crossMatching, matching::ANN_L2)); break;
+    case matching::CASCADE_HASHING_L2:      matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, crossMatching, matching::CASCADE_HASHING_L2)); break;
     case matching::FAST_CASCADE_HASHING_L2: matcherPtr.reset(new ImageCollectionMatcher_cascadeHashing(distRatio)); break;
-    case matching::BRUTE_FORCE_HAMMING:     matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, matching::BRUTE_FORCE_HAMMING)); break;
+    case matching::BRUTE_FORCE_HAMMING:     matcherPtr.reset(new ImageCollectionMatcher_generic(distRatio, crossMatching, matching::BRUTE_FORCE_HAMMING)); break;
     
     default: throw std::out_of_range("Invalid matcherType enum");
   }
