cf591954303816b3e47105d35ab33062a64d6ea8
Fabien Castan <fabcastan@gmail.com>
2021-06-01 18:16:14 +0200

    [camera] fix copyrights on new files

--- src/aliceVision/camera/Pinhole3DE.hpp
+++ src/aliceVision/camera/Pinhole3DE.hpp
@@ -1,6 +1,5 @@
 // This file is part of the AliceVision project.
-// Copyright (c) 2016 AliceVision contributors.
-// Copyright (c) 2012 openMVG contributors.
+// Copyright (c) 2021 AliceVision contributors.
 // This Source Code Form is subject to the terms of the Mozilla Public License,
 // v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
