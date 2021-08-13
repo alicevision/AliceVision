d1e97737f94f21fea8ac42388d66152bf84d9c1c
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-08-13 17:09:17 +0200

    [convert] use assimp to convert mesh instead of geogram

--- src/software/convert/main_convertMesh.cpp
+++ src/software/convert/main_convertMesh.cpp
@@ -8,15 +8,11 @@
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
 #include <aliceVision/system/Timer.hpp>
+#include <aliceVision/mesh/Texturing.hpp>
+#include <aliceVision/mesh/Mesh.hpp>
 
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
-
-#include <geogram/mesh/mesh.h>
-#include <geogram/mesh/mesh_io.h>
-
-#include <geogram/basic/command_line.h>
-#include <geogram/basic/command_line_args.h>
 
 #include <iostream>
 #include <fstream>
@@ -47,6 +43,7 @@ int aliceVision_main(int argc, char** argv)
     std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
     std::string inputMeshPath;
     std::string outputFilePath;
+    aliceVision::mesh::EFileType outputMeshFileType;
 
     po::options_description allParams("AliceVision convertMesh\n"
                                       "The program allows to convert a mesh to another mesh format.");
@@ -59,6 +56,9 @@ int aliceVision_main(int argc, char** argv)
         "Output file path for the new mesh file (*.obj, *.mesh, *.meshb, *.ply, *.off, *.stl)");
 
     po::options_description optionalParams("Optional parameters");
+    optionalParams.add_options()
+        ("outputMeshFileType", po::value<aliceVision::mesh::EFileType>(&outputMeshFileType)->default_value(aliceVision::mesh::EFileType::GLTF),
+            "output mesh file type");
 
     po::options_description logParams("Log parameters");
     logParams.add_options()
@@ -126,30 +126,29 @@ int aliceVision_main(int argc, char** argv)
         }
     }
 
-    GEO::initialize();
-    GEO::CmdLine::import_arg_group("standard");
-    GEO::CmdLine::import_arg_group("algo");
-
-    ALICEVISION_LOG_INFO("Geogram initialized.");
-
-    GEO::Mesh inputMesh;
-
     // load input mesh
-    if(!GEO::mesh_load(inputMeshPath, inputMesh))
+    mesh::Texturing texturing;
+    texturing.loadOBJWithAtlas(inputMeshPath);
+    mesh::Mesh* inputMesh = texturing.mesh;
+
+    if(!inputMesh)
     {
-        ALICEVISION_LOG_ERROR("Failed to load mesh file: \"" << inputMeshPath << "\".");
+        ALICEVISION_LOG_ERROR("Unable to read input mesh from the file: " << inputMeshPath);
+        return EXIT_FAILURE;
+    }
+
+    if(inputMesh->pts.empty() || inputMesh->tris.empty())
+    {
+        ALICEVISION_LOG_ERROR("Error: empty mesh from the file " << inputMeshPath);
+        ALICEVISION_LOG_ERROR("Input mesh: " << inputMesh->pts.size() << " vertices and " << inputMesh->tris.size()
+                                             << " facets.");
         return EXIT_FAILURE;
     }
 
     // save output mesh
     ALICEVISION_LOG_INFO("Convert mesh.");
-    if(!GEO::mesh_save(inputMesh, outputFilePath))
-    {
-        ALICEVISION_LOG_ERROR("Failed to save mesh file: \"" << outputFilePath << "\".");
-        return EXIT_FAILURE;
-    }
+    inputMesh->save(outputFilePath, outputMeshFileType);
 
-    ALICEVISION_LOG_INFO("Mesh file: \"" << outputFilePath << "\" saved.");
     ALICEVISION_LOG_INFO("Task done in (s): " + std::to_string(timer.elapsed()));
 
     return EXIT_SUCCESS;
