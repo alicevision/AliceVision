9eae075874a26cdfeb9562cf341234a617984359
Povilas Kanapickas <povilas@radix.lt>
2022-07-22 03:51:14 +0300

    [software/pipeline] Use vfs in rigLocalization

--- src/software/pipeline/main_rigLocalization.cpp
+++ src/software/pipeline/main_rigLocalization.cpp
@@ -20,7 +20,6 @@
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
 
-#include <boost/filesystem.hpp>
 #include <boost/progress.hpp>
 #include <boost/program_options.hpp> 
 #include <boost/accumulators/accumulators.hpp>
@@ -49,7 +48,6 @@
 
 using namespace aliceVision;
 
-namespace bfs = boost::filesystem;
 namespace bacc = boost::accumulators;
 namespace po = boost::program_options;
 
@@ -339,7 +337,7 @@ int aliceVision_main(int argc, char** argv)
   cameraExporters.reserve(numCameras);
 
   // this contains the full path and the root name of the file without the extension
-  const std::string basename = (bfs::path(exportAlembicFile).parent_path() / bfs::path(exportAlembicFile).stem()).string();
+  const std::string basename = (vfs::path(exportAlembicFile).parent_path() / vfs::path(exportAlembicFile).stem()).string();
 
   for(std::size_t i = 0; i < numCameras; ++i)
   {
@@ -358,9 +356,9 @@ int aliceVision_main(int argc, char** argv)
     const std::string &feedPath = mediaPath[idCamera];
     // contains the folder where the video, the images or the filelist is
     subMediaFilepath[idCamera] = 
-        bfs::is_directory(bfs::path(mediaPath[idCamera])) ? 
+        fs.is_directory(vfs::path(mediaPath[idCamera])) ?
           (mediaPath[idCamera]) : 
-          (bfs::path(mediaPath[idCamera]).parent_path().string());
+          (vfs::path(mediaPath[idCamera]).parent_path().string());
 
     // create the feedProvider
     feeders[idCamera] = new dataio::FeedProvider(fs, feedPath, calibFile);
