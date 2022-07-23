aaae5a1e38162130a908a2b73890b9d30aec1f45
Povilas Kanapickas <povilas@radix.lt>
2022-07-23 21:15:44 +0300

    [software/pipeline] Use vfs in rigCalibration

--- src/software/pipeline/main_rigCalibration.cpp
+++ src/software/pipeline/main_rigCalibration.cpp
@@ -19,8 +19,8 @@
 #include <aliceVision/system/Logger.hpp>
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
+#include <aliceVision/vfs/filesystem.hpp>
 
-#include <boost/filesystem.hpp>
 #include <boost/progress.hpp>
 #include <boost/program_options.hpp> 
 #include <boost/accumulators/accumulators.hpp>
@@ -47,7 +47,6 @@
 
 using namespace aliceVision;
 
-namespace bfs = boost::filesystem;
 namespace bacc = boost::accumulators;
 namespace po = boost::program_options;
 
@@ -338,9 +337,9 @@ int aliceVision_main(int argc, char** argv)
     const std::string &feedPath = mediaPath[idCamera];
     // contains the folder where the video, the images or the filelist is
     const std::string subMediaFilepath = 
-        bfs::is_directory(bfs::path(mediaPath[idCamera])) ? 
+        vfs::is_directory(vfs::path(mediaPath[idCamera])) ?
           (mediaPath[idCamera]) : 
-          (bfs::path(mediaPath[idCamera]).parent_path().string());
+          (vfs::path(mediaPath[idCamera]).parent_path().string());
 
     // create the feedProvider
     dataio::FeedProvider feed(feedPath, calibFile);
