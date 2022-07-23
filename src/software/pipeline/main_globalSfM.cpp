5ac329ce2df437ffdb45fa0d194e1804b4d4f7d9
Povilas Kanapickas <povilas@radix.lt>
2022-07-23 21:15:26 +0300

    [software/pipeline] Use vfs in globalSfM

--- src/software/pipeline/main_globalSfM.cpp
+++ src/software/pipeline/main_globalSfM.cpp
@@ -14,9 +14,9 @@
 #include <aliceVision/system/Logger.hpp>
 #include <aliceVision/system/main.hpp>
 #include <aliceVision/system/cmdline.hpp>
+#include <aliceVision/vfs/filesystem.hpp>
 
 #include <boost/program_options.hpp>
-#include <boost/filesystem.hpp>
 
 #include <cstdlib>
 
@@ -28,7 +28,6 @@
 using namespace aliceVision;
 
 namespace po = boost::program_options;
-namespace fs = boost::filesystem;
 
 int aliceVision_main(int argc, char **argv)
 {
@@ -182,17 +181,17 @@ int aliceVision_main(int argc, char **argv)
   }
 
   if(extraInfoFolder.empty())
-    extraInfoFolder = fs::path(outSfMDataFilepath).parent_path().string();
+    extraInfoFolder = vfs::path(outSfMDataFilepath).parent_path().string();
 
-  if (!fs::exists(extraInfoFolder))
-    fs::create_directory(extraInfoFolder);
+  if (!vfs::exists(extraInfoFolder))
+    vfs::create_directory(extraInfoFolder);
 
   // global SfM reconstruction process
   aliceVision::system::Timer timer;
   sfm::ReconstructionEngine_globalSfM sfmEngine(
     sfmData,
     extraInfoFolder,
-    (fs::path(extraInfoFolder) / "sfm_log.html").string());
+    (vfs::path(extraInfoFolder) / "sfm_log.html").string());
 
   sfmEngine.initRandomSeed(randomSeed);
 
@@ -217,19 +216,19 @@ int aliceVision_main(int argc, char **argv)
   {
     sfmEngine.getSfMData().addFeaturesFolders(featuresFolders);
     sfmEngine.getSfMData().addMatchesFolders(matchesFolders);
-    sfmEngine.getSfMData().setAbsolutePath(fs::path(outSfMDataFilepath).parent_path().string());
+    sfmEngine.getSfMData().setAbsolutePath(vfs::path(outSfMDataFilepath).parent_path().string());
   }
 
   ALICEVISION_LOG_INFO("Global structure from motion took (s): " << timer.elapsed());
   ALICEVISION_LOG_INFO("Generating HTML report...");
 
-  sfm::generateSfMReport(sfmEngine.getSfMData(), (fs::path(extraInfoFolder) / "sfm_report.html").string());
+  sfm::generateSfMReport(sfmEngine.getSfMData(), (vfs::path(extraInfoFolder) / "sfm_report.html").string());
 
   // export to disk computed scene (data & visualizable results)
   ALICEVISION_LOG_INFO("Export SfMData to disk");
 
   sfmDataIO::Save(sfmEngine.getSfMData(), outSfMDataFilepath, sfmDataIO::ESfMData::ALL);
-  sfmDataIO::Save(sfmEngine.getSfMData(), (fs::path(extraInfoFolder) / "cloud_and_poses.ply").string(), sfmDataIO::ESfMData::ALL);
+  sfmDataIO::Save(sfmEngine.getSfMData(), (vfs::path(extraInfoFolder) / "cloud_and_poses.ply").string(), sfmDataIO::ESfMData::ALL);
 
   if(!outputSfMViewsAndPoses.empty())
     sfmDataIO:: Save(sfmEngine.getSfMData(), outputSfMViewsAndPoses, sfmDataIO::ESfMData(sfmDataIO::VIEWS|sfmDataIO::EXTRINSICS|sfmDataIO::INTRINSICS));
