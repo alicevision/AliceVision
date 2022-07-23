3a24bf0fab6af2c10d899e81b530cb05df8f69d8
Povilas Kanapickas <povilas@radix.lt>
2022-07-23 21:14:23 +0300

    [software/utils] Use vfs in keyframeSelection

--- src/software/utils/main_keyframeSelection.cpp
+++ src/software/utils/main_keyframeSelection.cpp
@@ -8,9 +8,9 @@
 #include <aliceVision/system/Logger.hpp>
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
+#include <aliceVision/vfs/filesystem.hpp>
 
 #include <boost/program_options.hpp> 
-#include <boost/filesystem.hpp>
 
 #include <string>
 #include <vector>
@@ -22,8 +22,8 @@
 
 using namespace aliceVision::keyframe;
 
+namespace vfs = aliceVision::vfs;
 namespace po = boost::program_options;
-namespace fs = boost::filesystem;
 
 int aliceVision_main(int argc, char** argv)
 {
@@ -138,9 +138,9 @@ int aliceVision_main(int argc, char** argv)
 
   // check output folder and update to its absolute path
   {
-    const fs::path outDir = fs::absolute(outputFolder);
+    const auto outDir = vfs::absolute(outputFolder);
     outputFolder = outDir.string();
-    if(!fs::is_directory(outDir))
+    if (!vfs::is_directory(outDir))
     {
       ALICEVISION_LOG_ERROR("Cannot find folder: " << outputFolder);
       return EXIT_FAILURE;
