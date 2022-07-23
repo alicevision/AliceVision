1f29b9a2cbf91bbad45511044115a87552c90ff8
Povilas Kanapickas <povilas@radix.lt>
2022-07-23 21:15:42 +0300

    [software/pipeline] Use vfs in panoramaWarping

--- src/software/pipeline/main_panoramaWarping.cpp
+++ src/software/pipeline/main_panoramaWarping.cpp
@@ -6,7 +6,6 @@
 
 // Reading command line options
 #include <boost/program_options.hpp>
-#include <boost/filesystem.hpp>
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
 
@@ -22,6 +21,7 @@
 #include <aliceVision/panorama/remapBbox.hpp>
 #include <aliceVision/panorama/warper.hpp>
 #include <aliceVision/panorama/distance.hpp>
+#include <aliceVision/vfs/filesystem.hpp>
 
 // These constants define the current software version.
 // They must be updated when the command line is changed.
@@ -31,7 +31,6 @@
 using namespace aliceVision;
 
 namespace po = boost::program_options;
-namespace fs = boost::filesystem;
 
 bool computeOptimalPanoramaSize(std::pair<int, int>& optimalSize, const sfmData::SfMData& sfmData, const float ratioUpscale)
 {
@@ -372,9 +371,9 @@ int aliceVision_main(int argc, char** argv)
 
 				// Define output paths
 				const std::string viewIdStr = std::to_string(view.getViewId());
-				const std::string viewFilepath = (fs::path(outputDirectory) / (viewIdStr + ".exr")).string();
-				const std::string maskFilepath = (fs::path(outputDirectory) / (viewIdStr + "_mask.exr")).string();
-				const std::string weightFilepath = (fs::path(outputDirectory) / (viewIdStr + "_weight.exr")).string();
+				const std::string viewFilepath = (vfs::path(outputDirectory) / (viewIdStr + ".exr")).string();
+				const std::string maskFilepath = (vfs::path(outputDirectory) / (viewIdStr + "_mask.exr")).string();
+				const std::string weightFilepath = (vfs::path(outputDirectory) / (viewIdStr + "_weight.exr")).string();
 
 				// Create output images
 				std::unique_ptr<oiio::ImageOutput> out_view = oiio::ImageOutput::create(viewFilepath);
