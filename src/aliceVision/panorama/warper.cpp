adff309dbbc8a9cd7aebdaef027738ba958b99f5
Fabien Servant <fabien.servant@technicolor.com>
2022-07-25 16:01:42 +0200

    [panorama] some optimizations for panorama warping

--- src/aliceVision/panorama/warper.cpp
+++ src/aliceVision/panorama/warper.cpp
@@ -14,7 +14,7 @@ bool Warper::warp(const CoordinatesMap& map, const aliceVision::image::Image<ima
     _mask = map.getMask();
 
     const image::Sampler2d<image::SamplerLinear> sampler;
-    const aliceVision::image::Image<Eigen::Vector2d>& coordinates = map.getCoordinates();
+    const aliceVision::image::Image<Eigen::Vector2f>& coordinates = map.getCoordinates();
 
     /**
      * Create buffer
@@ -36,7 +36,7 @@ bool Warper::warp(const CoordinatesMap& map, const aliceVision::image::Image<ima
                 continue;
             }
 
-            const Eigen::Vector2d& coord = coordinates(i, j);
+            const Eigen::Vector2f& coord = coordinates(i, j);
             const image::RGBfColor pixel = sampler(source, coord(1), coord(0));
 
             _color(i, j) = pixel;
@@ -57,13 +57,13 @@ bool GaussianWarper::warp(const CoordinatesMap& map, const GaussianPyramidNoMask
     _mask = map.getMask();
 
     const image::Sampler2d<image::SamplerLinear> sampler;
-    const aliceVision::image::Image<Eigen::Vector2d>& coordinates = map.getCoordinates();
+    const aliceVision::image::Image<Eigen::Vector2f>& coordinates = map.getCoordinates();
 
     /**
      * Create a pyramid for input
      */
     const std::vector<image::Image<image::RGBfColor>>& mlsource = pyramid.getPyramidColor();
-    size_t max_level = pyramid.getScalesCount() - 1;
+    int max_level = pyramid.getScalesCount() - 1;
 
     /**
      * Create buffer
@@ -76,6 +76,14 @@ bool GaussianWarper::warp(const CoordinatesMap& map, const GaussianPyramidNoMask
      */
     for(size_t i = 0; i < _color.Height(); i++)
     {
+        int next_i = i + 1;
+
+        if (i == _color.Height() - 1)
+        {
+            next_i = i - 1;
+        }
+
+
         for(size_t j = 0; j < _color.Width(); j++)
         {
 
@@ -86,45 +94,42 @@ bool GaussianWarper::warp(const CoordinatesMap& map, const GaussianPyramidNoMask
             }
 
             int next_j = j + 1;
-            int next_i = i + 1;
+            
 
             if (j == _color.Width() - 1)
             {
                 next_j = j - 1;
             }
 
-            if (i == _color.Height() - 1)
-            {
-                next_i = i - 1;
-            }
-
+            
             if (!_mask(next_i, j) || !_mask(i, next_j))
             {
-                const Eigen::Vector2d& coord = coordinates(i, j);
+                const Eigen::Vector2f& coord = coordinates(i, j);
                 const image::RGBfColor pixel = sampler(mlsource[0], coord(1), coord(0));
                 _color(i, j) = pixel;
 
                 continue;
             }
 
-            const Eigen::Vector2d& coord_mm = coordinates(i, j);
-            const Eigen::Vector2d& coord_mp = coordinates(i, next_j);
-            const Eigen::Vector2d& coord_pm = coordinates(next_i, j);
+            const Eigen::Vector2f& coord_mm = coordinates(i, j);
+            const Eigen::Vector2f& coord_mp = coordinates(i, next_j);
+            const Eigen::Vector2f& coord_pm = coordinates(next_i, j);
 
-            double dxx = coord_pm(0) - coord_mm(0);
-            double dxy = coord_mp(0) - coord_mm(0);
-            double dyx = coord_pm(1) - coord_mm(1);
-            double dyy = coord_mp(1) - coord_mm(1);
-            double det = std::abs(dxx * dyy - dxy * dyx);
-            double scale = sqrt(det);
+            float dxx = coord_pm(0) - coord_mm(0);
+            float dxy = coord_mp(0) - coord_mm(0);
+            float dyx = coord_pm(1) - coord_mm(1);
+            float dyy = coord_mp(1) - coord_mm(1);
+            float det = std::abs(dxx * dyy - dxy * dyx);
+            float scale = /*sqrtf*/(det); //logsqrt = 0.5log
 
-            double flevel = std::max(0.0, log2(scale));
-            size_t blevel = std::min(max_level, size_t(floor(flevel)));
+            float flevel = std::max(0.0f,0.5f * log2f(scale));
+            int blevel = std::min(max_level, int(floor(flevel)));
 
-            double dscale, x, y;
-            dscale = 1.0 / pow(2.0, blevel);
+            float dscale, x, y;
+            dscale = pow(2.0, -blevel);
             x = coord_mm(0) * dscale;
             y = coord_mm(1) * dscale;
+
             /*Fallback to first level if outside*/
             if(x >= mlsource[blevel].Width() - 1 || y >= mlsource[blevel].Height() - 1)
             {
@@ -133,6 +138,7 @@ bool GaussianWarper::warp(const CoordinatesMap& map, const GaussianPyramidNoMask
             }
 
             _color(i, j) = sampler(mlsource[blevel], y, x);
+            
             if (clamp)
             {
                 if (_color(i, j).r() > HALF_MAX) _color(i, j).r() = HALF_MAX;
