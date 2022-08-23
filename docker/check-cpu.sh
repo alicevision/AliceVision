85b9de57d566cc9581931d41e8dd2fbc37db8774
sanchayanghosh <sanchayanghosh@outlook.com>
2022-08-23 21:34:48 +0530

    #1202: Made changes to the memory per process requirement - to 2GB per process for both the dependencies and the actual package

--- docker/check-cpu.sh
+++ docker/check-cpu.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# This script is used to check the amount of available memory and restricts the number of concurrent jobs/processes (-j${CPU_CORES}) used
+# to 1 GB per process. If the number of processes are less than the amount of available memory (considering 2GB per core) we restrict the number
+# of cores to the total number of cores present on the system.
+
+
 FREE_MEM=$(free -g | grep Mem | awk '
 {
 print $7
@@ -8,9 +13,9 @@ print $7
 
 
 
-test ${FREE_MEM} -ge 1 || FREE_MEM=$((${FREE_MEM} + 1))
+test ${FREE_MEM} -ge 2 || FREE_MEM=$((${FREE_MEM} + 2))
 
 
 test ${FREE_MEM} -lt $(nproc) || FREE_MEM=$(nproc)
 
-echo ${FREE_MEM}
+echo $((${FREE_MEM}/2))
