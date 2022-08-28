ee97bc442970fff609a3cdd82d91d8acce639145
sanchayanghosh <86283153+sanchayanghosh@users.noreply.github.com>
2022-08-29 02:03:00 +0530

    Update docker/check-cpu.sh help string
    Co-authored-by: Fabien Castan <fabcastan@gmail.com>

--- docker/check-cpu.sh
+++ docker/check-cpu.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# This script is used to check the amount of available memory and restricts the number of concurrent jobs/processes (-j${CPU_CORES}) used
-# to 1 GB per process. If the number of processes are less than the amount of available memory (considering 2GB per core) we restrict the number
-# of cores to the total number of cores present on the system.
+# This script is used to check the amount of available memory to ensure 2 GB of memory per process.
+# If the number of processes is greater than the amount of available memory (considering 2 GB per core) we restrict the number of cores to use.
+#
+# Sample usage:
+# CPU_CORES=`check-cpu.sh`
+# make -j${CPU_CORES}
 
 
 FREE_MEM=$(free -g | grep Mem | awk '
