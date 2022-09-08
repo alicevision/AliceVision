c0cabbf26ec76c4450f4264047ebbcd0567f5658
Povilas Kanapickas <povilas@radix.lt>
2022-09-08 18:30:30 +0300

    [system] Implement ProgressDisplay wrapper for boost::progress_display
    This wrapper is intended to abstract the exact way progress is
    displayed. This will be useful later as a way to inject the exact
    progress display implementation depending on how aliceVision is being
    used. For example, if aliceVision is used as a library then it makes
    more sense to give progress information back to the caller instead of
    formatting and sending it to console.

---
+++ src/aliceVision/system/ProgressDisplay.cpp
@@ -0,0 +1,75 @@
+// This file is part of the AliceVision project.
+// Copyright (c) 2022 AliceVision contributors.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "ProgressDisplay.hpp"
+#include <boost/progress.hpp>
+
+namespace aliceVision {
+namespace system {
+
+ProgressDisplayImpl::~ProgressDisplayImpl() = default;
+
+class ProgressDisplayImplEmpty : public ProgressDisplayImpl {
+public:
+    void restart(unsigned long expectedCount) override {}
+    void increment(unsigned long count) override {}
+    unsigned long count() override { return 0; }
+    unsigned long expectedCount() override { return 0; }
+};
+
+ProgressDisplay::ProgressDisplay() : _impl{std::make_shared<ProgressDisplayImplEmpty>()}
+{}
+
+class ProgressDisplayImplBoostProgress : public ProgressDisplayImpl {
+public:
+    ProgressDisplayImplBoostProgress(unsigned long expectedCount,
+                                     std::ostream& os,
+                                     const std::string& s1,
+                                     const std::string& s2,
+                                     const std::string& s3) :
+        _display{expectedCount, os, s1, s2, s3}
+    {
+    }
+
+    ~ProgressDisplayImplBoostProgress() override = default;
+
+    void restart(unsigned long expectedCount) override
+    {
+        _display.restart(expectedCount);
+    }
+
+    void increment(unsigned long count) override
+    {
+        _display += count;
+    }
+
+    unsigned long count() override
+    {
+        return _display.count();
+    }
+
+    unsigned long expectedCount() override
+    {
+        return _display.expected_count();
+    }
+
+private:
+    boost::progress_display _display;
+};
+
+
+ProgressDisplay createConsoleProgressDisplay(unsigned long expectedCount,
+                                             std::ostream& os,
+                                             const std::string& s1,
+                                             const std::string& s2,
+                                             const std::string& s3)
+{
+    auto impl = std::make_shared<ProgressDisplayImplBoostProgress>(expectedCount, os, s1, s2, s3);
+    return ProgressDisplay(impl);
+}
+
+} // namespace system
+} // namespace aliceVision
