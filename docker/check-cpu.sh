13192635e76a380c7237fad362a40130e385080b
Fabien Castan <fabcastan@gmail.com>
2022-09-16 21:31:53 +0200

    [docker] Update check-cpu.sh

--- docker/check-cpu.sh
+++ docker/check-cpu.sh
@@ -8,17 +8,28 @@
 # make -j${CPU_CORES}
 
 
+NPROC=$(nproc)
+
 FREE_MEM=$(free -g | grep Mem | awk '
 {
 print $7
 }
 ' | sed "s/Gi//")
 
+# Arbitrary minimum of RAM per process
+MIN_RAM_PER_CORE=2
 
+# The number of processes we can run in parallel to guarantee at least N GB of RAM per process
+MAX_CORES_RAM=$((${FREE_MEM} / ${MIN_RAM_PER_CORE}))
 
-test ${FREE_MEM} -ge 2 || FREE_MEM=$((${FREE_MEM} + 2))
+# Maximum usable cores regarding the number of available cores and the amount of available memory
+USABLE_CORES=$((${MAX_CORES_RAM} < ${NPROC} ? ${MAX_CORES_RAM} : ${NPROC}))
 
+# echo "NPROC=${NPROC}"
+# echo "FREE_MEM=${FREE_MEM}"
+# echo "MIN_RAM_PER_CORE=${MIN_RAM_PER_CORE}"
+# echo "MAX_CORES_RAM=${MAX_CORES_RAM}"
+# echo " => USABLE_CORES=${USABLE_CORES}"
+# echo ""
 
-test ${FREE_MEM} -lt $(nproc) || FREE_MEM=$(nproc)
-
-echo $((${FREE_MEM}/2))
+echo ${USABLE_CORES}
