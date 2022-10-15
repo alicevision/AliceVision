f4bd823465aa34281cc091318ae4c9687c270712
Povilas Kanapickas <povilas@radix.lt>
2022-10-15 10:53:43 +0300

    [image] Use build-time ALICEVISION_ROOT as fallback to env variable
    This still allows to override ALICEVISION_ROOT, but will avoid the need
    to set the environment variable in a cases when the prefix is known
    beforehand.

--- src/aliceVision/image/io.cpp
+++ src/aliceVision/image/io.cpp
@@ -18,6 +18,7 @@
 
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/preprocessor/stringize.hpp>
 
 #include <cstring>
 #include <stdexcept>
@@ -1125,7 +1126,13 @@ std::string getAliceVisionRoot()
     if (!aliceVisionRootOverride.empty())
         return aliceVisionRootOverride;
     const char* value = std::getenv("ALICEVISION_ROOT");
-    return value ? value : "";
+    if (value)
+        return value;
+#ifdef ALICEVISION_DEFAULT_ROOT
+    return BOOST_PP_STRINGIZE(ALICEVISION_DEFAULT_ROOT);
+#else
+    return "";
+#endif
 }
 
 std::string getAliceVisionOCIOConfig()
