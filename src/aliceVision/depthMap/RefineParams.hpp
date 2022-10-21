8d6db21327d1b2231dabcf981ad16e0eba860455
Grégoire De Lillo <gregoire-dl@users.noreply.github.com>
2022-10-21 11:00:00 +0200

    [depthMap] RefineParams: Disable useNormalMap option

--- src/aliceVision/depthMap/RefineParams.hpp
+++ src/aliceVision/depthMap/RefineParams.hpp
@@ -26,7 +26,7 @@ struct RefineParams
   double sigma = 15.0;
   double gammaC = 15.5;
   double gammaP = 8.0;
-  bool useNormalMap = true;
+  bool useNormalMap = false;
   bool doRefineFuse = true;
   bool doRefineOptimization = true;
   bool exportIntermediateResults = false;
