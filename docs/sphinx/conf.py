86e9bf0121f8136fad739f9b6713d72cf1b051cb
Loïc Vital <mugulmotion@gmail.com>
2022-11-14 12:19:24 +0100

    [docs] minor corrections for RTD build

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -20,6 +20,8 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index'
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
