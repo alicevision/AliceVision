4df63296590fb194014bde7b1e57d258a8b16ae2
Candice Bentéjac <candice.bentejac@gmail.com>
2022-11-17 18:43:32 +0100

    [keyframe] Fix typos in KeyframeSelector's docstrings

--- src/aliceVision/keyframe/KeyframeSelector.hpp
+++ src/aliceVision/keyframe/KeyframeSelector.hpp
@@ -27,29 +27,29 @@ class KeyframeSelector
 public:    
 
   /**
-   * @brief Process media paths and build a list of selected frames
-   * @param mediaPaths for each camera, give the media file name (path)
+   * @brief Process media paths and build a list of selected frames using a smart method based on optical flow estimation
+   * @param[in] mediaPaths for each camera, give the media file name (path)
    */
   void processSmart(const std::vector<std::string>& mediaPaths);
 
   /**
- * @brief build a list of selected frames
- * @param mediaPaths for each camera, give the media file name (path)
- */
+   * @brief Process media paths and build a list of selected frames using a regular sampling over time
+   * @param[in] mediaPaths for each camera, give the media file name (path)
+   */
   void processSimple(const std::vector<std::string>& mediaPaths);
 
   /**
-  * @Brief write selected frames indices
-  * @param outputFOlder folder for output images
-  * @param mediaPaths for each camera, give the media file name (path)
-  * @param brands for each camera, give the brand name
-  * @param bmodelsrands for each camera, give the models name
-  * @param mmFocals for each camera, give the focal in mm
-  */
+   * @brief Write selected frames indices
+   * @param[in] outputFolder folder for output images
+   * @param[in] mediaPaths for each camera, give the media file name (path)
+   * @param[in] brands for each camera, give the brand name
+   * @param[in] models for each camera, give the models name
+   * @param[in] mmFocals for each camera, give the focal in mm
+   */
   bool writeSelection(const std::string& outputFolder, const std::vector<std::string>& mediaPaths, const std::vector<std::string>& brands, const std::vector<std::string>& models, const std::vector<float>& mmFocals);
 
   /**
-   * @brief Set min frame step for process algorithm
+   * @brief Set the mininum frame step for the processing algorithm
    * @param[in] frameStep minimum number of frames between two keyframes
    */
   void setMinFrameStep(unsigned int frameStep)
@@ -58,7 +58,7 @@ public:
   }
 
   /**
-   * @brief Set max output frame number for process algorithm
+   * @brief Set the maximum number of output frames for the processing algorithm
    * @param[in] nbFrame maximum number of output frames (if 0, no limit)
    */
   void setMaxOutFrame(unsigned int nbFrame)
@@ -67,15 +67,16 @@ public:
   }
 
   /**
-   * @brief Get min frame step for process algorithm
+   * @brief Get the minimum frame step for the processing algorithm
    * @return minimum number of frames between two keyframes
    */
   unsigned int getMinFrameStep() const 
   {
       return _minFrameStep;
   }
+
   /**
-   * @brief Get max output frame number for process algorithm
+   * @brief Get the maximum number of output frames for the processing algorithm
    * @return maximum number of output frames (if 0, no limit)
    */
   unsigned int getMaxOutFrame() const 
@@ -85,7 +86,6 @@ public:
     
 private:
 
-  // Paths
   /// Minimum number of frame between two keyframes
   unsigned int _minFrameStep = 12;
   /// Maximum number of output frame (0 = no limit)
