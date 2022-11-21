59325429656cb2d0cb6eb5c20395ef386322dbc6
Grégoire De Lillo <gregoire-dl@users.noreply.github.com>
2022-11-21 09:21:59 +0000

    [depthMap] deviceColorConversion: Remove pragma once from cu file

--- src/aliceVision/depthMap/cuda/imageProcessing/deviceColorConversion.cu
+++ src/aliceVision/depthMap/cuda/imageProcessing/deviceColorConversion.cu
@@ -4,8 +4,6 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#pragma once
-
 #include "deviceColorConversion.hpp"
 
 #include <aliceVision/depthMap/cuda/host/hostUtils.hpp>
