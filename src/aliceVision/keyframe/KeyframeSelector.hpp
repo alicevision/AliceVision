fdbd56757afd8516af54f7ed10ca7ded98b2e16c
Candice Bentéjac <candice.bentejac@gmail.com>
2022-12-06 18:41:52 +0100

    [keyframe] Align the header's indentation with the source's
    Use the same indentation (4 spaces) for both the KeyframeSelector
    header and source files.

--- src/aliceVision/keyframe/KeyframeSelector.hpp
+++ src/aliceVision/keyframe/KeyframeSelector.hpp
@@ -26,73 +26,73 @@ class KeyframeSelector
 {
 public:    
 
-  /**
-   * @brief Process media paths and build a list of selected frames using a smart method based on optical flow estimation
-   * @param[in] mediaPaths for each camera, give the media file name (path)
-   */
-  void processSmart(const std::vector<std::string>& mediaPaths);
+    /**
+     * @brief Process media paths and build a list of selected frames using a smart method based on optical flow estimation
+     * @param[in] mediaPaths for each camera, give the media file name (path)
+     */
+    void processSmart(const std::vector<std::string>& mediaPaths);
 
-  /**
-   * @brief Process media paths and build a list of selected frames using a regular sampling over time
-   * @param[in] mediaPaths for each camera, give the media file name (path)
-   */
-  void processRegular(const std::vector<std::string>& mediaPaths);
+    /**
+     * @brief Process media paths and build a list of selected frames using a regular sampling over time
+     * @param[in] mediaPaths for each camera, give the media file name (path)
+     */
+    void processRegular(const std::vector<std::string>& mediaPaths);
 
-  /**
-   * @brief Write selected frames indices
-   * @param[in] outputFolder folder for output images
-   * @param[in] mediaPaths for each camera, give the media file name (path)
-   * @param[in] brands for each camera, give the brand name
-   * @param[in] models for each camera, give the models name
-   * @param[in] mmFocals for each camera, give the focal in mm
-   */
-  bool writeSelection(const std::string& outputFolder, const std::vector<std::string>& mediaPaths, const std::vector<std::string>& brands, const std::vector<std::string>& models, const std::vector<float>& mmFocals);
+    /**
+     * @brief Write selected frames indices
+     * @param[in] outputFolder folder for output images
+     * @param[in] mediaPaths for each camera, give the media file name (path)
+     * @param[in] brands for each camera, give the brand name
+     * @param[in] models for each camera, give the models name
+     * @param[in] mmFocals for each camera, give the focal in mm
+     */
+    bool writeSelection(const std::string& outputFolder, const std::vector<std::string>& mediaPaths, const std::vector<std::string>& brands, const std::vector<std::string>& models, const std::vector<float>& mmFocals);
 
-  /**
-   * @brief Set the mininum frame step for the processing algorithm
-   * @param[in] frameStep minimum number of frames between two keyframes
-   */
-  void setMinFrameStep(unsigned int frameStep)
-  {
-      _minFrameStep = frameStep;
-  }
+    /**
+     * @brief Set the mininum frame step for the processing algorithm
+     * @param[in] frameStep minimum number of frames between two keyframes
+     */
+    void setMinFrameStep(unsigned int frameStep)
+    {
+        _minFrameStep = frameStep;
+    }
 
-  /**
-   * @brief Set the maximum number of output frames for the processing algorithm
-   * @param[in] nbFrame maximum number of output frames (if 0, no limit)
-   */
-  void setMaxOutFrame(unsigned int nbFrame)
-  {
-      _maxOutFrame = nbFrame;
-  }
+    /**
+     * @brief Set the maximum number of output frames for the processing algorithm
+     * @param[in] nbFrame maximum number of output frames (if 0, no limit)
+     */
+    void setMaxOutFrame(unsigned int nbFrame)
+    {
+        _maxOutFrame = nbFrame;
+    }
 
-  /**
-   * @brief Get the minimum frame step for the processing algorithm
-   * @return minimum number of frames between two keyframes
-   */
-  unsigned int getMinFrameStep() const 
-  {
-      return _minFrameStep;
-  }
+    /**
+     * @brief Get the minimum frame step for the processing algorithm
+     * @return minimum number of frames between two keyframes
+     */
+    unsigned int getMinFrameStep() const
+    {
+        return _minFrameStep;
+    }
 
-  /**
-   * @brief Get the maximum number of output frames for the processing algorithm
-   * @return maximum number of output frames (if 0, no limit)
-   */
-  unsigned int getMaxOutFrame() const 
-  {
-      return _maxOutFrame;
-  }
-    
+    /**
+     * @brief Get the maximum number of output frames for the processing algorithm
+     * @return maximum number of output frames (if 0, no limit)
+     */
+    unsigned int getMaxOutFrame() const
+    {
+        return _maxOutFrame;
+    }
+
 private:
 
-  /// Minimum number of frame between two keyframes
-  unsigned int _minFrameStep = 12;
-  /// Maximum number of output frame (0 = no limit)
-  unsigned int _maxOutFrame = 0;
+    /// Minimum number of frame between two keyframes
+    unsigned int _minFrameStep = 12;
+    /// Maximum number of output frame (0 = no limit)
+    unsigned int _maxOutFrame = 0;
 
-  /// List of selected frames
-  std::vector<unsigned int> _selected;
+    /// List of selected frames
+    std::vector<unsigned int> _selected;
 };
 
 } // namespace keyframe 
