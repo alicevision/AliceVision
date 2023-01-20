454635919c9f73fba34cc8c4b26d58cd4883a2d2
Candice Bentéjac <candice.bentejac@gmail.com>
2023-01-20 20:28:30 +0100

    KeyframeSelection: Harmonize indentation across files
    Use the same indentation (4 spaces) in main_keyframeSelection.hpp and
    KeyframeSelector.hpp as in KeyframeSelector.cpp.

--- src/aliceVision/keyframe/KeyframeSelector.hpp
+++ src/aliceVision/keyframe/KeyframeSelector.hpp
@@ -30,109 +30,109 @@ namespace oiio = OIIO;
 class KeyframeSelector
 {
 public:
-  /**
-   * @brief KeyframeSelector constructor
-   * @param[in] mediaPath video file path or image sequence directory
-   * @param[in] sensorDbPath camera sensor width database path
-   * @param[in] outputFolder output keyframes directory
-   */
-  KeyframeSelector(const std::vector<std::string>& mediaPaths,
-                   const std::string& sensorDbPath,
-                   const std::string& outputFolder);
+    /**
+     * @brief KeyframeSelector constructor
+     * @param[in] mediaPath video file path or image sequence directory
+     * @param[in] sensorDbPath camera sensor width database path
+     * @param[in] outputFolder output keyframes directory
+     */
+    KeyframeSelector(const std::vector<std::string>& mediaPaths,
+                    const std::string& sensorDbPath,
+                    const std::string& outputFolder);
 
-  /**
-   * @brief KeyframeSelector copy constructor - NO COPY
-   * @param[in] copy keyframeSelector
-   */
-  KeyframeSelector(const KeyframeSelector& copy) = delete;
+    /**
+     * @brief KeyframeSelector copy constructor - NO COPY
+     * @param[in] copy keyframeSelector
+     */
+    KeyframeSelector(const KeyframeSelector& copy) = delete;
 
-  /**
-   * @brief Process media paths and build a list of selected keyframes using a regular sampling over time
-   */
-  void processRegular();
+    /**
+     * @brief Process media paths and build a list of selected keyframes using a regular sampling over time
+     */
+    void processRegular();
 
-  /**
-   * @brief Write the selected keyframes in the output folder
-   * @param[in] brands brand name for each camera
-   * @param[in] models model name for each camera
-   * @param[in] mmFocals focal in millimeters for each camera
-   * @return true if all the selected keyframes were successfully written, false otherwise
-   */
-  bool writeSelection(const std::vector<std::string>& brands, const std::vector<std::string>& models,
-                 const std::vector<float>& mmFocals) const;
+    /**
+     * @brief Write the selected keyframes in the output folder
+     * @param[in] brands brand name for each camera
+     * @param[in] models model name for each camera
+     * @param[in] mmFocals focal in millimeters for each camera
+     * @return true if all the selected keyframes were successfully written, false otherwise
+     */
+    bool writeSelection(const std::vector<std::string>& brands, const std::vector<std::string>& models,
+                    const std::vector<float>& mmFocals) const;
 
-  /**
-   * @brief Set the minimum frame step parameter for the processing algorithm
-   * @param[in] frameStep minimum number of frames between two keyframes
-   */
-  void setMinFrameStep(unsigned int frameStep)
-  {
-      _minFrameStep = frameStep;
-  }
+    /**
+     * @brief Set the minimum frame step parameter for the processing algorithm
+     * @param[in] frameStep minimum number of frames between two keyframes
+     */
+    void setMinFrameStep(unsigned int frameStep)
+    {
+        _minFrameStep = frameStep;
+    }
 
-  /**
-   * @brief Set the maximum frame step parameter for the processing algorithm
-   * @param[in] frameStep maximum number of frames between two keyframes
-   */
-  void setMaxFrameStep(unsigned int frameStep)
-  {
-      _maxFrameStep = frameStep;
-  }
+    /**
+     * @brief Set the maximum frame step parameter for the processing algorithm
+     * @param[in] frameStep maximum number of frames between two keyframes
+     */
+    void setMaxFrameStep(unsigned int frameStep)
+    {
+        _maxFrameStep = frameStep;
+    }
 
-  /**
-   * @brief Set the maximum output frame number parameter for the processing algorithm
-   * @param[in] nbFrame maximum number of output frames (if 0, no limit)
-   */
-  void setMaxOutFrame(unsigned int nbFrame)
-  {
-      _maxOutFrame = nbFrame;
-  }
+    /**
+     * @brief Set the maximum output frame number parameter for the processing algorithm
+     * @param[in] nbFrame maximum number of output frames (if 0, no limit)
+     */
+    void setMaxOutFrame(unsigned int nbFrame)
+    {
+        _maxOutFrame = nbFrame;
+    }
 
-  /**
-   * @brief Get the minimum frame step parameter for the processing algorithm
-   * @return minimum number of frames between two keyframes
-   */
-  unsigned int getMinFrameStep() const 
-  {
-      return _minFrameStep;
-  }
+    /**
+     * @brief Get the minimum frame step parameter for the processing algorithm
+     * @return minimum number of frames between two keyframes
+     */
+    unsigned int getMinFrameStep() const
+    {
+        return _minFrameStep;
+    }
 
-  /**
-   * @brief Get the maximum output frame number parameter for the processing algorithm
-   * @return maximum number of frames between two keyframes
-   */
-  unsigned int getMaxFrameStep() const 
-  {
-      return _maxFrameStep;
-  }
+    /**
+     * @brief Get the maximum output frame number parameter for the processing algorithm
+     * @return maximum number of frames between two keyframes
+     */
+    unsigned int getMaxFrameStep() const
+    {
+        return _maxFrameStep;
+    }
 
-  /**
-   * @brief Get the max output frame number for process algorithm
-   * @return maximum number of output frames (if 0, no limit)
-   */
-  unsigned int getMaxOutFrame() const 
-  {
-      return _maxOutFrame;
-  }
+    /**
+     * @brief Get the max output frame number for process algorithm
+     * @return maximum number of output frames (if 0, no limit)
+     */
+    unsigned int getMaxOutFrame() const
+    {
+        return _maxOutFrame;
+    }
     
 private:
-  /// Selected keyframes IDs
-  std::vector<unsigned int> _selectedKeyframes;
+    /// Selected keyframes IDs
+    std::vector<unsigned int> _selectedKeyframes;
 
-  /// Media paths
-  std::vector<std::string> _mediaPaths;
-  /// Camera sensor width database
-  std::string _sensorDbPath;
-  /// Output folder for keyframes
-  std::string _outputFolder;
+    /// Media paths
+    std::vector<std::string> _mediaPaths;
+    /// Camera sensor width database
+    std::string _sensorDbPath;
+    /// Output folder for keyframes
+    std::string _outputFolder;
 
 
-  /// Minimum number of frames between two keyframes
-  unsigned int _minFrameStep = 12;
-  /// Maximum number of frames between two keyframes
-  unsigned int _maxFrameStep = 36;
-  /// Maximum number of output frames (0 = no limit)
-  unsigned int _maxOutFrame = 0;
+    /// Minimum number of frames between two keyframes
+    unsigned int _minFrameStep = 12;
+    /// Maximum number of frames between two keyframes
+    unsigned int _maxFrameStep = 36;
+    /// Maximum number of output frames (0 = no limit)
+    unsigned int _maxOutFrame = 0;
 };
 
 } // namespace keyframe 
--- src/software/utils/main_keyframeSelection.cpp
+++ src/software/utils/main_keyframeSelection.cpp
@@ -27,112 +27,112 @@ namespace fs = boost::filesystem;
 
 int aliceVision_main(int argc, char** argv)
 {
-  // Command-line parameters
-  std::vector<std::string> mediaPaths;    // media file path list
-  std::vector<std::string> brands;        // media brand list
-  std::vector<std::string> models;        // media model list
-  std::vector<float> mmFocals;            // media focal (mm) list
-  std::string sensorDbPath;               // camera sensor width database
-  std::string outputFolder;               // output folder for keyframes
+    // Command-line parameters
+    std::vector<std::string> mediaPaths;    // media file path list
+    std::vector<std::string> brands;        // media brand list
+    std::vector<std::string> models;        // media model list
+    std::vector<float> mmFocals;            // media focal (mm) list
+    std::string sensorDbPath;               // camera sensor width database
+    std::string outputFolder;               // output folder for keyframes
 
-  // Algorithm variables
-  unsigned int minFrameStep = 12;
-  unsigned int maxFrameStep = 36;
-  unsigned int maxNbOutFrame = 0;
+    // Algorithm variables
+    unsigned int minFrameStep = 12;
+    unsigned int maxFrameStep = 36;
+    unsigned int maxNbOutFrame = 0;
 
-  po::options_description inputParams("Required parameters");  
-  inputParams.add_options()
-      ("mediaPaths", po::value<std::vector<std::string>>(&mediaPaths)->required()->multitoken(),
-        "Input video files or image sequence directories.")
-      ("sensorDbPath", po::value<std::string>(&sensorDbPath)->required(),
-        "Camera sensor width database path.")
-      ("outputFolder", po::value<std::string>(&outputFolder)->required(),
-        "Output folder in which the selected keyframes are written.");
+    po::options_description inputParams("Required parameters");
+    inputParams.add_options()
+        ("mediaPaths", po::value<std::vector<std::string>>(&mediaPaths)->required()->multitoken(),
+            "Input video files or image sequence directories.")
+        ("sensorDbPath", po::value<std::string>(&sensorDbPath)->required(),
+            "Camera sensor width database path.")
+        ("outputFolder", po::value<std::string>(&outputFolder)->required(),
+            "Output folder in which the selected keyframes are written.");
 
-  po::options_description metadataParams("Metadata parameters");  
-  metadataParams.add_options()
-      ("brands", po::value<std::vector<std::string>>(&brands)->default_value(brands)->multitoken(),
-        "Camera brands.")
-      ("models", po::value<std::vector<std::string>>(&models)->default_value(models)->multitoken(),
-        "Camera models.")
-      ("mmFocals", po::value<std::vector<float>>(&mmFocals)->default_value(mmFocals)->multitoken(),
-        "Focals in mm (ignored if equal to 0).");
-  
-  po::options_description algorithmParams("Algorithm parameters");
-  algorithmParams.add_options()
-      ("minFrameStep", po::value<unsigned int>(&minFrameStep)->default_value(minFrameStep), 
-        "Minimum number of frames between two keyframes.")
-      ("maxFrameStep", po::value<unsigned int>(&maxFrameStep)->default_value(maxFrameStep), 
-        "Maximum number of frames after which a keyframe can be taken (ignored if equal to 0).")
-      ("maxNbOutFrame", po::value<unsigned int>(&maxNbOutFrame)->default_value(maxNbOutFrame), 
-        "Maximum number of output keyframes (0 = no limit).\n"
-        "'minFrameStep' and 'maxFrameStep' will always be respected, so combining them with this "
-        "parameter might cause the selection to stop before reaching the end of the input sequence(s).");
+    po::options_description metadataParams("Metadata parameters");
+    metadataParams.add_options()
+        ("brands", po::value<std::vector<std::string>>(&brands)->default_value(brands)->multitoken(),
+            "Camera brands.")
+        ("models", po::value<std::vector<std::string>>(&models)->default_value(models)->multitoken(),
+            "Camera models.")
+        ("mmFocals", po::value<std::vector<float>>(&mmFocals)->default_value(mmFocals)->multitoken(),
+            "Focals in mm (ignored if equal to 0).");
+
+    po::options_description algorithmParams("Algorithm parameters");
+    algorithmParams.add_options()
+        ("minFrameStep", po::value<unsigned int>(&minFrameStep)->default_value(minFrameStep),
+            "Minimum number of frames between two keyframes.")
+        ("maxFrameStep", po::value<unsigned int>(&maxFrameStep)->default_value(maxFrameStep),
+            "Maximum number of frames after which a keyframe can be taken (ignored if equal to 0).")
+        ("maxNbOutFrame", po::value<unsigned int>(&maxNbOutFrame)->default_value(maxNbOutFrame),
+            "Maximum number of output keyframes (0 = no limit).\n"
+            "'minFrameStep' and 'maxFrameStep' will always be respected, so combining them with this "
+            "parameter might cause the selection to stop before reaching the end of the input sequence(s).");
 
 
-  aliceVision::CmdLine cmdline("This program is used to extract keyframes from single camera or a camera rig.\n"
-                               "AliceVision keyframeSelection");
-  cmdline.add(inputParams);
-  cmdline.add(metadataParams);
-  cmdline.add(algorithmParams);
-  if (!cmdline.execute(argc, argv)) {
-      return EXIT_FAILURE;
-  }
-
-  const std::size_t nbCameras = mediaPaths.size();
-
-  // Check output folder and update to its absolute path
-  {
-    const fs::path outDir = fs::absolute(outputFolder);
-    outputFolder = outDir.string();
-    if (!fs::is_directory(outDir)) {
-      ALICEVISION_LOG_ERROR("Cannot find folder: " << outputFolder);
-      return EXIT_FAILURE;
+    aliceVision::CmdLine cmdline("This program is used to extract keyframes from single camera or a camera rig.\n"
+                                "AliceVision keyframeSelection");
+    cmdline.add(inputParams);
+    cmdline.add(metadataParams);
+    cmdline.add(algorithmParams);
+    if (!cmdline.execute(argc, argv)) {
+        return EXIT_FAILURE;
     }
-  }
 
-  if (nbCameras < 1) {
-    ALICEVISION_LOG_ERROR("Program needs at least one media path.");
-    return EXIT_FAILURE;
-  }
+    const std::size_t nbCameras = mediaPaths.size();
 
-  if (maxFrameStep > 0 && minFrameStep >= maxFrameStep) {
-    ALICEVISION_LOG_ERROR("Setting 'minFrameStep' should be less than setting 'maxFrameStep'.");
-    return EXIT_FAILURE;
-  }
-
-  brands.resize(nbCameras);
-  models.resize(nbCameras);
-  mmFocals.resize(nbCameras);
-
-  // Debugging prints, print out all the parameters
-  {
-    if (nbCameras == 1)
-      ALICEVISION_LOG_INFO("Single camera");
-    else
-      ALICEVISION_LOG_INFO("Camera rig of " << nbCameras << " cameras.");
-
-    for (std::size_t i = 0; i < nbCameras; ++i) {
-      ALICEVISION_LOG_INFO("Camera: "            << mediaPaths.at(i)   << std::endl
-                        << "\t - brand: "        << brands.at(i)       << std::endl
-                        << "\t - model: "        << models.at(i)       << std::endl
-                        << "\t - focal (mm): "   << mmFocals.at(i)     << std::endl);
+    // Check output folder and update to its absolute path
+    {
+        const fs::path outDir = fs::absolute(outputFolder);
+        outputFolder = outDir.string();
+        if (!fs::is_directory(outDir)) {
+        ALICEVISION_LOG_ERROR("Cannot find folder: " << outputFolder);
+        return EXIT_FAILURE;
+        }
     }
-  }
 
-  // Initialize KeyframeSelector
-  KeyframeSelector selector(mediaPaths, sensorDbPath, outputFolder);
+    if (nbCameras < 1) {
+        ALICEVISION_LOG_ERROR("Program needs at least one media path.");
+        return EXIT_FAILURE;
+    }
 
-  // Set algorithm parameters
-  selector.setMinFrameStep(minFrameStep);
-  selector.setMaxFrameStep(maxFrameStep);
-  selector.setMaxOutFrame(maxNbOutFrame);
+    if (maxFrameStep > 0 && minFrameStep >= maxFrameStep) {
+        ALICEVISION_LOG_ERROR("Setting 'minFrameStep' should be less than setting 'maxFrameStep'.");
+        return EXIT_FAILURE;
+    }
 
-  // Process media paths with regular method
-  selector.processRegular();
+    brands.resize(nbCameras);
+    models.resize(nbCameras);
+    mmFocals.resize(nbCameras);
 
-  // Write selected keyframes
-  selector.writeSelection(brands, models, mmFocals);
+    // Debugging prints, print out all the parameters
+    {
+        if (nbCameras == 1)
+            ALICEVISION_LOG_INFO("Single camera");
+        else
+            ALICEVISION_LOG_INFO("Camera rig of " << nbCameras << " cameras.");
 
-  return EXIT_SUCCESS;
+        for (std::size_t i = 0; i < nbCameras; ++i) {
+            ALICEVISION_LOG_INFO("Camera: "              << mediaPaths.at(i)   << std::endl
+                                << "\t - brand: "        << brands.at(i)       << std::endl
+                                << "\t - model: "        << models.at(i)       << std::endl
+                                << "\t - focal (mm): "   << mmFocals.at(i)     << std::endl);
+        }
+    }
+
+    // Initialize KeyframeSelector
+    KeyframeSelector selector(mediaPaths, sensorDbPath, outputFolder);
+
+    // Set algorithm parameters
+    selector.setMinFrameStep(minFrameStep);
+    selector.setMaxFrameStep(maxFrameStep);
+    selector.setMaxOutFrame(maxNbOutFrame);
+
+    // Process media paths with regular method
+    selector.processRegular();
+
+    // Write selected keyframes
+    selector.writeSelection(brands, models, mmFocals);
+
+    return EXIT_SUCCESS;
 }
