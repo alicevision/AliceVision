2078640ff448dcb76f40ad626cc308cf52f2d450
Loïc Vital <mugulmotion@gmail.com>
2023-02-03 14:02:43 +0100

    [panorama] add copyrights

--- src/aliceVision/panorama/warper.hpp
+++ src/aliceVision/panorama/warper.hpp
@@ -1,3 +1,9 @@
+// This file is part of the AliceVision project.
+// Copyright (c) 2020 AliceVision contributors.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #pragma once
 
 #include "coordinatesMap.hpp"
