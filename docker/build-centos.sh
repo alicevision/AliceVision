90ab534f534eb122ad69ce6751d1d1a4c4d80fac
Hossam Hammady <github@hammady.net>
2023-02-18 11:50:13 -0500

    Specify container registry to push to

--- docker/build-centos.sh
+++ docker/build-centos.sh
@@ -11,7 +11,8 @@ test -z "$AV_VERSION" && AV_VERSION="$(git rev-parse --abbrev-ref HEAD)-$(git re
 test -z "$CUDA_VERSION" && CUDA_VERSION=11.3.1
 test -z "$CENTOS_VERSION" && CENTOS_VERSION=7
 test -z "$REPO_OWNER" && REPO_OWNER=alicevision
- 
+test -z "$DOCKER_REGISTRY" && DOCKER_REGISTRY=docker.io
+
 echo "AV_VERSION: $AV_VERSION"
 echo "CUDA_VERSION: $CUDA_VERSION"
 echo "CENTOS_VERSION: $CENTOS_VERSION"
@@ -19,20 +20,15 @@ echo "CENTOS_VERSION: $CENTOS_VERSION"
 docker/fetch.sh
 
 ## DEPENDENCIES
-DOCKER_TAG=${REPO_OWNER}/alicevision-deps:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg CENTOS_VERSION=${CENTOS_VERSION} \
-	--tag ${DOCKER_TAG} \
+	--tag ${REPO_OWNER}/alicevision-deps:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
 	-f docker/Dockerfile_centos_deps .
 
-if [ ! -z "$DOCKER_PUSH" ]; then
-	docker push ${DOCKER_TAG}
-fi
-
 ## ALICEVISION
-DOCKER_TAG=${REPO_OWNER}/alicevision:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}
+DOCKER_TAG=${DOCKER_REGISTRY}/${REPO_OWNER}/alicevision:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
--- docker/build-ubuntu.sh
+++ docker/build-ubuntu.sh
@@ -10,24 +10,20 @@ test -z "$AV_VERSION" && AV_VERSION="$(git rev-parse --abbrev-ref HEAD)-$(git re
 test -z "$CUDA_VERSION" && CUDA_VERSION=11.3.1
 test -z "$UBUNTU_VERSION" && UBUNTU_VERSION=20.04
 test -z "$REPO_OWNER" && REPO_OWNER=alicevision
+test -z "$DOCKER_REGISTRY" && DOCKER_REGISTRY=docker.io
 
 ./docker/fetch.sh
 
 ## DEPENDENCIES
-DOCKER_TAG=${REPO_OWNER}/alicevision-deps:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION}
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
-	--tag ${DOCKER_TAG} \
+	--tag ${REPO_OWNER}/alicevision-deps:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION} \
 	-f docker/Dockerfile_ubuntu_deps .
 
-if [ ! -z "$DOCKER_PUSH" ]; then
-	docker push ${DOCKER_TAG}
-fi
-
 ## ALICEVISION
-DOCKER_TAG=${REPO_OWNER}/alicevision:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION}
+DOCKER_TAG=${DOCKER_REGISTRY}/${REPO_OWNER}/alicevision:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION}
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
