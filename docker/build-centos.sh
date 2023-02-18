9113a610d0bcf061c420ac80c6750e0b6cd1400a
Hossam Hammady <github@hammady.net>
2023-02-17 22:17:31 -0500

    Parameterize docker repo owner and allow pushing

--- docker/build-centos.sh
+++ docker/build-centos.sh
@@ -10,6 +10,7 @@ test -e docker/fetch.sh || {
 test -z "$AV_VERSION" && AV_VERSION="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
 test -z "$CUDA_VERSION" && CUDA_VERSION=11.3.1
 test -z "$CENTOS_VERSION" && CENTOS_VERSION=7
+test -z "$REPO_OWNER" && REPO_OWNER=alicevision
  
 echo "AV_VERSION: $AV_VERSION"
 echo "CUDA_VERSION: $CUDA_VERSION"
@@ -18,20 +19,28 @@ echo "CENTOS_VERSION: $CENTOS_VERSION"
 docker/fetch.sh
 
 ## DEPENDENCIES
+DOCKER_TAG=${REPO_OWNER}/alicevision-deps:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg CENTOS_VERSION=${CENTOS_VERSION} \
-	--tag alicevision/alicevision-deps:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
+	--tag ${DOCKER_TAG} \
 	-f docker/Dockerfile_centos_deps .
 
+if [ ! -z "$DOCKER_PUSH" ]; then
+	docker push ${DOCKER_TAG}
+fi
+
 ## ALICEVISION
+DOCKER_TAG=${REPO_OWNER}/alicevision:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg CENTOS_VERSION=${CENTOS_VERSION} \
 	--build-arg AV_VERSION=${AV_VERSION} \
-	--tag alicevision/alicevision:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
+	--tag ${DOCKER_TAG} \
 	-f docker/Dockerfile_centos .
 
-
+if [ ! -z "$DOCKER_PUSH" ]; then
+	docker push ${DOCKER_TAG}
+fi
--- docker/build-ubuntu.sh
+++ docker/build-ubuntu.sh
@@ -9,22 +9,34 @@ test -e docker/fetch.sh || {
 test -z "$AV_VERSION" && AV_VERSION="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
 test -z "$CUDA_VERSION" && CUDA_VERSION=11.3.1
 test -z "$UBUNTU_VERSION" && UBUNTU_VERSION=20.04
+test -z "$REPO_OWNER" && REPO_OWNER=alicevision
 
 ./docker/fetch.sh
 
 ## DEPENDENCIES
+DOCKER_TAG=${REPO_OWNER}/alicevision-deps:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION}
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
-	--tag alicevision/alicevision-deps:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION} \
+	--tag ${DOCKER_TAG} \
 	-f docker/Dockerfile_ubuntu_deps .
 
+if [ ! -z "$DOCKER_PUSH" ]; then
+	docker push ${DOCKER_TAG}
+fi
+
 ## ALICEVISION
+DOCKER_TAG=${REPO_OWNER}/alicevision:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION}
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
 	--build-arg AV_VERSION=${AV_VERSION} \
-	--tag alicevision/alicevision:${AV_VERSION}-ubuntu${UBUNTU_VERSION}-cuda${CUDA_VERSION} \
+	--build-arg REPO_OWNER=${REPO_OWNER} \
+	--tag ${DOCKER_TAG} \
 	-f docker/Dockerfile_ubuntu .
+
+if [ ! -z "$DOCKER_PUSH" ]; then
+	docker push ${DOCKER_TAG}
+fi
