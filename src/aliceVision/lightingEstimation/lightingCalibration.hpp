1cc9598f7603dd8fdd14a26d0d178d46b9e88cc3
jmelou <jean.melou@gmail.com>
2023-03-01 18:18:24 +0100

    [lightCalibration] Add documentation

--- src/aliceVision/lightingEstimation/lightingCalibration.hpp
+++ src/aliceVision/lightingEstimation/lightingCalibration.hpp
@@ -16,24 +16,90 @@
 namespace aliceVision {
 namespace lightingEstimation {
 
+/**
+ * @brief Calibrates lighting direction for a set of images contained in a folder (WIP)
+ * @param[in] inputPath - Patht o the folder containing the images
+ * @param[in] outputPath - Path to the json file in which we write lights directions
+ */
 void lightCalibration(const std::string& inputPath, const std::string& outputPath);
 
+/**
+ * @brief Calibrates lighting direction for a set of images from a .sfm file
+ * @param[in] sfmData - .sfm file
+ * @param[in] inputJSON - Path to the json file containing the spheres parameters (see sphereDetection)
+ * @param[out] outputPath - Path to the json file in which we write lights directions
+ */
 void lightCalibration(const sfmData::SfMData& sfmData, const std::string& inputJSON, const std::string& outputPath);
 
+/**
+ * @brief Calibrates lighting direction for a set of images
+ * @param[in] imageList - List of pictures on which we want to calibrate lighting
+ * @param[in] allSpheresParams - A vector of array. Each one contains 3 floating-point: the coordinates of the sphere center in the picture frame and the radius of the sphere.
+ * @param[in] jsonName - The json file in which we write lights directions
+ * @param[in] focals  - Focal length of the cameras
+ */
 void lightCalibration(const std::vector<std::string>& imageList, const std::vector<std::array<float, 3>>& allSpheresParams, const std::string& jsonName, const std::vector<float>& focals);
 
+/**
+ * @brief Calibrates lighting direction for a set of images sharing same intrinsics and sphere parameters
+ * @param[in] imageList - List of pictures on which we want to calibrate lighting
+ * @param[in] sphereParam - An array of 3 floating-point: the coordinates of the sphere center in the picture frame and the radius of the sphere.
+ * @param[in] jsonName - The json file in which we write lights directions
+ * @param[in] focal  - Focal length of the camera
+ */
 void lightCalibration(const std::vector<std::string>& imageList, const std::array<float, 3>& sphereParam, const std::string& jsonName, const float focal);
 
+/**
+ * @brief Calibrates lighting direction of an image containing a sphere
+ * @param[in] picturePath - Path of the image file
+ * @param[in] sphereParam - An array of 3 floating-point: the coordinates of the sphere center in the picture frame and the radius of the sphere.
+ * @param[in] focal - Focal length of the camera
+ * @param[in] method - Method used for calibration (only "brightestPoint" for now)
+ * @param[out] lightingDirection - Output parameter for the estimated lighting direction
+ */
 void lightCalibrationOneImage(const std::string& picturePath, const std::array<float, 3>& sphereParam, const float focal, const std::string& method, Eigen::Vector3f& lightingDirection);
 
+/**
+ * @brief Computes the brightest point on a sphere
+ * This function cuts the input image around the sphere and applies a convolution filter to find the brightest point in the cut region.
+ * @param[in] sphereParam - An array of 3 floating-point: the coordinates of the sphere center in the picture frame and the radius of the sphere.
+ * @param[in] imageFloat - The grayscale image
+ * @param[out] brigthestPoint An Eigen::Vector2f vector containing the x and y coordinates of the brightest point on the image.
+ */
 void detectBrightestPoint(const std::array<float, 3>& sphereParam, const image::Image<float>& imageFloat, Eigen::Vector2f& brigthestPoint);
 
+/**
+ *  @brief Create a triangle kernel.
+ *  @param[in] kernelSize - Size of the kernel (should be an odd number).
+ *  @param[out] kernel - Vector where the kernel is stored.
+ */
 void createTriangleKernel(const size_t kernelSize, Eigen::VectorXf& kernel);
 
+/**
+ * @brief Computes the normal on a given point on a sphere
+ * @param[in] x_picture - The x coordinates of the point in the picture frame
+ * @param[in] y_picture - The y coordinates of the point in the picture frame
+ * @param[in] sphereParam - An array of 3 floating-point: the coordinates of the sphere center in the picture frame and the radius of the sphere.
+ * @param[out] currentNormal - The normal vector
+ */
 void getNormalOnSphere(const float x_picture, const float y_picture, const std::array<float, 3>& sphereParam, Eigen::Vector3f& currentNormal);
 
+/**
+ * @brief Cuts a region around a sphere from a grayscale image.
+ * @param[in] imageFloat - A grayscale image
+ * @param[in] sphereParam - An array of 3 floating-point: the coordinates of the sphere center in the picture frame and the radius of the sphere.
+ * @param[out] patch - The extracted region around the sphere.
+ * @param[out] patchOrigin - An array containing the x and y coordinates of the origin of the extracted region.
+ */
 void cutImage(const image::Image<float>& imageFloat, const std::array<float, 3>& sphereParam, image::Image<float>& patch, std::array<float, 2>& patchOrigin);
 
+/**
+ * @brief Write a JSON file containing light information. *
+ * @param[in] fileName - The path to the JSON file to generate.
+ * @param[in] imageList - Paths to the input images.
+ * @param[in] lightMat - A matrix containing the directions of the light sources.
+ * @param[in] intList - A vector of arrays containing the intensity of the light sources.
+ */
 void writeJSON(const std::string& fileName, const std::vector<std::string>& imageList, const Eigen::MatrixXf& lightMat, const std::vector<std::array<float, 3>>& intList);
 
 }
