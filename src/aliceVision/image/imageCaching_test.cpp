1e1c7cdc9157702b8e4d83200b33e3e8aa2a3335
Loïc Vital <mugulmotion@gmail.com>
2023-03-13 10:37:15 +0100

    [image] imageCaching: update unit tests

--- src/aliceVision/image/imageCaching_test.cpp
+++ src/aliceVision/image/imageCaching_test.cpp
@@ -15,8 +15,8 @@ using namespace aliceVision::image;
 
 BOOST_AUTO_TEST_CASE(create_cache) {
     ImageCache cache(256, 1024, EImageColorSpace::LINEAR);
-    BOOST_CHECK_EQUAL(cache.info().capacity, 256000000);
-    BOOST_CHECK_EQUAL(cache.info().maxSize, 1024000000);
+    BOOST_CHECK_EQUAL(cache.info().capacity, 256 * 1024 * 1024);
+    BOOST_CHECK_EQUAL(cache.info().maxSize, 1024 * 1024 * 1024);
     BOOST_CHECK_EQUAL(cache.readOptions().workingColorSpace, EImageColorSpace::LINEAR);
     BOOST_CHECK_EQUAL(cache.info().nbImages, 0);
     BOOST_CHECK_EQUAL(cache.info().contentSize, 0);
@@ -25,14 +25,14 @@ BOOST_AUTO_TEST_CASE(create_cache) {
 BOOST_AUTO_TEST_CASE(load_without_space) {
     ImageCache cache(0, 0, EImageColorSpace::LINEAR);
     const std::string filename = std::string(THIS_SOURCE_DIR) + "/image_test/lena.png";
-    BOOST_CHECK_THROW(auto img = cache.get<RGBAfColor>(filename, 0), std::exception);
+    BOOST_CHECK_THROW(auto img = cache.get<RGBAfColor>(filename), std::exception);
 }
 
 BOOST_AUTO_TEST_CASE(load_image_twice) {
     ImageCache cache(256, 1024, EImageColorSpace::LINEAR);
     const std::string filename = std::string(THIS_SOURCE_DIR) + "/image_test/lena.png";
-    auto img1 = cache.get<RGBAfColor>(filename, 0);
-    auto img2 = cache.get<RGBAfColor>(filename, 0);
+    auto img1 = cache.get<RGBAfColor>(filename);
+    auto img2 = cache.get<RGBAfColor>(filename);
     BOOST_CHECK_EQUAL(img1, img2);
     BOOST_CHECK_EQUAL(cache.info().nbImages, 1);
     BOOST_CHECK_EQUAL(cache.info().contentSize, img1->MemorySize());
@@ -43,12 +43,12 @@ BOOST_AUTO_TEST_CASE(load_image_twice) {
 BOOST_AUTO_TEST_CASE(load_all_pixel_types) {
     ImageCache cache(256, 1024, EImageColorSpace::LINEAR);
     const std::string filename = std::string(THIS_SOURCE_DIR) + "/image_test/lena.png";
-    auto imgUChar = cache.get<unsigned char>(filename, 0);
-    auto imgFloat = cache.get<float>(filename, 0);
-    auto imgRGB = cache.get<image::RGBColor>(filename, 0);
-    auto imgRGBf = cache.get<image::RGBfColor>(filename, 0);
-    auto imgRGBA = cache.get<image::RGBAColor>(filename, 0);
-    auto imgRGBAf = cache.get<image::RGBAfColor>(filename, 0);
+    auto imgUChar = cache.get<unsigned char>(filename);
+    auto imgFloat = cache.get<float>(filename);
+    auto imgRGB = cache.get<image::RGBColor>(filename);
+    auto imgRGBf = cache.get<image::RGBfColor>(filename);
+    auto imgRGBA = cache.get<image::RGBAColor>(filename);
+    auto imgRGBAf = cache.get<image::RGBAfColor>(filename);
     BOOST_CHECK_EQUAL(cache.info().nbImages, 6);
     BOOST_CHECK_EQUAL(cache.info().nbLoadFromDisk, 6);
 }
