56a5f72007112f9f378f4f71d62269488ee545a8
Fabien Castan <fabcastan@gmail.com>
2023-03-20 19:31:28 +0100

    [docker] add AV_DEPS_VERSION
    Use the date to create a version for pre-build dependencies

--- docker/build-centos.sh
+++ docker/build-centos.sh
@@ -7,31 +7,52 @@ test -e docker/fetch.sh || {
 	exit 1
 }
 
+test -z "$AV_DEPS_VERSION" && AV_DEPS_VERSION=2023.03.20
 test -z "$AV_VERSION" && AV_VERSION="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
 test -z "$CUDA_VERSION" && CUDA_VERSION=11.3.1
 test -z "$CENTOS_VERSION" && CENTOS_VERSION=7
- 
+test -z "$REPO_OWNER" && REPO_OWNER=alicevision
+test -z "$DOCKER_REGISTRY" && DOCKER_REGISTRY=docker.io
+
+
 echo "AV_VERSION: $AV_VERSION"
 echo "CUDA_VERSION: $CUDA_VERSION"
 echo "CENTOS_VERSION: $CENTOS_VERSION"
 
 docker/fetch.sh
 
+DEPS_DOCKER_TAG=${REPO_OWNER}/alicevision-deps:${AV_DEPS_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}
+
 ## DEPENDENCIES
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg CENTOS_VERSION=${CENTOS_VERSION} \
-	--tag alicevision/alicevision-deps:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
+	--tag ${DEPS_DOCKER_TAG} \
 	-f docker/Dockerfile_centos_deps .
+
+echo ""
+echo "  To upload results:"
+echo "docker push ${DEPS_DOCKER_TAG}"
+echo ""
+
+
+DOCKER_TAG=${REPO_OWNER}/alicevision:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}
 
 ## ALICEVISION
 docker build \
 	--rm \
 	--build-arg CUDA_VERSION=${CUDA_VERSION} \
 	--build-arg CENTOS_VERSION=${CENTOS_VERSION} \
+	--build-arg AV_DEPS_VERSION=${AV_DEPS_VERSION} \
 	--build-arg AV_VERSION=${AV_VERSION} \
-	--tag alicevision/alicevision:${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION} \
+	--tag ${DOCKER_TAG} \
 	-f docker/Dockerfile_centos .
 
+echo ""
+echo "  To upload results:"
+echo ""
+echo "docker push ${DEPS_DOCKER_TAG}"
+echo "docker push ${DOCKER_TAG}"
+echo ""
 
