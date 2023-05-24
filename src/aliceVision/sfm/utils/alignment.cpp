2ef8d0f94818403a9cd46d7548efbe8b92c9be18
Loïc Vital <mugulmotion@gmail.com>
2023-05-24 11:12:33 +0200

    [sfm/utils] ground level alignment: code comments

--- src/aliceVision/sfm/utils/alignment.cpp
+++ src/aliceVision/sfm/utils/alignment.cpp
@@ -1030,23 +1030,25 @@ void computeNewCoordinateSystemGroundAuto(const sfmData::SfMData& sfmData, Vec3&
 {
     out_t.fill(0.0);
 
+    // Collect landmark positions for ground detection
+    // Note: the Y axis is pointing down, therefore +Y is the direction to the ground
     std::vector<Vec3> points;
-
-    //Align with ground
     for (auto & plandmark: sfmData.getLandmarks())
     {
-        Vec3 X = plandmark.second.X;
-        bool foundUnder = false;
-
+        // Filter out landmarks with not enough observations
         if (plandmark.second.observations.size() < 3)
         {
             continue;
         }
 
+        // Filter out landmarks that lie above all the cameras that observe them
+        // This filtering step assumes that cameras should not be underneath the ground level
+        const Vec3 X = plandmark.second.X;
+        bool foundUnder = false;
         for (const auto & pObs : plandmark.second.observations)
         {
-            IndexT viewId = pObs.first;
-            Vec3 camCenter = sfmData.getPose(sfmData.getView(viewId)).getTransform().center();
+            const IndexT viewId = pObs.first;
+            const Vec3 camCenter = sfmData.getPose(sfmData.getView(viewId)).getTransform().center();
 
             if (X(1) > camCenter(1))
             {
@@ -1055,25 +1057,29 @@ void computeNewCoordinateSystemGroundAuto(const sfmData::SfMData& sfmData, Vec3&
             }
         }
 
+        // Store landmark position
         if (foundUnder)
         {
             points.push_back(X);
         }
     }
 
-    if (points.size() == 0)
+    if (points.empty())
     {
-        ALICEVISION_LOG_DEBUG("Ground detection failed as there is no valid point");
+        ALICEVISION_LOG_WARNING("Ground detection failed as there is no valid point");
+        return;
     }
     
-    
+    // Filter out statistical noise
+    // and take lowest point as the ground level
     const double noiseRatio = 1e-4;
-    int idxGround = double(points.size()) * noiseRatio;
-    std::nth_element(points.begin(), points.begin() + idxGround, points.end(), [](const Vec3 & pt1, const Vec3 & pt2) { return (pt1(1) > pt2(1)); });
-    double Yground = points[idxGround](1);
-    Vec3 groundAlign = {0.0, Yground, 0.0};
+    std::size_t idxGround = static_cast<std::size_t>(static_cast<double>(points.size()) * noiseRatio);
+    std::nth_element(
+        points.begin(), points.begin() + idxGround, points.end(),
+        [](const Vec3 & pt1, const Vec3 & pt2) { return (pt1(1) > pt2(1)); });
+    const double Yground = points[idxGround](1);
 
-    out_t = - groundAlign;
+    out_t = {0.0, -Yground, 0.0};
 }
 
 void computeNewCoordinateSystemAuto(const sfmData::SfMData& sfmData, double& out_S, Mat3& out_R, Vec3& out_t)
