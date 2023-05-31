ff88a55c12a90fa6a5e5e7b8bad7a63c47a22c27
Candice Bentéjac <candice.bentejac@gmail.com>
2023-05-31 14:14:05 +0200

    [photometricStereo] Add missing copyrights

--- src/aliceVision/photometricStereo/photometricDataIO.cpp
+++ src/aliceVision/photometricStereo/photometricDataIO.cpp
@@ -1,3 +1,9 @@
+// This file is part of the AliceVision project.
+// Copyright (c) 2023 AliceVision contributors.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #include "photometricDataIO.hpp"
 
 #include <aliceVision/image/io.hpp>
