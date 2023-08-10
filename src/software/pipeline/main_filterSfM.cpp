6aa4ef3cc84715e595adecc5c2b17af194cf4d1c
Abdelrahman AL MAROUK <abdelrahman.almarouk@gmail.com>
2023-08-10 10:12:24 +0200

    [filterSfM] add support for filtering landmarks

--- src/software/pipeline/main_filterSfM.cpp
+++ src/software/pipeline/main_filterSfM.cpp
@@ -11,6 +11,9 @@
 #include <aliceVision/system/main.hpp>
 #include <aliceVision/config.hpp>
 #include <aliceVision/sfmDataIO/viewIO.hpp>
+#include <aliceVision/mvsUtils/MultiViewParams.hpp>
+
+#include "nanoflann.hpp"
 
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
@@ -40,7 +43,171 @@ using namespace aliceVision::sfmDataIO;
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
-bool filterSfMData(SfMData& sfmData, int maxNbObservationsPerLandmark)
+static const std::size_t MAX_LEAF_ELEMENTS = 10;
+
+struct LandmarksAdaptator
+{
+    using Derived = LandmarksAdaptator; //!< In this case the dataset class is myself.
+    using T = double;
+
+    /// CRTP helper method
+    inline const Derived& derived() const { return *static_cast<const Derived*>(this); }
+    /// CRTP helper method
+    inline Derived& derived() { return *static_cast<Derived*>(this); }
+
+    const sfmData::Landmarks& _data;
+    LandmarksAdaptator(const sfmData::Landmarks& data)
+        : _data(data)
+    {
+    }
+
+    // Must return the number of data points
+    inline size_t kdtree_get_point_count() const { return _data.size(); }
+
+    // Returns the dim'th component of the idx'th point in the class:
+    inline T kdtree_get_pt(const size_t idx, int dim) const
+    {
+        auto it = _data.begin();
+        std::advance(it, idx);
+        return it->second.X(dim);
+    }
+
+    // Optional bounding-box computation: return false to default to a standard bbox computation loop.
+    //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it
+    //   again. Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX& bb) const
+    {
+        return false;
+    }
+};
+
+using KdTree = nanoflann::KDTreeSingleIndexAdaptor<
+    nanoflann::L2_Simple_Adaptor<double, LandmarksAdaptator>,
+    LandmarksAdaptator,
+    3, /* dim */
+    IndexT
+>;
+
+/**
+ * A result-set class used when performing a radius based search.
+ */
+class PixSizeSearch
+{
+public:
+    const double _radius_sq;
+    const std::vector<double>& _pixSize;
+    const double _pixSize_i;
+    bool found = false;
+
+    inline PixSizeSearch(double radius, const std::vector<double>& pixSize, int i)
+        : _radius_sq(radius * radius)
+        , _pixSize(pixSize)
+        , _pixSize_i(pixSize[i])
+    {
+    }
+
+    inline bool full() const { return found; }
+
+    inline bool addPoint(double dist, IndexT index)
+    {
+        if(dist < _radius_sq && _pixSize[index] < _pixSize_i)
+        {
+            found = true;
+            return false;
+        }
+        return true;
+    }
+
+    inline double worstDist() const { return _radius_sq; }
+};
+
+bool filterLandmarks(SfMData& sfmData, double radiusScale)
+{
+    mvsUtils::MultiViewParams mp(sfmData, "", "", "", false);
+    std::vector<double> landmarksPixSize(sfmData.getLandmarks().size());
+
+    ALICEVISION_LOG_INFO("Computing pixel size: started.");
+    #pragma omp parallel for
+    for(auto i = 0; i < sfmData.getLandmarks().size(); i++)
+    {
+        auto landmarkPair = sfmData.getLandmarks().begin();
+        std::advance(landmarkPair, i);
+        const sfmData::Landmark& landmark = landmarkPair->second;
+
+        // compute landmark pixSize
+        double pixSize = 0.;
+        int n = 0;
+        for(const auto& observationPair : landmark.observations)
+        {
+            const IndexT viewId = observationPair.first;
+            pixSize += mp.getCamPixelSize(Point3d(landmark.X.x(), landmark.X.y(), landmark.X.z()),
+                                          mp.getIndexFromViewId(viewId), observationPair.second.scale);
+            n++;
+        }
+        pixSize /= n;
+
+        landmarksPixSize[i] = pixSize;
+    }
+    ALICEVISION_LOG_INFO("Computing pixel size: done.");
+
+    //// sort landmarks by descending pixSize order
+    //std::stable_sort(landmarksPixSize.begin(), landmarksPixSize.end(), std::greater<>{});
+
+    ALICEVISION_LOG_INFO("Build nanoflann KdTree index.");
+    LandmarksAdaptator data(sfmData.getLandmarks());
+    KdTree tree(3, data, nanoflann::KDTreeSingleIndexAdaptorParams(10));
+    tree.buildIndex();
+    ALICEVISION_LOG_INFO("KdTree created for " << sfmData.getLandmarks().size() << " points.");
+    std::vector<IndexT> newIdx(sfmData.getLandmarks().size());
+    IndexT currentIdx = 0;
+
+    ALICEVISION_LOG_INFO("Identifying landmarks to remove: started.");
+    #pragma omp parallel for
+    for (auto i = 0; i < sfmData.getLandmarks().size(); i++)
+    {
+        PixSizeSearch search(landmarksPixSize[i] * radiusScale, landmarksPixSize, i);
+        bool found = tree.findNeighbors(search, sfmData.getLandmarks().at(i).X.data(), nanoflann::SearchParams());
+        if (!found)
+        {
+            newIdx[i] = -1;
+        }
+        else
+        {
+            newIdx[i] = currentIdx++;
+        }
+    }
+    ALICEVISION_LOG_INFO("Identifying landmarks to remove: done.");
+
+    ALICEVISION_LOG_INFO("Removing landmarks: started.");
+    Landmarks filteredLandmarks;
+    #pragma omp parallel for
+    for (auto i = 0; i < sfmData.getLandmarks().size(); i++)
+    {
+        if(newIdx[i] != -1)
+        {
+            filteredLandmarks[newIdx[i]] = sfmData.getLandmarks().at(i);
+        }
+    }
+    sfmData.getLandmarks() = filteredLandmarks;
+    ALICEVISION_LOG_INFO("Removing landmarks: done.");
+
+    //// take only best observations
+    //observationScores.resize(maxNbObservationsPerLandmark);
+
+
+
+    //// replace the observations
+    //Observations filteredObservations;
+    //for(auto observationScorePair : observationScores)
+    //{
+    //    filteredObservations[observationScorePair.second] = landmark.observations[observationScorePair.second];
+    //}
+    //landmark.observations = filteredObservations;
+    return true;
+}
+
+bool filterObservations(SfMData& sfmData, int maxNbObservationsPerLandmark)
 {
     #pragma omp parallel for
     for(auto i = 0; i < sfmData.getLandmarks().size(); i++)
@@ -97,6 +264,7 @@ int aliceVision_main(int argc, char *argv[])
     std::string inputSfmFilename;
     std::string outputSfmFilename;
     int maxNbObservationsPerLandmark = 5;
+    double radiusScale = 2;
 
     po::options_description requiredParams("Required parameters");
     requiredParams.add_options()
@@ -108,7 +276,9 @@ int aliceVision_main(int argc, char *argv[])
     po::options_description optionalParams("Optional parameters");
     optionalParams.add_options()
         ("maxNbObservationsPerLandmark", po::value<int>(&maxNbObservationsPerLandmark)->default_value(maxNbObservationsPerLandmark),
-         "Maximum number of allowed observations per landmark.");
+         "Maximum number of allowed observations per landmark.")
+        ("radiusScale", po::value<double>(&radiusScale)->default_value(radiusScale),
+         "Scale factor applied to pixel size based radius filter applied to landmarks.");
 
     CmdLine cmdline("AliceVision SfM filtering.");
     cmdline.add(requiredParams);
@@ -134,7 +304,14 @@ int aliceVision_main(int argc, char *argv[])
     }
 
     // filter SfM data
-    if(filterSfMData(sfmData, maxNbObservationsPerLandmark))
+    ALICEVISION_LOG_INFO("Filtering landmarks: started.");
+    bool success2 = filterLandmarks(sfmData, radiusScale);
+    ALICEVISION_LOG_INFO("Filtering landmarks: done.");
+    ALICEVISION_LOG_INFO("Filtering observations: started.");
+    bool success1 = filterObservations(sfmData, maxNbObservationsPerLandmark);
+    ALICEVISION_LOG_INFO("Filtering observations: done.");
+
+    if(success1)
     {
         sfmDataIO::Save(sfmData, outputSfmFilename, sfmDataIO::ESfMData::ALL);
         return EXIT_SUCCESS;
