d3111d7a6f754b7a70572585f0d106b8e89e24ec
Fabien SERVANT <fabien.servant@technicolor.com>
2023-08-18 15:10:50 +0200

    wrong variable name was used for memory

--- src/aliceVision/system/hardwareContext.hpp
+++ src/aliceVision/system/hardwareContext.hpp
@@ -24,7 +24,7 @@ public:
 
     void setUserMaxMemoryAvailable(size_t val)
     {
-        _maxUserCoresAvailable = val;
+        _maxUserMemoryAvailable = val;
     }
 
     unsigned int getUserMaxCoresAvailable() const
