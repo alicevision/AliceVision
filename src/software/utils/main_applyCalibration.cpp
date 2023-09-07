e777353591585486d96d4108d625a31361589700
Loïc Vital <mugulmotion@gmail.com>
2023-09-07 17:45:33 +0200

    [software] applyCalibration: update version

--- src/software/utils/main_applyCalibration.cpp
+++ src/software/utils/main_applyCalibration.cpp
@@ -20,7 +20,7 @@
 
 // These constants define the current software version.
 // They must be updated when the command line is changed.
-#define ALICEVISION_SOFTWARE_VERSION_MAJOR 1
+#define ALICEVISION_SOFTWARE_VERSION_MAJOR 2
 #define ALICEVISION_SOFTWARE_VERSION_MINOR 0
 
 namespace po = boost::program_options;
