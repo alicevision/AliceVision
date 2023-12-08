5ed4b8a2e06e160908c5cdb9e1bd9ead246c3ae4
Fabien Servant <fabien.servant@technicolor.com>
2023-12-08 09:37:17 +0100

    rename _structure to _landmarks for homogeneity

--- src/aliceVision/sfmData/SfMData.cpp
+++ src/aliceVision/sfmData/SfMData.cpp
@@ -79,12 +79,12 @@ bool SfMData::operator==(const SfMData& other) const
     }
 
     // Points IDs are not preserved
-    if (_structure.size() != other._structure.size())
+    if (_landmarks.size() != other._landmarks.size())
         return false;
 
-    Landmarks::const_iterator landMarkIt = _structure.begin();
-    Landmarks::const_iterator otherLandmarkIt = other._structure.begin();
-    for (; landMarkIt != _structure.end() && otherLandmarkIt != other._structure.end(); ++landMarkIt, ++otherLandmarkIt)
+    Landmarks::const_iterator landMarkIt = _landmarks.begin();
+    Landmarks::const_iterator otherLandmarkIt = other._landmarks.begin();
+    for (; landMarkIt != _landmarks.end() && otherLandmarkIt != other._landmarks.end(); ++landMarkIt, ++otherLandmarkIt)
     {
         // Points IDs are not preserved
         // Landmark
@@ -267,7 +267,7 @@ void SfMData::combine(const SfMData& sfmData)
     _rigs.insert(sfmData._rigs.begin(), sfmData._rigs.end());
 
     // structure
-    _structure.insert(sfmData._structure.begin(), sfmData._structure.end());
+    _landmarks.insert(sfmData._landmarks.begin(), sfmData._landmarks.end());
 
     // constraints
     constraints2d.insert(constraints2d.end(), sfmData.constraints2d.begin(), sfmData.constraints2d.end());
@@ -277,7 +277,7 @@ void SfMData::clear()
 {
     _views.clear();
     _intrinsics.clear();
-    _structure.clear();
+    _landmarks.clear();
     _posesUncertainty.clear();
     _landmarksUncertainty.clear();
     constraints2d.clear();
