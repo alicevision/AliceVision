e167f89c0a65ed4283671516486f5e612fb10639
Fabien Servant <fabien.servant@technicolor.com>
2023-12-19 15:00:57 +0100

    add distortion derivative for fisheye1 and brown

--- src/aliceVision/camera/DistortionBrown.cpp
+++ src/aliceVision/camera/DistortionBrown.cpp
@@ -5,7 +5,7 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "DistortionBrown.hpp"
-
+#include <aliceVision/system/Logger.hpp>
 #include <vector>
 
 namespace aliceVision {
@@ -25,7 +25,63 @@ Vec2 DistortionBrown::distoFunction(const std::vector<double>& params, const Vec
     return d;
 }
 
-Vec2 DistortionBrown::addDistortion(const Vec2& p) const { return (p + distoFunction(_distortionParams, p)); }
+Vec2 DistortionBrown::addDistortion(const Vec2& p) const 
+{ 
+    return (p + distoFunction(_distortionParams, p)); 
+}
+
+Eigen::Matrix2d DistortionBrown::getDerivativeAddDistoWrtPt(const Vec2& p) const 
+{
+    const double k1 = _distortionParams[0];
+    const double k2 = _distortionParams[1];
+    const double k3 = _distortionParams[2];
+    const double t1 = _distortionParams[3]; 
+    const double t2 = _distortionParams[4];
+
+    const double r2 = p(0) * p(0) + p(1) * p(1);
+    const double r4 = r2 * r2;
+    const double r6 = r4 * r2;
+    double px = p(0);
+    double py = p(1);
+
+    Eigen::Matrix2d ret;
+    ret(0, 0) = k1*r2 + k2*r4 + k3*r6 + 2*px*px*(k1 + 2*k2*r2 + 3*k3*r4) + 6*px*t2;
+    ret(0, 1) = 2*py*(px*(k1 + 2*k2*r2 + 3*k3*r4) + 2*t1 + t2);
+    ret(1, 0) = 2*px*(py*(k1 + 2*k2*r2 + 3*k3*r4) + t1 + 2*t2);
+    ret(1, 1) = k1*r2 + k2*r4 + k3*r6 + 2*py*py*(k1 + 2*k2*r2 + 3*k3*r4) + 6*py*t1;
+
+    return ret;
+}
+
+Eigen::MatrixXd DistortionBrown::getDerivativeAddDistoWrtDisto(const Vec2& p) const
+{
+    const double k1 = _distortionParams[0];
+    const double k2 = _distortionParams[1];
+    const double k3 = _distortionParams[2];
+    const double t1 = _distortionParams[3]; 
+    const double t2 = _distortionParams[4];
+
+    const double r2 = p(0) * p(0) + p(1) * p(1);
+    const double r4 = r2 * r2;
+    const double r6 = r4 * r2;
+    double px = p(0);
+    double py = p(1);
+
+    Eigen::Matrix<double, 2, 5> ret;
+    ret(0, 0) = px*r2;
+    ret(0, 1) = px*r4;
+    ret(0, 2) = px*r6;
+    ret(0, 3) = 2*py*py;
+    ret(0, 4) = 3*px*px + py*py;
+
+    ret(1, 0) = py*r2;
+    ret(1, 1) = py*r4;
+    ret(1, 2) = py*r6;
+    ret(1, 3) = px*px + 3.0 * py * py;
+    ret(1, 4) = 2.0 * px * px;
+
+    return ret;
+}
 
 Vec2 DistortionBrown::removeDistortion(const Vec2& p) const
 {
@@ -38,6 +94,18 @@ Vec2 DistortionBrown::removeDistortion(const Vec2& p) const
     }
 
     return p_u;
+}
+
+Eigen::Matrix2d DistortionBrown::getDerivativeRemoveDistoWrtPt(const Vec2& p) const
+{
+    ALICEVISION_THROW_ERROR("Brown inverse jacobian are not implemented");
+    return Eigen::Matrix2d::Identity();
+}
+
+Eigen::MatrixXd DistortionBrown::getDerivativeRemoveDistoWrtDisto(const Vec2& p) const
+{
+    ALICEVISION_THROW_ERROR("Brown inverse jacobian are not implemented");
+    return Eigen::Matrix<double, 2, 5>::Zero();
 }
 
 }  // namespace camera
