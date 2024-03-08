2d7e5a6dd5ef4103bd5e2673361a09eb687bd9a2
Fabien Servant <fabien.servant@technicolor.com>
2024-03-08 10:15:36 +0100

    Intersection code for aabb

--- src/aliceVision/geometry/Intersection.cpp
+++ src/aliceVision/geometry/Intersection.cpp
@@ -1,5 +1,5 @@
 // This file is part of the AliceVision project.
-// Copyright (c) 2023 AliceVision contributors.
+// Copyright (c) 2024 AliceVision contributors.
 // This Source Code Form is subject to the terms of the Mozilla Public License,
 // v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -44,6 +44,132 @@ bool rayIntersectUnitSphere(Vec3& coordinates, const Vec3& start, const Vec3& di
     coordinates = start + lambda * direction;
 
     return true;
+}
+
+bool rayIntersectAABB(const Eigen::Vector3d & bbMin, const Eigen::Vector3d & bbMax, const Eigen::Vector3d& start, const Eigen::Vector3d& direction, double& boundsMin, double& boundsMax)
+{
+    // x+lambda*dx>bbmin.x
+    // y+lambda*dy>bbmin.y
+    // z+lambda*dz>bbmin.z
+    // x+lambda*dx<bbmax.x
+    // y+lambda*dy<bbmax.y
+    // z+lambda*dz<bbmax.z
+
+    // lambda > (bbmin.x-x)/dx
+    // lambda > (bbmin.y-y)/dy
+    // lambda > (bbmin.z-z)/dz
+    // lambda < (bbmax.x-x)/dx
+    // lambda < (bbmax.y-y)/dy
+    // lambda < (bbmax.z-z)/dz
+
+
+    boundsMin = std::numeric_limits<double>::lowest();
+    boundsMax = std::numeric_limits<double>::max();
+
+    if (std::abs(direction.x()) > 1e-12)
+    {
+        double lmin = (bbMin.x() - start.x()) / direction.x();
+        double lmax = (bbMax.x() - start.x()) / direction.x();
+
+        if (direction.x() > 0.0)
+        {
+            boundsMin = std::max(boundsMin, lmin);
+            boundsMax = std::min(boundsMax, lmax);
+        }
+        else
+        {
+            boundsMin = std::max(boundsMin, lmax);
+            boundsMax = std::min(boundsMax, lmin);
+        }
+    }
+    else 
+    {
+        if (start.x() < bbMin.x() || start.x() > bbMax.x())
+        {
+            return false;
+        }
+    }
+
+    if (std::abs(direction.y()) > 1e-12)
+    {
+        double lmin = (bbMin.y() - start.y()) / direction.y();
+        double lmax = (bbMax.y() - start.y()) / direction.y();
+
+        if (direction.y() > 0.0)
+        {
+            boundsMin = std::max(boundsMin, lmin);
+            boundsMax = std::min(boundsMax, lmax);
+        }
+        else
+        {
+            boundsMin = std::max(boundsMin, lmax);
+            boundsMax = std::min(boundsMax, lmin);
+        }
+    }
+    else 
+    {
+        if (start.y() < bbMin.y() || start.y() > bbMax.y())
+        {
+            return false;
+        }
+    }
+
+    if (std::abs(direction.z()) > 1e-12)
+    {
+        double lmin = (bbMin.z() - start.z()) / direction.z();
+        double lmax = (bbMax.z() - start.z()) / direction.z();
+
+        if (direction.z() > 0.0)
+        {
+            boundsMin = std::max(boundsMin, lmin);
+            boundsMax = std::min(boundsMax, lmax);
+        }
+        else
+        {
+            boundsMin = std::max(boundsMin, lmax);
+            boundsMax = std::min(boundsMax, lmin);
+        }
+    }
+    else 
+    {
+        if (start.z() < bbMin.z() || start.z() > bbMax.z())
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isSegmentIntersectAABB(const Eigen::Vector3d & bbMin, const Eigen::Vector3d & bbMax, const Eigen::Vector3d& start, const Eigen::Vector3d& end)
+{
+    double boundsMin, boundsMax;
+
+    //Compute direction
+    Eigen::Vector3d direction = end - start;
+    double scale = direction.norm();
+    if (scale < 1e-12)
+    {
+        return false;
+    }
+    direction = direction / scale;
+
+    if (!rayIntersectAABB(bbMin, bbMax, start, direction, boundsMin, boundsMax))
+    {
+        return false;
+    }
+
+    if (boundsMax < 0.0)
+    {
+        return false;
+    }
+
+    if (boundsMax > scale)
+    {
+        return false;
+    }
+
+    return (boundsMin < boundsMax);
 }
 
 }  // namespace geometry
--- src/aliceVision/geometry/Intersection.hpp
+++ src/aliceVision/geometry/Intersection.hpp
@@ -1,5 +1,5 @@
 // This file is part of the AliceVision project.
-// Copyright (c) 2023 AliceVision contributors.
+// Copyright (c) 2024 AliceVision contributors.
 // This Source Code Form is subject to the terms of the Mozilla Public License,
 // v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -19,6 +19,46 @@ namespace geometry {
  * @return true if an intersection is found
  */
 bool rayIntersectUnitSphere(Vec3& coordinates, const Vec3& start, const Vec3& direction);
+
+/**
+ * Brief check if a point is inside an axis aligned bounding box
+ * @param bbMin the minimal values of the bounding box on each axis
+ * @param bbMax the maximal values of the bounding box on each axis
+ * @return true if the point is inside the bounding box
+*/
+inline bool isPointInsideAABB(const Eigen::Vector3d & bbMin, const Eigen::Vector3d & bbMax, const Eigen::Vector3d& pt)
+{
+    if (pt.x() + 1e-12 < bbMin.x()) return false;
+    if (pt.y() + 1e-12 < bbMin.y()) return false;
+    if (pt.z() + 1e-12 < bbMin.z()) return false;
+    if (pt.x() - 1e-12 > bbMax.x()) return false;
+    if (pt.y() - 1e-12 > bbMax.y()) return false;
+    if (pt.z() - 1e-12 > bbMax.z()) return false;
+
+    return true;
+}
+
+/**
+ * Brief Compute the two intersections of the rays with the bounding box
+ * @param bbMin the minimal values of the bounding box on each axis
+ * @param bbMax the maximal values of the bounding box on each axis
+ * @param start the starting coordinates for the ray
+ * @param direction the direction of the ray
+ * @param boundsMin the scale of the direction vector for which the ray enters the bounding box
+ * @param boundsMin the scale of the direction vector for which the ray leaves the bounding box
+ * @return false if the ray does not intersect the bounding box
+*/
+bool rayIntersectAABB(const Eigen::Vector3d & bbMin, const Eigen::Vector3d & bbMax, const Eigen::Vector3d& start, const Eigen::Vector3d& direction, double& boundsMin, double& boundsMax);
+
+/**
+ * @Brief is my segment intersecting the bounding box ?
+ * @param bbMin the minimal values of the bounding box on each axis
+ * @param bbMax the maximal values of the bounding box on each axis
+ * @param start the starting coordinates for the segment
+ * @param end the ending coordinates for the segment
+ * @return true if the segment intersect the bounding box
+*/
+bool isSegmentIntersectAABB(const Eigen::Vector3d & bbMin, const Eigen::Vector3d & bbMax, const Eigen::Vector3d& start, const Eigen::Vector3d& end);
 
 }  // namespace geometry
 }  // namespace aliceVision
