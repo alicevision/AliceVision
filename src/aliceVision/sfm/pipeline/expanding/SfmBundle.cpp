47880f4b4349de7e30aafbf7436958ebbb62805e
Fabien Servant <fabien.servant@technicolor.com>
2024-04-23 11:04:34 +0200

    Add a SfmBundle class

---
+++ src/aliceVision/sfm/pipeline/expanding/SfmBundle.cpp
@@ -0,0 +1,92 @@
+// This file is part of the AliceVision project.
+// Copyright (c) 2023 AliceVision contributors.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "SfmBundle.hpp"
+#include <aliceVision/sfm/sfmFilters.hpp>
+#include <aliceVision/sfm/bundle/BundleAdjustmentSymbolicCeres.hpp>
+#include <aliceVision/sfm/bundle/BundleAdjustmentCeres.hpp>
+
+namespace aliceVision {
+namespace sfm {
+
+bool SfmBundle::process(sfmData::SfMData & sfmData, const track::TracksHandler & tracksHandler, const std::set<IndexT> & viewIds)
+{   
+    BundleAdjustmentSymbolicCeres::CeresOptions options;
+    BundleAdjustment::ERefineOptions refineOptions;
+
+    refineOptions |= BundleAdjustment::REFINE_ROTATION; 
+    refineOptions |= BundleAdjustment::REFINE_TRANSLATION;
+    refineOptions |= BundleAdjustment::REFINE_STRUCTURE;
+    refineOptions |= BundleAdjustment::REFINE_INTRINSICS_ALL;
+
+    if (!initialize(sfmData, tracksHandler, viewIds))
+    {
+        return false;
+    }
+
+    options.setSparseBA();
+    BundleAdjustmentSymbolicCeres bundleObject(options, _minNbCamerasToRefinePrincipalPoint);
+
+    //Repeat until nothing change
+    do 
+    {
+        const bool success = bundleObject.adjust(sfmData, refineOptions);
+        if (!success)
+        {
+            return false;
+        }
+    }
+    while (cleanup(sfmData));
+
+    return true;
+}
+
+bool SfmBundle::cleanup(sfmData::SfMData & sfmData)
+{
+    // Remove observations with too large residuals
+    const std::size_t nbOutliersResidualErr = removeOutliersWithPixelResidualError(sfmData, _featureConstraint, _maxReprojectionError, _minTrackLength);
+
+    // Remove landmarks without enough observed parallax
+    const std::size_t nbOutliersAngleErr = removeOutliersWithAngleError(sfmData, _minAngleForLandmark);
+
+    // Remove poses without enough observations in an interative fashion
+    const std::size_t nbOutliers = nbOutliersResidualErr + nbOutliersAngleErr;
+    std::set<IndexT> removedViewsIdIteration;
+    bool somethingErased = eraseUnstablePosesAndObservations(sfmData, _minPointsPerPose, _minTrackLength, &removedViewsIdIteration);
+
+    bool somethingChanged = /*somethingErased || */(nbOutliers > _bundleAdjustmentMaxOutlier);
+
+    return somethingChanged;
+}
+
+bool SfmBundle::initialize(const sfmData::SfMData & sfmData, const track::TracksHandler & tracksHandler, const std::set<IndexT> & viewIds)
+{
+    bool enableLocalStrategy = _useLBA;
+
+    if (sfmData.getPoses().size() < _minNbCamerasLBA)
+    {
+        enableLocalStrategy = false;
+    }
+
+    if (enableLocalStrategy)
+    {
+        /*if (_lbaPolicy)
+        {
+            _lbaPolicy->build(sfmData, tracksHandler, viewIds);
+        }*/
+
+        /*_lbaGraph = std::make_shared<LocalBundleAdjustmentGraph>(sfmData);
+        _lbaGraph->setGraphDistanceLimit(_LBAGraphDistanceLimit);
+        _lbaGraph->updateGraphWithNewViews(sfmData, tracksHandler.getTracksPerView(), sfmData.getValidViews(), _LBAMinNbOfMatches);
+        _lbaGraph->computeGraphDistances(sfmData, viewIds);
+        _lbaGraph->convertDistancesToStates(sfmData);*/
+    }
+
+    return true;
+}
+
+} // namespace sfm
+} // namespace aliceVision
---
+++ src/aliceVision/sfm/pipeline/expanding/SfmBundle.hpp
@@ -0,0 +1,63 @@
+// This file is part of the AliceVision project.
+// Copyright (c) 2024 AliceVision contributors.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <aliceVision/types.hpp>
+#include <aliceVision/sfmData/SfMData.hpp>
+#include <aliceVision/sfm/bundle/BundleAdjustment.hpp>
+#include <aliceVision/track/TracksHandler.hpp>
+//#include <aliceVision/sfm/pipeline/expanding/LbaPolicy.hpp>
+//#include <aliceVision/sfm/pipeline/expanding/ExpansionHistory.hpp>
+
+namespace aliceVision {
+namespace sfm {
+
+class SfmBundle
+{
+public:
+    /**
+     * @brief Process bundle
+     * @param sfmData the scene description to optimize
+     * @param tracksHandler the tracks manager
+     * @param viewIds the set of views to bundle
+     * @return false if an error occured
+    */
+    bool process(sfmData::SfMData & sfmData, const track::TracksHandler & tracksHandler, const std::set<IndexT> & viewIds);
+
+private:
+    /**
+     * Initialize bundle properties
+    */
+    bool initialize(const sfmData::SfMData & sfmData, const track::TracksHandler & tracksHandler, const std::set<IndexT> & viewIds);
+
+    /**
+     * Cleanup sfmData 
+     * @param sfmData the scene to clean
+     * @return true if enough change occured during the cleaning
+    */
+    bool cleanup(sfmData::SfMData & sfmData);
+
+private:
+    //std::unique_ptr<LbaPolicy> _lbaPolicy;
+
+private:
+
+    EFeatureConstraint _featureConstraint = EFeatureConstraint::SCALE;
+    double _maxReprojectionError = 40.0;
+    double _minAngleForLandmark = 2.0;
+    size_t _minTrackLength = 2;
+    size_t _minPointsPerPose = 30;
+    size_t _bundleAdjustmentMaxOutlier = 50;
+    size_t _minNbCamerasToRefinePrincipalPoint = 3;
+    bool _useLBA = true;
+    size_t _minNbCamerasLBA = 100;
+    size_t _LBAGraphDistanceLimit = 1;
+    size_t _LBAMinNbOfMatches = 50;
+};
+
+} // namespace sfm
+} // namespace aliceVision
