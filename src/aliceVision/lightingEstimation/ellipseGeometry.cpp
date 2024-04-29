61e98b0ca5c8ec7008f160d629aa8e04a54f5f09
jmelou <jean.melou@gmail.com>
2024-04-29 11:54:13 +0200

    [LightingCalibration] Ellipse geometry : use OpenCV mask
    - debug
    - add or change comments

--- src/aliceVision/lightingEstimation/ellipseGeometry.cpp
+++ src/aliceVision/lightingEstimation/ellipseGeometry.cpp
@@ -126,80 +126,66 @@ void sphereRayIntersection(const Eigen::Vector3f& direction,
 
 void estimateSphereNormals(const std::array<float, 3>& sphereCenter,
                            const float sphereRadius,
-                           const image::Image<float>& ellipseMask,
                            const Eigen::Matrix3f& K,
                            image::Image<image::RGBfColor>& normals,
                            image::Image<float>& newMask) 
 {
     Eigen::Matrix3f invK = K.inverse();
-    bool print = false;
 
-    for (int i = 1; i < ellipseMask.rows(); ++i) {
-        for (int j = 1; j < ellipseMask.cols(); ++j) {
-            if (ellipseMask(i, j) == 1) {
-                // Get homogeneous coordinates of the pixel :
-                Eigen::Vector3f coordinates = Eigen::Vector3f(j, i, 1.0f);
+    for (int i = 0; i < normals.rows(); ++i) {
+        for (int j = 0; j < normals.cols(); ++j) {
+            // Get homogeneous coordinates of the pixel :
+            Eigen::Vector3f coordinates = Eigen::Vector3f(j, i, 1.0f);
 
-                // Get the direction of the ray :
-                Eigen::Vector3f direction = invK*coordinates;
+            // Get the direction of the ray :
+            Eigen::Vector3f direction = invK*coordinates;
 
-                // Estimate the interception of the ray with the sphere :
-                float delta = 0.0;
-                Eigen::Vector3f normal = Eigen::Vector3f(0.0f, 0.0f, 0.0f);
+            // Estimate the interception of the ray with the sphere :
+            float delta = 0.0;
+            Eigen::Vector3f normal = Eigen::Vector3f(0.0f, 0.0f, 0.0f);
 
-                print = false;
-            
-                sphereRayIntersection(direction, sphereCenter, sphereRadius, delta, normal);
+            sphereRayIntersection(direction, sphereCenter, sphereRadius, delta, normal);
 
-                // If the ray intercepts the sphere we add this pixel to the new mask :
-                if(delta > 0)
-                {
-                    newMask(i, j) = 1.0;
-                    normals(i, j) = image::RGBfColor(normal[0], normal[1], normal[2]);
-                }
-                else
-                {
-                    newMask(i, j) = 0.0;
-                    normals(i, j) = image::RGBfColor(0.0, 0.0, 0.0);
-                }
+            // If the ray intercepts the sphere we add this pixel to the new mask :
+            if(delta > 0)
+            {
+                newMask(i, j) = 1.0;
+                normals(i, j) = image::RGBfColor(normal[0], normal[1], normal[2]);
+            }
+            else
+            {
+                newMask(i, j) = 0.0;
+                normals(i, j) = image::RGBfColor(0.0, 0.0, 0.0);
             }
         }
     }
 }
 
-void getRealNormalOnSphere(const std::string& maskPath,
+void getRealNormalOnSphere(const cv::Mat& maskCV,
                        const Eigen::Matrix3f& K,
                        const float sphereRadius,
                        image::Image<image::RGBfColor>& normals,
                        image::Image<float>& newMask)
 {
-    // Charger l'image depuis le chemin spécifié
-    cv::Mat img = cv::imread(maskPath);
 
-    // Convertir l'image en niveaux de gris
-    cv::Mat gray;
-    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-
-    // Appliquer un seuillage pour convertir l'image en noir et blanc
+    // Apply a threshold to the image
     int thresholdValue = 150;
     cv::Mat thresh;
-    cv::threshold(gray, thresh, thresholdValue, 255, 0);
+    cv::threshold(maskCV, thresh, thresholdValue, 255, 0);
 
-    // Trouver les contours
+    // Find contours
     std::vector<std::vector<cv::Point>> contours;
     std::vector<cv::Vec4i> hierarchy;
     cv::findContours(thresh, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
-    // Sélectionner le premier contour
+    // Fit the ellipse to the first contour
     std::vector<cv::Point> cnt = contours[0];
-
-    // Ajuster l'ellipse sur l'objet sélectionné
     cv::RotatedRect ellipse = cv::fitEllipse(cnt);
     float angle = ellipse.angle;
     cv::Size2f size = ellipse.size;
     cv::Point2f center = ellipse.center;
 
-    // Convertir l'ellipse en un tableau de 5 paramètres:
+    // Ellipse is converted as five-parameter array
     std::array<float, 5> ellipseParameters;
 
     ellipseParameters[0] = angle;
@@ -210,10 +196,35 @@ void getRealNormalOnSphere(const std::string& maskPath,
 
     std::array<float, 3> sphereCenter;
     estimateSphereCenter(ellipseParameters, sphereRadius, K, sphereCenter);
-  
-    image::Image<float> mask;
-    photometricStereo::loadMask(maskPath, mask);
-    estimateSphereNormals(sphereCenter, sphereRadius, mask, K, normals, newMask);
+    estimateSphereNormals(sphereCenter, sphereRadius, K, normals, newMask);
+
+void getEllipseMaskFromSphereParameters(const std::array<float, 3>& sphereParam, const Eigen::Matrix3f& K, std::array<float, 5>& ellipseParameters, cv::Mat maskCV)
+{
+
+    // Distance between center of image and center of disk :
+    float imX = K(0, 2);
+    float imY = K(1, 2);
+    float f = K(0, 0);
+    float delta = sqrt((sphereParam[0])*(sphereParam[0]) + (sphereParam[1])*(sphereParam[1]));
+    // sphere params = x,y, radius
+    // ellipse params  = angle, x, y, semi minor axe, semi major axe
+
+    // Ellipse from sphere parameters
+    // semi minor axe = radius;
+    // semi major axe = formula from paper
+    // main direction = disc center to picture center
+    // ellipse center = disc center
+
+    float radians =  atan((sphereParam[0]) / (sphereParam[1]));
+    ellipseParameters[0] = radians * (180.0/3.141592653589793238463);
+
+    ellipseParameters[1] = sphereParam[0];
+    ellipseParameters[2] = sphereParam[1];
+    ellipseParameters[3] = sphereParam[2];
+
+
+    // a² = b² ((distance between image center and disc center)² + f² + b²)/(f² + b²)
+    ellipseParameters[4] = sqrt((ellipseParameters[3]*ellipseParameters[3]) * (delta * delta + f*f + ellipseParameters[3]*ellipseParameters[3])/(f*f + ellipseParameters[3]*ellipseParameters[3]));
 }
 
 } // namespace lightingEstimation
