692fec4207d39791875eb1e0dc0e931b5bae8e67
Phil Underwood <beardydoc@gmail.com>
2024-08-17 20:42:13 +0100

    update cuda docker version numbers

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -9,10 +9,11 @@ test -e docker/fetch.sh || {
 	exit 1
 }
 
-CUDA_VERSION=11.0 UBUNTU_VERSION=20.04 docker/build-ubuntu.sh
-CUDA_VERSION=11.0 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh
+CUDA_VERSION=11.0.3 UBUNTU_VERSION=20.04 docker/build-ubuntu.sh
+CUDA_VERSION=11.0.3 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh
 CUDA_VERSION=10.2 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh
 CUDA_VERSION=9.2 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh
 
+CUDA_VERSION=11.0.3 CENTOS_VERSION=7 docker/build-centos.sh
 CUDA_VERSION=10.2 CENTOS_VERSION=7 docker/build-centos.sh
 CUDA_VERSION=9.2 CENTOS_VERSION=7 docker/build-centos.sh
