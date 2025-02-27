94cd032244debca8d821dd65139db8ffefcb7180
Candice Bentéjac <candice.bentejac@gmail.com>
2025-02-27 14:19:37 +0100

    [src] Use `_DistanceType` to comply with nanoflann 1.7.0 changes

--- src/software/convert/main_importE57.cpp
+++ src/software/convert/main_importE57.cpp
@@ -59,10 +59,12 @@ struct PointInfoVectorAdaptator
 using PointInfoKdTree =
   nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<double, PointInfoVectorAdaptator>, PointInfoVectorAdaptator, 3>;
 
-template<typename DistanceType, typename IndexType = size_t>
+template<typename _DistanceType, typename IndexType = size_t>
 class BestPointInRadius
 {
   public:
+    // Necessary since nanoflann 1.7.0: https://github.com/jlblancoc/nanoflann/commit/9c84c4c32c2bfa7077cc8873dd3b5bcbb557da90
+    using DistanceType = _DistanceType;
     const DistanceType m_radius;
     const std::vector<dataio::E57Reader::PointInfo>& m_points;
     const std::map<int, Eigen::Vector3d>& m_cameras;
