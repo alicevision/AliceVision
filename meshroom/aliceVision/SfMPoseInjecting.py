CodeBeaverAI/Meshroom

[nodes] Replace `desc.Node.internalFolder` with `{nodeCacheFolder}`

meshroom/nodes/aliceVision/SfMPoseInjecting.py
@@ -50,6 +50,6 @@ Use a JSON file to inject poses inside the SfMData.
             name="output",
             label="SfMData",
             description="Path to the output SfM file.",
-            value=desc.Node.internalFolder + "sfmData.sfm",
+            value="{nodeCacheFolder}/sfmData.sfm",
         ),
     ]

meshroom/nodes/aliceVision/SfmBootstraping.py
@@ -73,6 +73,6 @@ class SfMBootStraping(desc.AVCommandLineNode):
             name="output",
             label="SfMData",
             description="Path to the output SfMData file.",
-            value=desc.Node.internalFolder + "sfm.json",
+            value="{nodeCacheFolder}/sfm.json",
         ),
     ]

meshroom/nodes/aliceVision/SfmExpanding.py
@@ -164,6 +164,6 @@ class SfMExpanding(desc.AVCommandLineNode):
             name="output",
             label="SfMData",
             description="Path to the output SfMData file.",
-            value=desc.Node.internalFolder + "sfm.json",
+            value="{nodeCacheFolder}/sfm.json",
         ),
     ]

alicevision/AliceVision

meshroom/aliceVision/SfMPoseInjecting.py
@@ -50,6 +50,6 @@ Use a JSON file to inject poses inside the SfMData.
             name="output",
             label="SfMData",
             description="Path to the output SfM file.",
-            value=desc.Node.internalFolder + "sfmData.abc",
+            value="{nodeCacheFolder}/sfmData.abc",
         ),
     ]

meshroom/aliceVision/SfmBootstraping.py
@@ -79,6 +79,6 @@ class SfMBootStraping(desc.AVCommandLineNode):
             name="output",
             label="SfMData",
             description="Path to the output SfMData file.",
-            value=desc.Node.internalFolder + "bootstrap.abc",
+            value="{nodeCacheFolder}/bootstrap.abc",
         ),
     ]

meshroom/aliceVision/SfmExpanding.py
@@ -170,12 +170,12 @@ class SfMExpanding(desc.AVCommandLineNode):
             name="output",
             label="SfMData",
             description="Path to the output SfMData file.",
-            value=desc.Node.internalFolder + "sfmExpanded.abc",
+            value="{nodeCacheFolder}/sfmExpanded.abc",
         ),
         desc.File(
             name="outputViewsAndPoses",
             label="Views And Poses",
             description="Path to the output SfMData file with cameras (views and poses).",
-            value=desc.Node.internalFolder + "cameras.sfm",
+            value="{nodeCacheFolder}/cameras.sfm",
         )
     ]