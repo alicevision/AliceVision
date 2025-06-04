1adbd8a4e37b415364c75b099f81b616baabfa4a
nicolas-lambert-tc <nicolas.lambert@technicolor.com>
2025-06-04 16:12:24 +0200

    [ui] Attribute: Refacto of the validation process, using the new validators paradigm
    Co-authored-by: Candice Bentéjac <candice.bentejac@gmail.com>

--- meshroom/aliceVision/LdrToHdrMerge.py
+++ meshroom/aliceVision/LdrToHdrMerge.py
@@ -5,6 +5,7 @@ import json
 from meshroom.core import desc
 from meshroom.core.utils import COLORSPACES, EXR_STORAGE_DATA_TYPE, VERBOSE_LEVEL
 from pyalicevision import parallelization as avpar
+from .ldrToHdrCommon import ImageCountShouldBeAMultipleOfBracketNumber
 
 def findMetadata(d, keys, defaultValue):
     v = None
@@ -67,8 +68,7 @@ to favour well-exposed pixels, and ghost artefacts caused by moving subjects can
             range=(0, 15, 1),
             invalidate=False,
             commandLineGroup="user",  # not used directly on the command line
-            errorMessage="The set number of brackets is not a multiple of the number of input images.\n"
-                         "Errors will occur during the computation.",
+            validators=[ImageCountShouldBeAMultipleOfBracketNumber()],
             exposed=True,
         ),
         desc.IntParam(
@@ -261,23 +261,15 @@ to favour well-exposed pixels, and ghost artefacts caused by moving subjects can
         if "userNbBrackets" not in node.getAttributes().keys():
             # Old version of the node
             return
-        node.userNbBrackets.validValue = True  # Reset the status of "userNbBrackets"
 
         cameraInitOutput = node.input.inputRootLink
         if not cameraInitOutput:
             node.nbBrackets.value = 0
             return
         if node.userNbBrackets.value != 0:
-            # The number of brackets has been manually forced: check whether it is valid or not
-            if cameraInitOutput and cameraInitOutput.node and cameraInitOutput.node.hasAttribute("viewpoints"):
-                viewpoints = cameraInitOutput.node.viewpoints.value
-                # The number of brackets should be a multiple of the number of input images
-                if (len(viewpoints) % node.userNbBrackets.value != 0):
-                    node.userNbBrackets.validValue = False
-                else:
-                    node.userNbBrackets.validValue = True
-            node.nbBrackets.value = node.userNbBrackets.value
-            return
+           if len(node.userNbBrackets.getErrorMessages()) > 0:
+                node.nbBrackets.value = node.userNbBrackets.value
+                return
 
         if not cameraInitOutput.node.hasAttribute("viewpoints"):
             if cameraInitOutput.node.hasAttribute("input"):
