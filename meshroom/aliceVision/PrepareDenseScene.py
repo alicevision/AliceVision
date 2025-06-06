cdc5796db6de81d8113e5d3fa43bcbc2babc92d9
Candice Bentéjac <candice.bentejac@gmail.com>
2025-06-06 19:04:50 +0200

    [meshroom] `PrepareDenseScene`: Expose additional parameters
    Expose `Images Folders` and `Masks Folder` parameters.

--- meshroom/aliceVision/PrepareDenseScene.py
+++ meshroom/aliceVision/PrepareDenseScene.py
@@ -32,6 +32,7 @@ This node export undistorted images so the depth map and texturing can be comput
             name="imagesFolders",
             label="Images Folders",
             description="Use images from specific folder(s). Filename should be the same or the image UID.",
+            exposed=True,
         ),
         desc.ListAttribute(
             elementDesc=desc.File(
@@ -43,6 +44,7 @@ This node export undistorted images so the depth map and texturing can be comput
             name="masksFolders",
             label="Masks Folders",
             description="Use masks from specific folder(s). Filename should be the same or the image UID.",
+            exposed=True,
         ),
         desc.ChoiceParam(
             name="maskExtension",
