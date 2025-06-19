8cd259039ce1330484c878ebecb3494c9b9c5c26
Candice Bentéjac <candice.bentejac@gmail.com>
2025-06-19 11:55:36 +0200

    [src] Move `__init__.py` for pyalicevision to src/aliceVision

--- src/aliceVision/__init__.py
+++ src/aliceVision/__init__.py
@@ -2,9 +2,7 @@ import os
 
 # Windows only
 if hasattr(os, 'add_dll_directory'):
-
     paths = os.environ['PATH'].split(os.pathsep)
-
     for p in paths:
         if not os.path.isdir(p):
             continue
