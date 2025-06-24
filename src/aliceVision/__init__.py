f032c5440c3018e0c348041d640692a6af4e8cfa
Candice Bentéjac <candice.bentejac@gmail.com>
2025-06-24 13:26:11 +0200

    [src] Update DLL discovery in `__init__.py` for the binding on Windows
    Rather than relying on the `PATH` variable, we are relying either
    on the `ALICEVISION_ROOT` variable, which is set when bundling,
    or the new `ALICEVISION_LIBPATH`, which is set with the same extra paths
    as the `PATH` variable was, except that it does not have any ambiguity.

--- src/aliceVision/__init__.py
+++ src/aliceVision/__init__.py
@@ -1,9 +1,20 @@
 import os
 
 # Windows only
-if hasattr(os, 'add_dll_directory'):
-    paths = os.environ['PATH'].split(os.pathsep)
-    for p in paths:
+if hasattr(os, "add_dll_directory"):
+    import pathlib
+    pyavFolder = pathlib.Path(__file__).resolve().parent
+    os.add_dll_directory(pyavFolder)
+
+    avRoot = os.environ.get("ALICEVISION_ROOT", "")
+    if not avRoot:
+        avRoot = pyavFolder.parent.parent.parent
+    avBin = os.path.join(avRoot, "bin")
+
+    os.add_dll_directory(avBin)
+
+    libfolders = os.environ.get("ALICEVISION_LIBPATH", "").split(os.pathsep)
+    for p in libfolders:
         if not os.path.isdir(p):
             continue
         os.add_dll_directory(p)
