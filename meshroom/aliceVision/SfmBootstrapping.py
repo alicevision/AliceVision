072b35f012c485285c6ec0a54d1c8dfff6e32d50
Fabien Servant <fabien.servant@technicolor.com>
2025-07-28 11:30:10 +0200

    Update node version

--- meshroom/aliceVision/SfmBootstrapping.py
+++ meshroom/aliceVision/SfmBootstrapping.py
@@ -1,4 +1,4 @@
-__version__ = "4.0"
+__version__ = "4.1"
 
 from meshroom.core import desc
 from meshroom.core.utils import VERBOSE_LEVEL
--- meshroom/aliceVision/SfmExpanding.py
+++ meshroom/aliceVision/SfmExpanding.py
@@ -1,4 +1,4 @@
-__version__ = "2.0"
+__version__ = "2.1"
 
 from meshroom.core import desc
 from meshroom.core.utils import VERBOSE_LEVEL
