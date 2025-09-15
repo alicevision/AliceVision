16040692dbdcf108daa4f61322cc7ac5170b5c52
Cedric Thebault <cedric.thebault@technicolor.com>
2025-09-15 09:53:15 +0200

    Add a new sample scene
    A sphere with a circle of cameras around

--- src/aliceVision/sfmDataIO/sceneSample.hpp
+++ src/aliceVision/sfmDataIO/sceneSample.hpp
@@ -11,11 +11,26 @@
 namespace aliceVision {
 namespace sfmDataIO {
 
+enum ESceneType
+{
+    SCENE_CUBE,
+    SCENE_SPHERE,
+};
+
+ESceneType ESceneType_stringToEnum(const std::string& SScene);
+std::string ESceneType_enumToString(const ESceneType EScene);
+std::ostream& operator<<(std::ostream& os, ESceneType p);
+std::istream& operator>>(std::istream& in, ESceneType& p);
+
 /**
  * @brief Create an SfmData with some arbitrary content.
  * This is used in unit tests to validate read/write sfmData files.
  */
-void generateSampleScene(sfmData::SfMData& output);
+void generateSampleScene(sfmData::SfMData& output, ESceneType scene=ESceneType::SCENE_CUBE);
+
+void generateCubeScene(sfmData::SfMData& output);
+
+void generateSphereScene(sfmData::SfMData& output, int pointsNb, int posesNb);
 
 }  // namespace sfmDataIO
 }  // namespace aliceVision
