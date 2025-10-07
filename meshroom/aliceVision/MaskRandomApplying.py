e5b9f2313c9ce3074de1324d738e17ab67fc3611
Fabien Servant <fabien.servant@technicolor.com>
2025-10-07 15:55:49 +0200

    Node to set random pixel values on masked coordinates

---
+++ meshroom/aliceVision/MaskRandomApplying.py
@@ -0,0 +1,51 @@
+__version__ = "1.0"
+
+from meshroom.core import desc
+from meshroom.core.utils import DESCRIBER_TYPES, VERBOSE_LEVEL
+
+import os.path
+
+
+class MaskRandomApplying(desc.AVCommandLineNode):
+    commandLine = "aliceVision_maskRandomApplying {allParams}"
+
+    size = desc.DynamicNodeSize("input")
+    category = "Utils"
+    documentation = """ Apply random values to images on the pixel whose mask value is 0 """
+
+    inputs = [
+        desc.File(
+            name="input",
+            label="SfmData",
+            description="Input SfmData with the list of views to process",
+            value="",
+        ),
+        desc.File(
+            name="masks",
+            label="Input Masks",
+            description="Input Masks assumed to have same names than input images",
+            value="",
+        ),
+        desc.ChoiceParam(
+            name="verboseLevel",
+            label="Verbose Level",
+            description="Verbosity level (fatal, error, warning, info, debug, trace).",
+            values=VERBOSE_LEVEL,
+            value="info",
+        )
+    ]
+
+    outputs = [
+        desc.File(
+            name="outputSfmData",
+            label="Output",
+            description="Path to the output sfmData.",
+            value="{nodeCacheFolder}/sfmData.abc",
+        ),
+        desc.File(
+            name="outputDirectory",
+            label="Output Images Directory",
+            description="Path to the directory with modified images.",
+            value="{nodeCacheFolder}",
+        )
+    ]
