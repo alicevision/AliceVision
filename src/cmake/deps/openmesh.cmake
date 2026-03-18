e432edeeaf423b3b1e63788125d1f11068ae5273
Sylvain Maziere <sylvain.maziere@technicolor.com>
2026-03-18 11:51:43 +0100

    Move openmesh pkgconfig file into correct dir

--- src/cmake/deps/openmesh.cmake
+++ src/cmake/deps/openmesh.cmake
@@ -9,6 +9,12 @@
 if(AV_BUILD_OPENMESH)
     set(OPENMESH_TARGET OpenMesh)
 
+    if(UNIX)
+        set(_fix_pkgfile ${CMAKE_COMMAND} -E rename <INSTALL_DIR>/libdata/pkgconfig/openmesh.pc <INSTALL_DIR>/lib/pkgconfig/openmesh.pc && ${CMAKE_COMMAND} -E rm -r <INSTALL_DIR>/libdata)
+    else()
+        set(_fix_pkgfile "")
+    endif()
+
     ExternalProject_Add(${OPENMESH_TARGET}
         URL              ${DEP_OPENMESH_URL}
         URL_HASH         ${DEP_OPENMESH_HASH}
@@ -23,7 +29,7 @@ if(AV_BUILD_OPENMESH)
         INSTALL_DIR      ${CMAKE_INSTALL_PREFIX}
         CONFIGURE_COMMAND
             ${CMAKE_COMMAND}
-            -DCMAKE_BUILD_TYPE=Release
+            -DCMAKE_BUILD_TYPE=${DEPS_CMAKE_BUILD_TYPE}
             -DBUILD_APPS=OFF
             -DOPENMESH_DOCS=OFF
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
@@ -32,10 +38,16 @@ if(AV_BUILD_OPENMESH)
             ${CMAKE_COMMAND} --build <BINARY_DIR>
                 --config Release
                 --parallel ${AV_BUILD_DEPENDENCIES_PARALLEL}
+        INSTALL_COMMAND
+            ${CMAKE_COMMAND} --install <BINARY_DIR>
+                COMMAND
+            ${_fix_pkgfile}
     )
 
     set(OPENMESH_CMAKE_FLAGS
         -DOPENMESH_DIR:PATH=${CMAKE_INSTALL_PREFIX}/share/OpenMesh/cmake
     )
     av_register_dep(${OPENMESH_TARGET})
+
+    unset(_fix_pkgfile)
 endif()
