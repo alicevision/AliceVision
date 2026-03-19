0a012696b2e01b642d0896b3cc43149c9b18fcad
Bastien DURIX <bastien.durix@fittingbox.com>
2026-03-19 12:02:19 +0100

    removed windows.h inclusion

--- src/software/pipeline/main_lightingCalibration.cpp
+++ src/software/pipeline/main_lightingCalibration.cpp
@@ -34,8 +34,6 @@
 #include <fstream>
 #include <filesystem>
 
-#include <Windows.h>
-  
 // These constants define the current software version.
 // They must be updated when the command line is changed.
 #define ALICEVISION_SOFTWARE_VERSION_MAJOR 1
