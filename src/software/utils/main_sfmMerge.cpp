83f89a329501b2cfc779dd8bdca147bb40efa06b
Fabien Servant <fabien.servant@technicolor.com>
2026-04-15 09:36:31 +0200

    Adding missing poses in sfmMerge

--- src/software/utils/main_sfmMerge.cpp
+++ src/software/utils/main_sfmMerge.cpp
@@ -123,6 +123,19 @@ bool simpleMerge(sfmData::SfMData & sfmData1, const sfmData::SfMData & sfmData2,
     }
 
     {
+        auto& poses1 = sfmData1.getPoses();
+        auto& poses2 = sfmData2.getPoses();
+        const size_t totalSize = poses1.size() + poses2.size();
+
+        poses1.insert(poses2.begin(), poses2.end());
+        if (poses1.size() < totalSize && !ignoreDuplicates)
+        {
+            ALICEVISION_LOG_ERROR("Unhandled error: common Poses ID between both SfMData");
+            return false;
+        }
+    }
+
+    {
         auto& intrinsics1 = sfmData1.getIntrinsics();
         auto& intrinsics2 = sfmData2.getIntrinsics();
         const size_t totalSize = intrinsics1.size() + intrinsics2.size();
