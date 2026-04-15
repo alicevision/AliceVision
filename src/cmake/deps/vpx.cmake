e3208930df6064c52576b6ece64006ebf90c21cf
Philipp Remy <philipp@philippremy.net>
2026-04-15 18:47:08 +0200

    FIX: libvpx: Apple specific configure flags
    If ALICEVISION_USE_RPATH is set, tell libVPX to set its install name directory to @rpath using environment LDFLAGS, as the autoconf script does not have a native switch for that.
    
    This commit fixes a build logic issue in libVPX, where the configure script is only able to detect Darwin up to version 24.X.X (macOS Sonoma). macOS 26 Tahoe is Darwin 25.X.X, causing the configure script to fall back to generic-gnu, which assumes a Linux host OS. As a workaround in CMake, we detect (using CMAKE_SYSTEM_VERSION) if the host Darwin is > 2. In that case, we explicitly set the newest available toolchain for libVPX, which logically is [x86_64/arm64]-darwin24-gcc.
    
    FIXME: To be removed when libVPX either changes its detection logic or a newer ABI- and API-compatible version with proper Darwin > 24 support is released.
    
    Signed-off-by: Philipp Remy <philipp@philippremy.net>

--- src/cmake/deps/vpx.cmake
+++ src/cmake/deps/vpx.cmake
@@ -10,6 +10,30 @@
 if(AV_BUILD_VPX AND AV_BUILD_FFMPEG)
     set(VPX_TARGET libvpx)
 
+    if(APPLE AND ALICEVISION_USE_RPATH)
+      set(VPX_APPLE_LDFLAGS_ENV_CMD ${CMAKE_COMMAND} -E env LDFLAGS=-Wl,-install_name,@rpath/libvpx.dylib)
+    else()
+      set(VPX_APPLE_LDFLAGS_ENV_CMD)
+    endif()
+
+    # This is currently required until libVPX properly supports macOS 26 Tahoe.
+    # The configure.sh script only detects Darwin versions up to 24.X.X,
+    # but macOS 26 Tahoe is version 25.X.X, causing the configure script
+    # to fall back to generic-gnu, which assumes Linux and therefore pulls
+    # in the wrong linker flags.
+    # If we detect macOS 26 or higher, we explicitly set the toolchain to
+    # be (x86_64/arm64)-darwin24-gcc. For this, use CMAKE_SYSTEM_VERSION.
+    set(VPX_TOOLCHAIN_FLAG)
+    if(APPLE)
+        if(CMAKE_SYSTEM_VERSION VERSION_GREATER_EQUAL 25) # Tahoe and later
+            if(CMAKE_OSX_ARCHITECTURES MATCHES "x86_64")
+                set(VPX_TOOLCHAIN_FLAG --target=x86_64-darwin24-gcc)
+            elseif(CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
+                set(VPX_TOOLCHAIN_FLAG --target=arm64-darwin24-gcc)
+            endif()
+        endif()
+    endif()
+
     ExternalProject_Add(${VPX_TARGET}
         GIT_REPOSITORY  ${DEP_VPX_GIT_REPO}
         GIT_TAG         ${DEP_VPX_GIT_TAG}
@@ -20,11 +44,13 @@ if(AV_BUILD_VPX AND AV_BUILD_FFMPEG)
         UPDATE_COMMAND  ""
         INSTALL_DIR     ${CMAKE_INSTALL_PREFIX}
         CONFIGURE_COMMAND
+            ${VPX_APPLE_LDFLAGS_ENV_CMD}
             <SOURCE_DIR>/configure
                 --prefix=<INSTALL_DIR>
                 --enable-shared
                 --disable-static
                 --disable-examples
+                ${VPX_TOOLCHAIN_FLAG}
         BUILD_COMMAND make -j${AV_BUILD_DEPENDENCIES_PARALLEL}
     )
     av_register_dep(${VPX_TARGET})
