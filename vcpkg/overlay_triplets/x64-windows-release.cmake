cabfe6255b96da6c8a3c69a38d601ae70042f465
Candice Bentéjac <candice.bentejac@gmail.com>
2026-08-12 16:55:02 +0200

    [vcpkg] Add custom triplet

---
+++ vcpkg/overlay_triplets/x64-windows-release.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_PROVIDED_FORTRAN ON)
+
+if(PORT STREQUAL "onnx")
+    # simply set to use single flag or list append to customize multiple flags
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DONNX_DISABLE_STATIC_REGISTRATION=1")
+endif()
